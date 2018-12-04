9b5c3c8f89548d77e05a5426793f3b074e5d76b3
chasers <chase@pagemodified.com>
2018-12-03 17:30:41 -0700

    Trying handle_cast

--- lib/logtail/main.ex
+++ lib/logtail/main.ex
@@ -10,6 +10,11 @@ defmodule Logtail.Main do
     {:ok, args}
   end
 
+  def handle_cast(table_name, timestamp, log_entry) do
+    insert_log(table_name, timestamp, log_entry)
+    {:noreply, table_name, timestamp, log_entry}
+  end
+
   def insert_log(table_name, timestamp, log_entry) do
     case :ets.info(table_name) do
       :undefined ->
