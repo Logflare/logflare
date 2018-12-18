6b586695410be526587d49e13f8b39ac7c1496c4
chasers <chase@pagemodified.com>
2018-12-18 11:38:16 -0700

    Authorizing socket and joining correct source

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -54,9 +54,12 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
 
-export default socket
+const createSocket = (sourceId) => {
+  let channel = socket.channel(`source:${sourceId}`, {})
+  channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+  };
+
+window.createSocket = createSocket;
--- lib/logtail_web/channels/user_socket.ex
+++ lib/logtail_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule LogtailWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", LogtailWeb.RoomChannel
+  channel "source:*", LogtailWeb.SourceChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
