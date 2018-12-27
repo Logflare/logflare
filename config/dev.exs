447f7cbc5b9610d963bc359e49a060eb82f52902
chasers <chase@pagemodified.com>
2018-12-27 14:06:00 -0700

    dev github oauth app

--- config/dev.exs
+++ config/dev.exs
@@ -50,3 +50,7 @@ config :phoenix, :stacktrace_depth, 20
    database: "logtail_dev",
    hostname: "localhost",
    pool_size: 10
+
+ config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+   client_id: "c4779963f3cd015f51f4",
+   client_secret: "7e3939f96209dbf6409321fcb18b1779caec1158"
