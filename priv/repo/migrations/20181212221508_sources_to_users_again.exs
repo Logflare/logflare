5581538411dba976a37cebb1859b5f69e4174c7e
chasers <chase@pagemodified.com>
2018-12-27 13:02:29 -0700

    remove shitty migration

--- priv/repo/migrations/20181212221508_sources_to_users_again.exs
+++
@@ -1,9 +0,0 @@
-defmodule Logflare.Repo.Migrations.SourcesToUsersAgain do
-  use Ecto.Migration
-
-  def change do
-    alter table(:sources) do
-      add :user_id, references (:users)
-    end
-  end
-end
