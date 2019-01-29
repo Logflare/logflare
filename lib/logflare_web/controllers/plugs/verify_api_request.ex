86ca434b1ce9dc817a6ca7c0646af4929536ec2c
chasers <chase@pagemodified.com>
2019-01-29 11:15:38 -0700

    Check that log entry is not nil.

--- lib/logflare_web/controllers/plugs/verify_api_request.ex
+++ lib/logflare_web/controllers/plugs/verify_api_request.ex
@@ -15,9 +15,11 @@ defmodule LogflareWeb.Plugs.VerifyApiRequest do
     api_key = headers["x-api-key"]
     source = conn.params["source"]
     source_name = conn.params["source_name"]
+    log_entry = conn.params["log_entry"]
 
     conn
       |> check_api_key(api_key)
+      |> check_log_entry(log_entry)
       |> check_source_and_name(source, source_name)
       |> check_source_token(source)
   end
@@ -32,6 +34,20 @@ defmodule LogflareWeb.Plugs.VerifyApiRequest do
         |> render("index.json", message: message)
         |> halt()
       _ ->
+        conn
+    end
+  end
+
+  defp check_log_entry(conn, log_entry) do
+    case log_entry == nil do
+      true ->
+        message = "Log entry needed."
+        conn
+        |> put_status(403)
+        |> put_view(LogflareWeb.LogView)
+        |> render("index.json", message: message)
+        |> halt()
+      false ->
         conn
     end
   end
