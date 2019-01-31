80603824ba73130eb759bc4fe6b16263c8436937
chasers <chase@pagemodified.com>
2019-01-30 23:28:29 -0700

    TTLing logs to 24 hours. Could be better with delete_match.

--- lib/logflare/table_owner.ex
+++ lib/logflare/table_owner.ex
@@ -19,7 +19,40 @@ defmodule Logflare.TableOwner do
     table = website_table
     table_args = [:named_table, :ordered_set, :public]
     :ets.new(table, table_args)
+    state = [{:table, website_table}]
+    check_ttl(5000)
     {:reply, website_table, state}
+  end
+
+  def handle_info(:ttl, state) do
+    website_table = state[:table]
+    first = :ets.first(website_table)
+
+    case first != :"$end_of_table" do
+      true ->
+        {_monotime, timestamp, _unique_int} = first
+        now = System.os_time(:microsecond)
+        day_ago = now - 86400000000
+        if timestamp < day_ago do
+          # :ets.delete_match(website_table) I'm too dumb for this
+          # Match an argument list of three, where the second argument is a number > 3:
+          # [{['_', '$1', '_'],
+          #   [{ '>', '$1', 3}],
+          #     []}]
+          :ets.delete(website_table, first)
+          IO.puts("deleted stuff")
+        end
+        check_ttl(100)
+        # GenServer.cast(self(), :ttl) # loop
+        {:noreply, state}
+      false ->
+        check_ttl(5000) # Reschedule once more
+        {:noreply, state}
+    end
+  end
+
+  defp check_ttl(ms) do
+    Process.send_after(self(), :ttl, ms)
   end
 
 end
