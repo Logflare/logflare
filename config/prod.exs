72b12d7cc697a1ebd4950d030b3291242789b360
chasers <chase@pagemodified.com>
2019-03-17 17:02:03 -0700

    Update pool size to match

--- config/prod.exs
+++ config/prod.exs
@@ -67,9 +67,7 @@ config :phoenix, :serve_endpoints, true
 #
 
 config :logflare, Logflare.Repo,
-  # adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
+  pool_size: 22,
   ssl: true,
   prepare: :unnamed
 
