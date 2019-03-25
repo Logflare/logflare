04359a825df979086146b2f64052c2fdac9464ff
chasers <chase@pagemodified.com>
2019-03-25 10:28:07 -0700

    Remove port on email links.

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :logflare, LogflareWeb.Endpoint,
   http: [port: 4000],
-  url: [host: "logflare.app", port: 80, scheme: "https"],
+  url: [host: "logflare.app", scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   code_reloader: false,
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Logflare.Mixfile do
   def project do
     [
       app: :logflare,
-      version: "0.7.2",
+      version: "0.7.3",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
