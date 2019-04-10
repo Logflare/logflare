c0419a6de6256e69d4324e15ba555cc703b644a7
chasers <chase@pagemodified.com>
2019-04-10 10:30:49 -0700

    Cowboy max connections infinity

--- lib/logflare_web/endpoint.ex
+++ lib/logflare_web/endpoint.ex
@@ -1,44 +1,48 @@
 defmodule LogflareWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :logflare
 
-  socket "/socket", LogflareWeb.UserSocket,
-   websocket: true
+  socket("/socket", LogflareWeb.UserSocket, websocket: true)
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :logflare, gzip: false,
+  plug(Plug.Static,
+    at: "/",
+    from: :logflare,
+    gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.Logger
+  plug(Plug.Logger)
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Jason
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
+  plug(Plug.Session,
     store: :cookie,
     key: "_logflare_key",
     signing_salt: "INPMyhPE"
+  )
 
-  plug LogflareWeb.Router
+  plug(LogflareWeb.Router)
 
   @doc """
   Callback invoked for dynamically configuring the endpoint.
