99b9eb6a4c458655792bb3b5045c862476d12790
ontofractal <ontofractal@protonmail.com>
2019-05-01 17:24:21 +0300

    add elixir_logger_view module

---
+++ lib/logflare_web/views/elixir_logger_view.ex
@@ -0,0 +1,8 @@
+defmodule LogflareWeb.ElixirLoggerView do
+  use LogflareWeb, :view
+  alias LogflareWeb.ElixirLoggerView
+
+  def render("index.json", %{message: message}) do
+    %{message: message}
+  end
+end
