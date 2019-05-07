5a35e8f84d76331ccf32075cabfc84f56773abaa
chasers <chase@pagemodified.com>
2019-05-07 16:27:47 -0700

    Validating that we have access to that GCP project on form submit

--- lib/logflare/user.ex
+++ lib/logflare/user.ex
@@ -2,6 +2,8 @@ defmodule Logflare.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Logflare.Google.BigQuery
+
   schema "users" do
     field(:email, :string)
     field(:provider, :string)
@@ -38,5 +40,21 @@ defmodule Logflare.User do
       :bigquery_project_id
     ])
     |> validate_required([:email, :provider, :token])
+    |> validate_gcp_project(:bigquery_project_id, user_id: user.id)
+  end
+
+  def validate_gcp_project(changeset, field, options \\ []) do
+    validate_change(changeset, field, fn _, bigquery_project_id ->
+      case BigQuery.create_dataset(Integer.to_string(options[:user_id]), bigquery_project_id) do
+        {:ok, _} ->
+          []
+
+        {:error, %Tesla.Env{status: 409}} ->
+          []
+
+        {:error, _message} ->
+          [{field, options[:message] || "Check your GCP permissions!"}]
+      end
+    end)
   end
 end
