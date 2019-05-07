7d1b205b6a9fa1eca017d3ab77e25c43d06dc3f0
ontofractal <ontofractal@protonmail.com>
2019-05-07 20:03:51 +0300

    add schema builder test for build_table_schema/1 @list(map) of depth 2

--- test/logflare/google/bigquery/schema_builder_test.exs
+++ test/logflare/google/bigquery/schema_builder_test.exs
@@ -1,18 +1,15 @@
 defmodule Logflare.Google.BigQuery.TableSchemaBuilderTest do
   import Logflare.BigQuery.TableSchemaBuilder
-  use ExUnit.Case
+  import Logflare.Google.BigQuery.TestUtils
+  alias GoogleApi.BigQuery.V2.Model.TableFieldSchema, as: TFS
+  use ExUnit.Case, async: true
 
   describe "schema builder" do
-    @tag run: true
     test "build_table_schema/1 @list(map) of depth 1" do
       tfs =
         build_fields_schemas([
-          %{
-            "string1" => "string1val"
-          },
-          %{
-            "string2" => "string1val"
-          }
+          %{"string1" => "string1val"},
+          %{"string2" => "string1val"}
         ])
 
       assert tfs == [
@@ -31,6 +28,66 @@ defmodule Logflare.Google.BigQuery.TableSchemaBuilderTest do
                  type: "STRING"
                }
              ]
+    end
+
+    @tag run: true
+    test "build_table_schema/1 @list(map) of depth 2" do
+      tfs =
+        build_fields_schemas([
+          %{
+            "string1" => "string1val",
+            "map_lvl_2" => %{
+              "string3" => "string"
+            }
+          },
+          %{
+            "string2" => "string1val",
+            "map_lvl_2" => %{
+              "string4" => "string"
+            }
+          }
+        ])
+
+      expected = [
+        %TFS{
+          description: nil,
+          mode: "NULLABLE",
+          name: "map_lvl_2",
+          type: "REPEATED",
+          fields: [
+            %TFS{
+              description: nil,
+              fields: nil,
+              mode: "NULLABLE",
+              name: "string3",
+              type: "STRING"
+            },
+            %TFS{
+              description: nil,
+              fields: nil,
+              mode: "NULLABLE",
+              name: "string4",
+              type: "STRING"
+            }
+          ]
+        },
+        %TFS{
+          description: nil,
+          fields: nil,
+          mode: "NULLABLE",
+          name: "string1",
+          type: "STRING"
+        },
+        %TFS{
+          description: nil,
+          fields: nil,
+          mode: "NULLABLE",
+          name: "string2",
+          type: "STRING"
+        }
+      ]
+
+      assert_equal_schemas(tfs, expected)
     end
   end
 end
