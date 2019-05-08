3a9f375b7f23761d87b29b0fb157837845d773e7
ontofractal <ontofractal@protonmail.com>
2019-05-08 19:37:15 +0300

    add nested values types validation for key0.[2,3].key2.[1,2].key_lvl3

--- test/logflare/google/validator/value_types_validator.ex
+++ test/logflare/google/validator/value_types_validator.ex
@@ -3,10 +3,10 @@ defmodule Logflare.Google.BigQuery.Validator.NestedValuesTest do
   import Logflare.Google.BigQuery.Validator.NestedValues
 
   describe "nested values validation" do
-    test "diverging types in stacktrace.[0,1].key1.key2" do
+    test "diverging types in key0.[0,1].key1.key2" do
       payload = fn v ->
         %{
-          "stacktrace" => [
+          "key0" => [
             %{"key1" => %{"key2" => v}, "key11" => "string"},
             %{"key1" => %{"key2" => "string"}, "key12" => 0}
           ]
@@ -17,10 +17,10 @@ defmodule Logflare.Google.BigQuery.Validator.NestedValuesTest do
       refute valid?(payload.(1))
     end
 
-    test "diverging types in stacktrace.[0,1].key1.[0].key2" do
+    test "diverging types in key0.[0,1].key1.[0].key2" do
       payload = fn v ->
         %{
-          "stacktrace" => [
+          "key0" => [
             %{"key1" => [%{"key2" => v}]},
             %{"key1" => [%{"key2" => "string"}]}
           ]
@@ -29,6 +29,34 @@ defmodule Logflare.Google.BigQuery.Validator.NestedValuesTest do
 
       assert valid?(payload.("string"))
       refute valid?(payload.(1))
+    end
+
+    test "diverging types in key0.[2,3].key2.[1,2].key_lvl3" do
+      payload = fn v1, v2 ->
+        %{
+          "key0" => [
+            %{"key1" => "string"},
+            %{"key1" => "string"},
+            %{
+              "key2" => [
+                %{"keylvl3" => v1},
+                %{"keylvl3" => "string"}
+              ]
+            },
+            %{
+              "key2" => [
+                %{"keylvl3" => "string"},
+                %{"keylvl3" => v2}
+              ]
+            }
+          ]
+        }
+      end
+
+      assert valid?(payload.("string", "string"))
+      refute valid?(payload.(1, "string"))
+      refute valid?(payload.("string", true))
+      refute valid?(payload.(1, %{}))
     end
   end
 end
