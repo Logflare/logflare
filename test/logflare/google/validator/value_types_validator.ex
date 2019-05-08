a7a7055e634964e50d98a45cebe6440548a47e33
ontofractal <ontofractal@protonmail.com>
2019-05-08 19:30:40 +0300

    add nested values types validation for key0.[0,1].key1.key2

---
+++ test/logflare/google/validator/value_types_validator.ex
@@ -0,0 +1,21 @@
+defmodule Logflare.Google.BigQuery.Validator.NestedValuesTest do
+  use ExUnit.Case
+  import Logflare.Google.BigQuery.Validator.NestedValues
+
+  describe "nested values validation" do
+    test "diverging types in stacktrace.[0,1].key1.key2" do
+      payload = fn v ->
+        %{
+          "stacktrace" => [
+            %{"key1" => %{"key2" => v}, "key11" => "string"},
+            %{"key1" => %{"key2" => "string"}, "key12" => 0}
+          ]
+        }
+      end
+
+      assert valid?(payload.("string"))
+      refute valid?(payload.(1))
+    end
+
+  end
+end
