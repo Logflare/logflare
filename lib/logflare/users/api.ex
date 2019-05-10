10d03883fbcf4cf99090cb9fa974b9dcac3204e7
ontofractal <ontofractal@protonmail.com>
2019-05-10 22:03:09 +0300

    add descriptive success tuples to  Users.Api

--- lib/logflare/users/api.ex
+++ lib/logflare/users/api.ex
@@ -10,9 +10,15 @@ defmodule Logflare.Users.API do
     source_rate = Sources.get_api_rate_by_id(sid, bucket: :default)
     user_rate = get_total_user_api_rate(user.id)
 
-    quotas = Users.Cache.get_api_quotas(user.id, sid)
-
-    source_rate <= quotas.source and user_rate <= quotas.user
+    with {:ok, quotas} <- Users.Cache.get_api_quotas(user.id, sid),
+         {:source_rate, true} <- {:source_rate, source_rate <= quotas.source},
+         {:user_rate, true} <- {:user_rate, user_rate <= quotas.user} do
+      :ok
+    else
+      {:error, reason} -> {:error, reason}
+      {:source_rate, false} -> {:error, "Source rate is over the API quota"}
+      {:user_rate, false} -> {:error, "User rate is over the API quota"}
+    end
   end
 
   def get_total_user_api_rate(user) do
