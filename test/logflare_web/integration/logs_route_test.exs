158f5469a5e9ec9e0a47e882afd80bf39e92afd8
ontofractal <ontofractal@protonmail.com>
2019-05-12 00:07:40 +0300

    add cases to LogsRouteTest to validate api_key and source existence

--- test/logflare_web/integration/logs_route_test.exs
+++ test/logflare_web/integration/logs_route_test.exs
@@ -4,15 +4,33 @@ defmodule LogflareWeb.LogsRouteTest do
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.mode(Logflare.Repo, {:shared, self()})
-    u = insert(:user, api_key: Faker.String.base64())
-    {:ok, user: u}
+    s = insert(:source, token: Faker.UUID.v4())
+    u = insert(:user, api_key: Faker.String.base64(), sources: [s])
+    {:ok, user: u, sources: [s]}
   end
 
   describe "POST /logs" do
-    test "without api key", %{conn: conn, user: user} do
-
+    test "fails without api key", %{conn: conn, user: user} do
       conn = post(conn, "/logs", %{"log_entry" => %{}})
       assert json_response(conn, 403) == %{"message" => "Unknown x-api-key."}
+    end
+
+    test "fails without source or source_name", %{conn: conn, user: user} do
+      conn =
+        conn
+        |> put_req_header("x-api-key", user.api_key)
+        |> post("/logs", %{"log_entry" => %{}})
+
+      assert json_response(conn, 403) == %{"message" => "Source or source_name needed."}
+    end
+
+    test "succeeds with api_key and source", %{conn: conn, user: user, sources: [s]} do
+      conn =
+        conn
+        |> put_req_header("x-api-key", user.api_key)
+        |> post("/logs", %{"log_entry" => %{}, "source" => s.token})
+
+      assert json_response(conn, 200) == %{}
     end
   end
 end
