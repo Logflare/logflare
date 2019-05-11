6e99db2426b3f5a0dc073de3f015d00d1eaed6a1
ontofractal <ontofractal@protonmail.com>
2019-05-11 20:33:51 +0300

    update Users.API to return user/source API metrics data and message

--- lib/logflare/sources/sources.ex
+++ lib/logflare/sources/sources.ex
@@ -1,7 +1,11 @@
 defmodule Logflare.Sources do
   alias Logflare.SourceRateCounter, as: SRC
 
+  def get_metrics(sid, bucket: :default) do
+    SRC.get_metrics(sid, :default)
+  end
+
   def get_api_rate_by_id(sid, bucket: :default) do
-    SRC.get_avg_rate(sid) 
+    SRC.get_avg_rate(sid)
   end
 end
--- lib/logflare/users/api.ex
+++ lib/logflare/users/api.ex
@@ -1,29 +1,60 @@
 defmodule Logflare.Users.API do
   @callback action_allowed?(map) :: boolean
+
   alias Logflare.{Users, Sources}
   @api_call_logs {:api_call, :logs_post}
 
   def action_allowed?(%{type: @api_call_logs} = action) do
+  def verify_api_rates_quotas(%{type: @api_call_logs} = action) do
     %{source_id: sid, user: user} = action
 
-    source_rate = Sources.get_api_rate_by_id(sid, bucket: :default)
-    user_rate = get_total_user_api_rate(user.id)
+    source_bucket_metrics = Sources.get_metrics(sid, bucket: :default)
+    user_sum_of_sources = get_total_user_api_rate(user.id)
 
-    with {:ok, quotas} <- Users.Cache.get_api_quotas(user.id, sid),
-         {:source_rate, true} <- {:source_rate, source_rate <= quotas.source},
-         {:user_rate, true} <- {:user_rate, user_rate <= quotas.user} do
-      :ok
-    else
-      {:error, reason} -> {:error, reason}
-      {:source_rate, false} -> {:error, "Source rate is over the API quota"}
-      {:user_rate, false} -> {:error, "User rate is over the API quota"}
-    end
+    # possible error values are user.id is nil and source_id is nil which
+    # should not ever happen here, raising on pattern match is acceptable
+    {:ok, quotas} = Users.Cache.get_api_quotas(user.id, sid)
+
+    source_limit = source_bucket_metrics.duration * quotas.source
+    source_remaining = source_limit - source_bucket_metrics.sum
+
+    user_limit = source_bucket_metrics.duration * quotas.user
+    user_remaining = user_limit - user_sum_of_sources
+
+    {status, message} =
+      cond do
+        source_remaining <= 0 ->
+          {:error, "Source rate is over the API quota"}
+
+        user_remaining <= 0 ->
+          {:error, "User rate is over the API quota"}
+
+        source_remaining > 0 and user_remaining > 0 ->
+          {:ok, nil}
+      end
+
+    metrics_message = %{
+      message: message,
+      metrics: %{
+        user: %{
+          remaining: user_remaining,
+          limit: user_limit
+        },
+        source: %{
+          remaining: source_remaining,
+          limit: source_limit
+        }
+      }
+    }
+
+    {status, metrics_message}
   end
 
   def get_total_user_api_rate(user) do
     user
     |> Users.Cache.list_source_ids()
-    |> Enum.map(&Sources.get_api_rate_by_id(&1, bucket: :default))
+    |> Enum.map(&Sources.get_metrics(&1, bucket: :default))
+    |> Enum.map(&Map.get(&1, :sum))
     |> Enum.sum()
   end
 end
