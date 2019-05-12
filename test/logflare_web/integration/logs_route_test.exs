189c8033785f3a5a3984021bd670f4c335eb8b30
ontofractal <ontofractal@protonmail.com>
2019-05-12 16:03:30 +0300

    setup table GenServers for LogsRouteTest test cases

--- test/logflare_web/integration/logs_route_test.exs
+++ test/logflare_web/integration/logs_route_test.exs
@@ -1,10 +1,14 @@
 defmodule LogflareWeb.LogsRouteTest do
   use LogflareWeb.ConnCase
+  alias Logflare.TableManager
   import Logflare.DummyFactory
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.mode(Logflare.Repo, {:shared, self()})
     s = insert(:source, token: Faker.UUID.v4())
+    Logflare.TableCounter.start_link()
+    Logflare.SystemCounter.start_link()
+    TableManager.start_link([s.token])
     u = insert(:user, api_key: Faker.String.base64(), sources: [s])
     {:ok, user: u, sources: [s]}
   end
