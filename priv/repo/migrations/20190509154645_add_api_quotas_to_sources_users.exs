83b21fa552ecd39848a7756705ac11f26f175633
ontofractal <ontofractal@protonmail.com>
2019-05-13 19:39:21 +0300

    update api_quotas for new and existing users/sources

--- priv/repo/migrations/20190509154645_add_api_quotas_to_sources_users.exs
+++ priv/repo/migrations/20190509154645_add_api_quotas_to_sources_users.exs
@@ -1,13 +1,26 @@
 defmodule Logflare.Repo.Migrations.AddApiQuotasToSourcesUsers do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:sources) do
-      add :api_quota, :integer, default: 0, null: false
+      add :api_quota, :integer, default: 5, null: false
     end
 
     alter table(:users) do
-      add :api_quota, :integer, default: 0, null: false
+      add :api_quota, :integer, default: 125, null: false
+    end
+
+    execute "UPDATE sources SET api_quota = 100"
+    execute "UPDATE users SET api_quota = 125"
+  end
+
+  def down do
+    alter table(:sources) do
+      remove :api_quota
+    end
+
+    alter table(:users) do
+      remove :api_quota
     end
   end
 end
