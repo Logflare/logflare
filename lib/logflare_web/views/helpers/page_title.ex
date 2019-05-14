07fece40e760acf2ff3bf760ec73c5a485525c29
chasers <chase@pagemodified.com>
2019-05-14 06:24:39 -0700

    Custom title tag for sources

---
+++ lib/logflare_web/views/helpers/page_title.ex
@@ -0,0 +1,18 @@
+defmodule LogflareWeb.Helpers.PageTitle do
+  alias LogflareWeb.{SourceView, MarketingView}
+
+  @suffix "Logflare | tail -f cloudflare"
+
+  def page_title(assigns), do: assigns |> get |> put_suffix
+
+  defp put_suffix(nil), do: @suffix
+  defp put_suffix(title), do: title <> " | " <> @suffix
+
+  # defp get(%{view_module: MarketingView}), do: "Works"
+
+  defp get(%{view_module: SourceView, view_template: "show.html", source: source}) do
+    source.name
+  end
+
+  defp get(_), do: nil
+end
--- lib/logflare_web/views/layout_view.ex
+++ lib/logflare_web/views/layout_view.ex
@@ -1,3 +1,4 @@
 defmodule LogflareWeb.LayoutView do
   use LogflareWeb, :view
+  import LogflareWeb.Helpers.PageTitle
 end
