821ab9ba3de288e5cbcbd7fc1b5c5f7e0f6ed53d
ontofractal <ontofractal@protonmail.com>
2019-05-17 21:20:44 +0300

    update logs injest cleaner to remove elements with empty containers

--- lib/logflare/logs/cleaner.ex
+++ lib/logflare/logs/cleaner.ex
@@ -1,15 +1,16 @@
 defmodule Logflare.Logs.Injest.MetadataCleaner do
+  defguard nil_or_empty(x) when x in [%{}, [], "", {}, nil]
+
   def reject_empty_kvs(xs) when is_list(xs) do
     xs
     |> Enum.reduce([], fn
+      x, acc when nil_or_empty(x) ->
+        acc
+
       x, acc when is_map(x) or is_list(x) ->
         [reject_empty_kvs(x) | acc]
 
-      nil, acc ->
-        acc
-
-      x, acc ->
-        [x | acc]
+      x, acc -> [x | acc]
     end)
     |> Enum.reverse()
   end
@@ -17,7 +18,7 @@ defmodule Logflare.Logs.Injest.MetadataCleaner do
   def reject_empty_kvs(map) when is_map(map) do
     map
     |> Enum.reduce(%{}, fn
-      {_, nil}, acc ->
+      {_, v}, acc when nil_or_empty(v) ->
         acc
 
       {k, v}, acc when is_map(v) or is_list(v) ->
