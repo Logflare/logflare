38074ec19a757b0e23f357216fb2d0cb220cfe1b
ontofractal <ontofractal@protonmail.com>
2019-05-17 21:19:10 +0300

    add a test case to the injest cleanup of empty containers in metadata

--- test/logflare/logs/injest/metadata_cleaner_test.exs
+++ test/logflare/logs/injest/metadata_cleaner_test.exs
@@ -9,14 +9,16 @@ defmodule Logflare.Logs.Injest.MetadataCleanerTest do
       meta_with_nils =
         PayloadTestUtils.standard_metadata(:cloudflare)
         |> put_in(~w(request cf country), nil)
-        |> put_in(~w(request headers host), nil)
-        |> put_in(~w(response headers vary), nil)
+        |> put_in(~w(request cf colo), "")
+        |> put_in(~w(request headers host), %{})
+        |> put_in(~w(response headers vary), [])
 
       meta_cleaned = Cleaner.reject_empty_kvs(meta_with_nils)
 
       get_keys_in = &Map.keys(get_in(meta_cleaned, &1))
 
       assert Map.keys(meta_cleaned["request"]) == ~w[cf headers method url]
+      assert "colo" not in get_keys_in.(~w(request cf))
       assert "country" not in get_keys_in.(~w(request cf))
       assert "host" not in get_keys_in.(~w(request headers))
       assert "vary" not in get_keys_in.(~w(response headers))
@@ -25,8 +27,15 @@ defmodule Logflare.Logs.Injest.MetadataCleanerTest do
     test "removes nils from standard elixir logger exception metadata" do
       meta_with_nils =
         PayloadTestUtils.standard_metadata(:elixir_logger_exception)
-        |> update_in(["stacktrace"], &Enum.map(&1, fn m -> %{m | "line" => nil} end))
-        |> update_in(["stacktrace"], &[nil | &1])
+        |> update_in(
+          ["stacktrace"],
+          fn xs ->
+            for map <- xs do
+              %{map | "line" => nil, "module" => [], "arity_or_args" => %{}, "function" => ""}
+            end
+          end
+        )
+        |> update_in(["stacktrace"], &(&1 ++ [%{}, nil, [], "", {}]))
         |> put_in(["pid"], nil)
 
       meta_cleaned = Cleaner.reject_empty_kvs(meta_with_nils)
