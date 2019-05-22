39e3fb00d17a0e2960e8fb0a6b8324665440a482
ontofractal <ontofractal@protonmail.com>
2019-05-22 21:05:16 +0300

    add unit tests to ElixirLoggerController

--- test/logflare_web/controllers/elixir_logger_controller_test.exs
+++ test/logflare_web/controllers/elixir_logger_controller_test.exs
@@ -4,61 +4,66 @@ defmodule LogflareWeb.ElixirLoggerControllerTest do
   alias Logflare.{Repo, TableCounter, SystemCounter, TableBuffer, TableManager, SourceRateCounter}
   use LogflareWeb.ConnCase
 
-  @moduletag integration: true
   @endpoint LogflareWeb.ElixirLoggerController
 
-  setup do
-    :ok = Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
-    s = insert(:source, token: Faker.UUID.v4(), rules: [])
-    u = insert(:user, sources: [s])
+  describe "ElixirLoggerController integration" do
+    @describetag integration: true
 
-    TableCounter.start_link()
-    SystemCounter.start_link()
-    TableManager.start_link([s.token])
+    setup do
+      :ok = Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
+      s = insert(:source, token: Faker.UUID.v4(), rules: [])
+      u = insert(:user, sources: [s])
 
-    Process.sleep(3_000)
+      TableCounter.start_link()
+      SystemCounter.start_link()
+      TableManager.start_link([s.token])
 
-    {:ok, users: [u], sources: [s], conn: Phoenix.ConnTest.build_conn()}
-  end
+      Process.sleep(3_000)
 
-  describe "ElixirLoggerController" do
+      {:ok, users: [u], sources: [s], conn: Phoenix.ConnTest.build_conn()}
+    end
+
     test "create action", %{conn: conn, users: [u], sources: [s]} do
+      log_event = build_log_event()
+
       conn =
         conn
         |> assign(:user, u)
         |> assign(:source, s)
-        |> post(:create, %{
-          "batch" => [
-            %{
-              "message" => "message",
-              "metadata" => %{},
-              "timestamp" => NaiveDateTime.to_iso8601(NaiveDateTime.utc_now()),
-              "level" => "info"
-            },
-            %{
-              "message" => "message",
-              "metadata" => %{},
-              "timestamp" => NaiveDateTime.to_iso8601(NaiveDateTime.utc_now()),
-              "level" => "info"
-            },
-            %{
-              "message" => "message",
-              "metadata" => %{},
-              "timestamp" => NaiveDateTime.to_iso8601(NaiveDateTime.utc_now()),
-              "level" => "info"
-            }
-          ]
-        })
+        |> post(:create, %{"batch" => [log_event, log_event, log_event]})
 
-      assert TableBuffer.get_count(s.token) == 3
-      Process.sleep(1000)
-      # sleep needed by SourceRateCounter to send itself
-      # a message to get and calculate metrics
-      %{average: avg, duration: duration, sum: sum} = SourceRateCounter.get_metrics(s.token)
-      assert avg in [1, 2]
-      assert duration == 60
-      assert sum == 3
       assert json_response(conn, 200) == %{"message" => "Logged!"}
     end
+  end
+
+  describe "ElixirLoggerController" do
+    setup do
+      :ok = Ecto.Adapters.SQL.Sandbox.mode(Repo, {:shared, self()})
+      s = insert(:source, token: Faker.UUID.v4(), rules: [])
+      u = insert(:user, sources: [s])
+
+      {:ok, users: [u], sources: [s], conn: Phoenix.ConnTest.build_conn()}
+    end
+
+    test "create action", %{conn: conn, users: [u], sources: [s]} do
+      log_event = build_log_event()
+
+      conn =
+        conn
+        |> assign(:user, u)
+        |> assign(:source, s)
+        |> post(:create, %{"batch" => [log_event, log_event, log_event]})
+
+      assert json_response(conn, 200) == %{"message" => "Logged!"}
+    end
+  end
+
+  def build_log_event() do
+      %{
+        "message" => "log message",
+        "metadata" => %{},
+        "timestamp" => NaiveDateTime.to_iso8601(NaiveDateTime.utc_now()),
+        "level" => "info"
+      }
   end
 end
