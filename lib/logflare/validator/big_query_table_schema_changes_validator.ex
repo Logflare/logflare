e1431822e7dc704ae8fa80f75b9807260e61a1a0
ontofractal <ontofractal@protonmail.com>
2019-05-23 20:25:27 +0300

    add log metadata to typemap fn

--- lib/logflare/validator/big_query_table_schema_changes_validator.ex
+++ lib/logflare/validator/big_query_table_schema_changes_validator.ex
@@ -27,10 +27,49 @@ defmodule Logflare.Validator.BigQuery.SchemaChange do
     |> Enum.into(Map.new())
   end
 
+  def to_typemap(metadata) do
+    for {k, v} <- metadata, into: Map.new() do
+      v =
+        cond do
+          match?(%DateTime{}, v) or
+              match?(%NaiveDateTime{}, v) ->
+            %{t: :datetime}
+
+          is_list(v) ->
+            %{
+              t: :map,
+              fields: Enum.reduce(v, %{}, &Map.merge(&2, to_typemap(&1)))
+            }
+
+          is_map(v) ->
+            %{t: :map, fields: to_typemap(v)}
+
+          true ->
+            %{t: type_of(v)}
+        end
+
+      {String.to_existing_atom(k), v}
+    end
+  end
+
   def bq_type_to_ex("TIMESTAMP"), do: :datetime
   def bq_type_to_ex("RECORD"), do: :map
   def bq_type_to_ex("INTEGER"), do: :integer
   def bq_type_to_ex("STRING"), do: :string
   def bq_type_to_ex("BOOLEAN"), do: :boolean
   def bq_type_to_ex("ARRAY"), do: :list
+
+  defp type_of(arg) when is_binary(arg), do: :string
+  defp type_of(arg) when is_map(arg), do: :map
+  defp type_of(arg) when is_list(arg), do: :list
+  defp type_of(arg) when is_bitstring(arg), do: :bitstring
+  defp type_of(arg) when is_float(arg), do: :float
+  defp type_of(arg) when is_function(arg), do: :function
+  defp type_of(arg) when is_integer(arg), do: :integer
+  defp type_of(arg) when is_pid(arg), do: :pid
+  defp type_of(arg) when is_port(arg), do: :port
+  defp type_of(arg) when is_reference(arg), do: :reference
+  defp type_of(arg) when is_tuple(arg), do: :tuple
+  defp type_of(arg) when arg in [true, false], do: :boolean
+  defp type_of(arg) when is_atom(arg), do: :atom
 end
