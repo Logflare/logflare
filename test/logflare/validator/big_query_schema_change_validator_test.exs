d194a2621b0856f15cd3f46051547cf104615013
ontofractal <ontofractal@protonmail.com>
2019-05-23 23:01:31 +0300

    add more success test cases for BigQuery schema change validator

--- test/logflare/validator/big_query_schema_change_validator_test.exs
+++ test/logflare/validator/big_query_schema_change_validator_test.exs
@@ -5,73 +5,59 @@ defmodule Logflare.Validator.BigQuery.SchemaChangeTest do
   import Logflare.Validator.BigQuery.SchemaChange
 
   describe "bigquery bigquery schema change validation" do
+    test "valid?/1 returns true for valid params" do
+    end
+
     test "correctly creates a typemap from schema" do
-      schema = build(:table, :third)
-      typemap = to_typemap(schema)
-
-      assert typemap === %{
-               timestamp: %{t: :datetime},
-               event_message: %{t: :string},
-               metadata: %{
-                 t: :map,
-                 fields: %{
-                   datacenter: %{t: :string},
-                   ip_address: %{t: :string},
-                   request_method: %{t: :string},
-                   user: %{
-                     t: :map,
-                     fields: %{
-                       browser: %{t: :string},
-                       id: %{t: :integer},
-                       vip: %{t: :boolean},
-                       company: %{t: :string},
-                       login_count: %{t: :integer},
-                       address: %{
-                         t: :map,
-                         fields: %{
-                           street: %{t: :string},
-                             city: %{t: :string},
-                             st: %{t: :string}
-
-                         }
-                       }
-                     }
-                   }
-                 }
-               }
-             }
+        assert :schema
+               |> build(:third)
+               |> to_typemap(from: :bigquery_schema) == typemap_for_third()
     end
 
     test "correctly builds a typemap from metadata" do
-      schema = build(:table, :third)
+        assert :metadata
+               |> build(:third)
+               |> to_typemap(from: :bigquery_schema) == typemap_for_third()
+    end
 
-      metadata = %{
-        "event_message" => "This is an example.",
-        "metadata" => [
-          %{
-            "ip_address" => "100.100.100.100",
-            "datacenter" => "aws",
-            "request_method" => "POST",
-            "user" => %{
-              "address" => %{
-                "city" => "New York",
-                "st" => "NY",
-                "street" => "123 W Main St"
-              },
-              "browser" => "Firefox",
-              "company" => "Apple",
-              "id" => 38,
-              "login_count" => 154,
-              "vip" => true
+    test "valid? returns true for correct metadata and schema" do
+      schema = build(:schema, :third)
+      metadata = build(:metadata, :third)
+
+      assert valid?(metadata, schema)
+    end
+  end
+
+  def typemap_for_third() do
+    %{
+      timestamp: %{t: :datetime},
+      event_message: %{t: :string},
+      metadata: %{
+        t: :map,
+        fields: %{
+          datacenter: %{t: :string},
+          ip_address: %{t: :string},
+          request_method: %{t: :string},
+          user: %{
+            t: :map,
+            fields: %{
+              browser: %{t: :string},
+              id: %{t: :integer},
+              vip: %{t: :boolean},
+              company: %{t: :string},
+              login_count: %{t: :integer},
+              address: %{
+                t: :map,
+                fields: %{
+                  street: %{t: :string},
+                  city: %{t: :string},
+                  st: %{t: :string}
+                }
+              }
             }
           }
-        ],
-        "timestamp" => ~N[2019-04-12 16:44:38]
+        }
       }
-
-      typemap = to_typemap(metadata)
-
-      assert typemap == build(:table, :third) |> to_typemap()
-    end
+    }
   end
 end
