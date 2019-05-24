e98196efa277c640fc06f92ada49a61b98cfb117
ontofractal <ontofractal@protonmail.com>
2019-05-24 21:32:46 +0300

    add get_by_user and get_by_source fns to Logs.RejectedEvents

--- lib/logflare/logs/rejected_logs.ex
+++ lib/logflare/logs/rejected_logs.ex
@@ -6,4 +6,18 @@ defmodule Logflare.Logs.RejectedEvents do
   def child_spec(_) do
     %{id: @cache, start: {Cachex, :start_link, [@cache, []]}}
   end
+
+  def get_by_user(%User{} = user) do
+    get!(user.token)
+  end
+
+  def get_by_source(%Source{} = source) do
+    source.user
+    |> get_by_user()
+    |> Enum.find(fn {k, v} -> k === source.token end)
+  end
+
+  defp get!(key) do
+    Cachex.get!(@cache, key)
+  end
 end
