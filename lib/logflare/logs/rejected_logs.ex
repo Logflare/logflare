3d5185a5f1bbcd83a69f1cac37806f490f74903e
ontofractal <ontofractal@protonmail.com>
2019-05-27 15:45:44 +0300

    improve typespecs for RejectedLogs

--- lib/logflare/logs/rejected_logs.ex
+++ lib/logflare/logs/rejected_logs.ex
@@ -3,18 +3,22 @@ defmodule Logflare.Logs.RejectedEvents do
   @cache __MODULE__
   import Cachex.Spec
 
+  @type rejected_log_event :: %{
+          message: String.t(),
+          payload: list(map) | map
+        }
   def child_spec(_) do
     %{id: @cache, start: {Cachex, :start_link, [@cache, []]}}
   end
 
-  @spec get_by_user(Logflare.User.t()) :: map
+  @spec get_by_user(Logflare.User.t()) :: %{atom => list(rejected_log_event)}
   def get_by_user(%User{sources: sources}) do
     for source <- sources, into: Map.new() do
       {source.token, get_by_source(source)}
     end
   end
 
-  @spec get_by_source(Logflare.Source.t()) :: map
+  @spec get_by_source(Logflare.Source.t()) :: list(rejected_log_event)
   def get_by_source(%Source{token: token}) do
     get!(token)
   end
@@ -22,6 +26,7 @@ defmodule Logflare.Logs.RejectedEvents do
   @doc """
   Expected to be called only in a log event params validation plug
   """
+  @spec injest(%{error: atom, batch: list(map), source: Source.t()}) :: term
   def injest(%{error: error, batch: batch, source: %Source{token: token}}) do
     log = %{
       message: error.message(),
@@ -37,7 +42,7 @@ defmodule Logflare.Logs.RejectedEvents do
   end
 
   @spec insert(atom, map) :: list(map)
-  def insert(token, log) do
+  defp insert(token, log) do
     Cachex.get_and_update!(@cache, token, fn
       xs when is_list(xs) -> Enum.take([log | xs], 500)
       _ -> [log]
