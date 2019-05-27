9cc504b8933d5bea37a4f28cc9a13b6cc05df1f7
ontofractal <ontofractal@protonmail.com>
2019-05-27 18:12:25 +0300

    add test module for AdminController, a test case for unauthorized user

---
+++ test/logflare_web/controllers/admin_controller_test.exs
@@ -0,0 +1,42 @@
+defmodule LogflareWeb.AdminControllerTest do
+  @moduledoc false
+  import Logflare.DummyFactory
+  alias Logflare.{Repo, Source}
+  alias Logflare.{TableCounter, SystemCounter, TableManager}
+  use LogflareWeb.ConnCase
+
+  describe "Admin controller" do
+    setup do
+      s1u1 = insert(:source, token: Faker.UUID.v4(), rules: [])
+      u1 = insert(:user, sources: [s1u1])
+      s1u2 = insert(:source, token: Faker.UUID.v4(), rules: [])
+      u2 = insert(:user, sources: [s1u2])
+
+      sources = [s1u1, s1u2]
+      users = [u1, u2]
+
+      TableCounter.start_link()
+      SystemCounter.start_link()
+      TableManager.start_link([s1u1.token])
+      TableManager.start_link([s1u2.token])
+
+      Process.sleep(1_000)
+
+      {:ok, users: users, sources: sources}
+    end
+
+    test "halts and returns 401 for non-admin user", %{
+      conn: conn,
+      users: [u1 | _],
+      sources: sources
+    } do
+      conn =
+        conn
+        |> assign(:user, u1)
+        |> get("/admin/dashboard")
+
+      assert conn.halted == true
+      assert conn.status == 401
+    end
+  end
+end
