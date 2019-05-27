a459e0ce369879b5d20b17321b0599bf82d5248d
ontofractal <ontofractal@protonmail.com>
2019-05-27 18:30:16 +0300

    DRY out AdminController, use source.metrics for rate,max_rate,latest, etc

--- lib/logflare_web/controllers/admin_controller.ex
+++ lib/logflare_web/controllers/admin_controller.ex
@@ -2,9 +2,7 @@ defmodule LogflareWeb.AdminController do
   use LogflareWeb, :controller
   import Ecto.Query, only: [from: 2]
 
-  alias Logflare.Repo
-  alias Logflare.SourceData
-  alias Number.Delimit
+  alias Logflare.{Repo, Source}
 
   def dashboard(conn, _params) do
     query =
@@ -16,28 +14,11 @@ defmodule LogflareWeb.AdminController do
           token: s.token
         }
 
-    sources =
-      for source <- Repo.all(query) do
-        {:ok, token} = Ecto.UUID.Atom.load(source.token)
-
-        rate = Delimit.number_to_delimited(SourceData.get_rate(source))
-        timestamp = SourceData.get_latest_date(source)
-        average_rate = Delimit.number_to_delimited(SourceData.get_avg_rate(source))
-        max_rate = Delimit.number_to_delimited(SourceData.get_max_rate(source))
-        buffer_count = Delimit.number_to_delimited(SourceData.get_buffer(token))
-        event_inserts = Delimit.number_to_delimited(SourceData.get_total_inserts(token))
-
-        source
-        |> Map.put(:rate, rate)
-        |> Map.put(:token, token)
-        |> Map.put(:latest, timestamp)
-        |> Map.put(:avg, average_rate)
-        |> Map.put(:max, max_rate)
-        |> Map.put(:buffer, buffer_count)
-        |> Map.put(:inserts, event_inserts)
-      end
-
-    sorted_sources = Enum.sort_by(sources, &Map.fetch(&1, :latest), &>=/2)
+    sorted_sources =
+      query
+      |> Repo.all()
+      |> Enum.map(&Source.update_metrics_latest/1)
+      |> Enum.sort_by(&Map.fetch(&1, :latest), &>=/2)
 
     render(conn, "dashboard.html", sources: sorted_sources)
   end
