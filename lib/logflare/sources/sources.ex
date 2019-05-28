80af876bd0986771c92c7cc654e5657e4ce38853
ontofractal <ontofractal@protonmail.com>
2019-05-28 22:03:33 +0300

    add get_by_public_token and get_by_pk fns to Sources context

--- lib/logflare/sources/sources.ex
+++ lib/logflare/sources/sources.ex
@@ -6,6 +6,8 @@ defmodule Logflare.Sources do
     SRC.get_metrics(sid, :default)
   end
 
+  @spec get_by_name(binary()) ::
+          nil | [%{optional(atom()) => any()}] | %{optional(atom()) => any()}
   def get_by_name(source_name) when is_binary(source_name) do
     Source
     |> Repo.get_by(name: source_name)
@@ -20,6 +22,30 @@ defmodule Logflare.Sources do
   def get_by_id(source_id) when is_atom(source_id) do
     Source
     |> Repo.get_by(token: source_id)
+    |> Repo.preload(:user)
+    |> Repo.preload(:rules)
+  end
+
+  def get_by_public_token(public_token) when is_binary(public_token) do
+    Source
+    |> Repo.get_by(public_token: public_token)
+    |> preload_defaults()
+  end
+
+  def get_by_pk(pk) when is_binary(pk) do
+    pk
+    |> String.to_integer()
+    |> get_by_pk()
+  end
+
+  def get_by_pk(pk) when is_integer(pk) do
+    Source
+    |> Repo.get(pk)
+    |> preload_defaults()
+  end
+
+  def preload_defaults(source) do
+    source
     |> Repo.preload(:user)
     |> Repo.preload(:rules)
   end
