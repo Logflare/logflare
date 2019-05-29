6c5152536fc7fa2cf5136606e2bc328d1eb7cc54
ontofractal <ontofractal@protonmail.com>
2019-05-29 20:54:55 +0300

    add a test case for UserController successful update action

--- test/logflare_web/controllers/user_controller_test.exs
+++ test/logflare_web/controllers/user_controller_test.exs
@@ -42,5 +42,35 @@ defmodule LogflareWeb.UserControllerTest do
       refute s1_new.id == nope_user_id
       assert html_response(conn, 401) =~ "Not allowed"
     end
+
+    test "of allowed fields succeeds", %{
+      conn: conn,
+      users: [u1 | _]
+    } do
+      new = %{
+        email: Faker.Internet.free_email(),
+        provider: Faker.String.base64(),
+        email_preferred: Faker.Internet.free_email(),
+        name: Faker.String.base64(),
+        image: Faker.Internet.image_url(),
+        email_me_product: true,
+        phone: Faker.Phone.EnUs.phone()
+        # bigquery_project_id: Faker.String.base64()
+      }
+
+      conn =
+        conn
+        |> assign(:user, u1)
+        |> put("/account/edit", %{"user" => new})
+
+      s1_new =
+        Users.get_by_id(u1.id)
+        |> Map.from_struct()
+        |> Map.take(Map.keys(new))
+
+      refute conn.assigns[:changeset]
+      assert s1_new == new
+      assert html_response(conn, 302) =~ user_path(conn, :edit)
+    end
   end
 end
