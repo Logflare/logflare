1859c03b3a8d5f1b540c9509a3326d5bb88b4a0a
ontofractal <ontofractal@protonmail.com>
2019-05-31 19:28:24 +0300

    improve AdminController test module

--- test/logflare_web/controllers/admin_controller_test.exs
+++ test/logflare_web/controllers/admin_controller_test.exs
@@ -1,15 +1,13 @@
 defmodule LogflareWeb.AdminControllerTest do
   @moduledoc false
   import Logflare.DummyFactory
-  alias Logflare.{Repo, Source}
-  alias Logflare.{SourceCounter, SystemCounter, SourceManager}
   use LogflareWeb.ConnCase
 
   describe "Admin controller" do
     setup do
-      s1u1 = insert(:source, token: Faker.UUID.v4(), rules: [])
+      s1u1 = insert(:source, rules: [])
       u1 = insert(:user, sources: [s1u1])
-      s1u2 = insert(:source, token: Faker.UUID.v4(), rules: [])
+      s1u2 = insert(:source, rules: [])
       u2 = insert(:user, sources: [s1u2])
 
       a1 = insert(:user, admin: true)
@@ -17,20 +15,13 @@ defmodule LogflareWeb.AdminControllerTest do
       sources = [s1u1, s1u2]
       users = [u1, u2]
 
-      SourceCounter.start_link()
-      SystemCounter.start_link()
-      SourceManager.start_link([s1u1.token])
-      SourceManager.start_link([s1u2.token])
-
-      Process.sleep(1_000)
-
       {:ok, users: users, sources: sources, admins: [a1]}
     end
 
     test "halts and returns 401 for non-admin user", %{
       conn: conn,
       users: [u1 | _],
-      sources: sources
+      sources: _sources
     } do
       conn =
         conn
@@ -38,13 +29,13 @@ defmodule LogflareWeb.AdminControllerTest do
         |> get("/admin/dashboard")
 
       assert conn.halted == true
-      assert conn.status == 401
+      assert conn.status == 403
     end
 
     test "renders dashboard for admin", %{
       conn: conn,
       admins: [a1],
-      sources: sources
+      sources: _sources
     } do
       conn =
         conn
