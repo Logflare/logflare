e276397caa6126513af9e075c75ceb43d2a150ee
ontofractal <ontofractal@protonmail.com>
2019-06-03 19:37:01 +0300

    Users module cleanup

--- lib/logflare/users/users.ex
+++ lib/logflare/users/users.ex
@@ -7,32 +7,11 @@ defmodule Logflare.Users do
   def get_by(keyword) do
     User
     |> Repo.get_by(keyword)
-    |> Repo.preload(:sources)
+    |> default_preloads()
   end
 
-  def user_owns_token?(token_param) when is_binary(token_param) do
-
-  end
-
-  def list_source_ids(user) when is_integer(user) do
-    get_by(id: user.id)
-    |> list_source_ids()
-  end
-
-  def list_source_ids(user) do
+  def default_preloads(user) do
     user
-    |> Map.get(:sources)
-    |> Enum.map(& &1.token)
-  end
-
-  def get_sources(%User{id: user_id}) do
-    from(s in Source,
-      where: s.user_id == ^user_id,
-      order_by: [desc: s.favorite],
-      # TODO: maybe order by latest events?
-      order_by: s.name
-    )
-    |> Repo.all()
-    |> Repo.preload(:user)
+    |> Repo.preload(:sources)
   end
 end
