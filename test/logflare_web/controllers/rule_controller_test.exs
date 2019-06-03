414188b08eef5033b8e2eab85d4a92bea40a4b51
ontofractal <ontofractal@protonmail.com>
2019-06-03 15:52:41 +0300

    add a test module for RuleController and a test case for create action

---
+++ test/logflare_web/controllers/rule_controller_test.exs
@@ -0,0 +1,50 @@
+defmodule LogflareWeb.RuleControllerTest do
+  @moduledoc false
+  use LogflareWeb.ConnCase
+
+  alias Logflare.{SystemCounter, Sources, Repo}
+  import Logflare.DummyFactory
+
+  setup do
+    s1 = insert(:source, public_token: Faker.String.base64(16))
+    s2 = insert(:source)
+    s3 = insert(:source)
+    u1 = insert(:user, sources: [s1, s2])
+    u2 = insert(:user, sources: [s3])
+
+    users = Repo.preload([u1, u2], :sources)
+
+    sources = [s1, s2, s3]
+
+    {:ok, users: users, sources: sources}
+  end
+
+  describe "RuleController create" do
+    test "succeeds for authorized user", %{
+      conn: conn,
+      users: [u1, u2],
+      sources: [u1s1, u1s2, u2s1 | _]
+    } do
+      conn =
+        conn
+        |> assign(:user, u1)
+        |> assign(:source, u1s1)
+        |> post(
+          source_rule_path(conn, :create, u1s1.id),
+          %{
+            "rule" => %{
+              regex: "\| 4.. \| ",
+              sink: u1s2.token
+            }
+          }
+        )
+
+      rules_db = Sources.get_by(id: u1s1.id).rules
+
+      assert length(rules_db) == 1
+      assert hd(rules_db) == %{regex: "\| 4.. \| "}
+      assert get_flash(conn, :info) === "Rule created successfully!"
+      assert redirected_to(source_rule_path(conn, :index, u1s1.id), 302)
+    end
+  end
+end
