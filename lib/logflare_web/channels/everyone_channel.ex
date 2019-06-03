c5f0f2ed8a8eb3173296baa20a779de28c1da7ec
ontofractal <ontofractal@protonmail.com>
2019-06-03 19:37:29 +0300

    minor channel updates

--- lib/logflare_web/channels/everyone_channel.ex
+++ lib/logflare_web/channels/everyone_channel.ex
@@ -2,14 +2,6 @@ defmodule LogflareWeb.EveryoneChannel do
   use LogflareWeb, :channel
 
   def join("everyone", _payload, socket) do
-    if authorized?() do
-      {:ok, socket}
-    else
-      {:error, %{reason: "Not authorized!"}}
-    end
-  end
-
-  defp authorized?() do
-    true
+    {:ok, socket}
   end
 end
