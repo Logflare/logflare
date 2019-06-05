6373fe1dd78f0612babc979ee89707ea0492440f
ontofractal <ontofractal@protonmail.com>
2019-06-05 13:54:29 +0300

    extract source tempalte utc/local datetime functions to dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -6,6 +6,8 @@ class Dashboard {
     constructor({apiKey}) {
         this.apiKey = apiKey
         this.initApiClipboard()
+        this.dateAdd()
+        this.dateSwap()
     }
 
     initApiClipboard = () => {
@@ -30,6 +32,64 @@ class Dashboard {
     toggleApiKey = (bool) => {
         return bool ? `CLICK ME` : this.apiKey
     }
+
+    dateSwap = () => {
+        this.localTime = !this.localTime
+        document.getElementById("swap-date").innerHTML = this.timeToggle(this.localTime)
+
+        var utcs = document.getElementsByClassName("utc")
+
+        for (var i = 0; i < utcs.length; i++) {
+            utcs[i].classList.toggle("d-none")
+        }
+
+        var local_times = document.getElementsByClassName("local-time")
+
+        for (var i = 0; i < local_times.length; i++) {
+            local_times[i].classList.toggle("d-none")
+        }
+    }
+
+    timeToggle = (bool) => {
+        switch (bool) {
+            case true:
+                return `<span id="swap-date"><i class="fa fa-toggle-on pointer-cursor" aria-hidden="true"></i></span>`
+                break
+            case false:
+                return `<span id="swap-date"><i class="fa fa-toggle-off pointer-cursor" aria-hidden="true"></i></span>`
+        }
+    }
+
+
+    dateAdd = () => {
+        switch (this.localTime) {
+            case true:
+                var timestamps = document.getElementsByClassName("log-datestamp")
+
+                for (var i = 0; i < timestamps.length; i++) {
+                    let time = timestamps[i]
+                    let local_time = formatLocalTime(time.innerHTML / 1000)
+                    let utc_time = (date = new Date()) => {
+                        return new Date((time.innerHTML / 1000) + date.getTimezoneOffset() * 60 * 1000)
+                    }
+                    timestamps[i].innerHTML = `<span class="local-time">${local_time}</span><span class="utc d-none">${formatLocalTime(utc_time())} UTC</span>`
+                }
+                break
+            case false:
+                var timestamps = document.getElementsByClassName("log-datestamp")
+
+                for (var i = 0; i < timestamps.length; i++) {
+                    let time = timestamps[i]
+                    let local_time = formatLocalTime(time.innerHTML / 1000)
+                    let utc_time = (date = new Date()) => {
+                        return new Date((time.innerHTML / 1000) + date.getTimezoneOffset() * 60 * 1000)
+                    }
+                    timestamps[i].innerHTML = `<span class="local-time d-none">${local_time}</span><span class="utc">${formatLocalTime(utc_time())} UTC</span>`
+                }
+        }
+    }
+
+
 }
 
 export default Dashboard
