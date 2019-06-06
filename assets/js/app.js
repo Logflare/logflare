b6a39651a1ddf89f3fd963370a2a38e8450fc980
ontofractal <ontofractal@protonmail.com>
2019-06-06 22:32:03 +0300

    update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,15 @@
-import css from '../css/app.scss';
-import Dashboard from "./dashboard"
-
+import css from "../css/app.scss"
+import "phoenix"
 import "phoenix_html"
-window.Dashboard = Dashboard
-
 import socket from "./socket"
+import "bootstrap"
+import "@babel/polyfill"
+import * as Dashboard from "./dashboard"
+import * as Source from "./source"
+import * as Logs from "./logs"
+import * as formatters from "./formatters"
 
-import 'bootstrap'
-
-global.$ = global.jQuery = $;
-
-global.dateFns = dateFns;
+window.Dashboard = Dashboard
+window.Logs = Logs
+window.Source = Source
+window.$ = $
