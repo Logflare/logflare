40dc670cfeffbc59deeb2ecf73a98b3441c98dd4
ontofractal <ontofractal@protonmail.com>
2019-06-10 18:29:22 +0300

    wire phoenix LiveView

--- mix.exs
+++ mix.exs
@@ -86,7 +86,8 @@ defmodule Logflare.Mixfile do
       {:bertex, ">= 0.0.0"},
       {:excoveralls, "~> 0.11", only: :test},
       {:placebo, "~> 1.2"},
-      {:logflare_logger_backend, git: "git://github.com/Logflare/logflare_logger_backend.git"}
+      {:logflare_logger_backend, git: "git://github.com/Logflare/logflare_logger_backend.git"},
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"}
     ]
   end
 
