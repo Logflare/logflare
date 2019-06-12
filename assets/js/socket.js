80fb09a535f69b9d6189aa990f8d66e18e1d21ec
ontofractal <ontofractal@protonmail.com>
2019-06-12 22:35:14 +0300

    delete createEveryoneSocket from socket.js

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -5,23 +5,4 @@ let socket = new Socket("/socket", { params: { token: window.userToken, public_t
 
 socket.connect()
 
-function createEveryoneSocket() {
-  let channel = socket.channel(`everyone`, {})
-  channel.join()
-    .receive("ok", resp => {
-      console.log("Everyone socket joined successfully", resp)
-    })
-    .receive("error", resp => {
-      console.log("Unable to join", resp)
-    })
-
-  channel.on(`everyone:update`, swapCount)
-}
-
-function swapCount(event) {
-  $("#total-logged").html(`<span class="flash-text">${event.total_logs_logged}</span>`)
-}
-
-window.createEveryoneSocket = createEveryoneSocket
-
 export default socket
