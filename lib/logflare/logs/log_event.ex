a67612d6b534564ef4b977ece3ec0e2667302c1b
ontofractal <ontofractal@protonmail.com>
2019-06-13 22:23:45 +0300

    update LogEvent to use embedded Ecto schemas

--- lib/logflare/logs/log_event.ex
+++ lib/logflare/logs/log_event.ex
@@ -1,9 +1,68 @@
-defmodule Logflare.Logs.LogEvent do
-  use TypedStruct
+defmodule Logflare.LogEvent do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Logflare.Logs.Injest.MetadataCleaner
 
-  typedstruct do
-    field :body, map, enforce: true
-    field :valid, boolean
-    field :validation_error, String.t()
+  defmodule Body do
+    @moduledoc false
+    use Ecto.Schema
+
+    @primary_key false
+    embedded_schema do
+      field :metadata, :map, default: %{}
+      field :message, :string
+      field :timestamp, :integer
+    end
   end
+
+  @primary_key false
+  embedded_schema do
+    embeds_one :body, Body
+    field :source, :map
+    field :valid?, :boolean
+    field :validation_error, {:array, :string}
+  end
+
+  def mapper(params) do
+    message = params["log_entry"] || params["message"]
+    metadata = params["metadata"]
+    timestamp = params["timestamp"] || System.system_time(:microsecond)
+
+    %{
+      "body" => %{
+        "message" => message,
+        "metadata" => metadata,
+        "timestamp" => timestamp
+      }
+    }
+    |> MetadataCleaner.deep_reject_nil_and_empty()
+  end
+
+  def make(params, %{source: source}) do
+    changeset =
+      %__MODULE__{}
+      |> cast(mapper(params), [:source, :valid?, :validation_error])
+      |> cast_embed(:body, with: &make_body/2)
+      |> validate_required([:body])
+
+    body = struct!(Body, changeset.changes.body.changes)
+
+    struct!(__MODULE__, changeset.changes)
+    |> Map.put(:body, body)
+    |> Map.put(:validation_error, changeset_error_to_string(changeset))
+    |> Map.put(:source, source)
+    |> Map.put(:valid?, changeset.valid?)
+  end
+
+  def make_body(_struct, params) do
+    %__MODULE__.Body{}
+    |> cast(params, [
+      :metadata,
+      :message,
+      :timestamp
+    ])
+    |> validate_required([:message, :timestamp])
+    |> validate_length(:message, min: 1)
+  end
+
 end
