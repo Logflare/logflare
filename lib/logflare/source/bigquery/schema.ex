6f4a33f58aa8ca796969afeb8bdae23aa548db1b
ontofractal <ontofractal@protonmail.com>
2019-06-14 20:02:01 +0300

    minor bugfixes

--- lib/logflare/source/bigquery/schema.ex
+++ lib/logflare/source/bigquery/schema.ex
@@ -45,7 +45,7 @@ defmodule Logflare.Source.BigQuery.Schema do
       {:ok, table} ->
         schema = SchemaBuilder.deep_sort_by_fields_name(table.schema)
         Logger.info("Table schema manager started: #{state.source_token}")
-        Sources.Cache.put_bq_schema(source_token, schema)
+        Sources.Cache.put_bq_schema(state.source_token, schema)
         {:ok, %{state | schema: schema, schema_not_sorted: table.schema}}
 
       _ ->
