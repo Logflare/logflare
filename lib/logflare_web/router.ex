fe38f383586f276f76bd11afc13950e99ded1ef2
chasers <chase@pagemodified.com>
2019-06-14 14:41:10 -0700

    Init new_index

--- lib/logflare_web/router.ex
+++ lib/logflare_web/router.ex
@@ -54,6 +54,7 @@ defmodule LogflareWeb.Router do
   scope "/", LogflareWeb do
     pipe_through :browser
     get "/", MarketingController, :index
+    get "/new-index", MarketingController, :new_index
     get "/bigquery-datastudio", MarketingController, :big_query
   end
 
