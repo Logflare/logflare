5f96e131d0b1ad2809169a336102eb9604e5f298
chasers <chase@pagemodified.com>
2019-06-17 14:26:13 -0700

    Per second logs on homepage

--- lib/logflare_web/live/global_log_metrics_lv.ex
+++ lib/logflare_web/live/global_log_metrics_lv.ex
@@ -6,7 +6,9 @@ defmodule LogflareWeb.GlobalLogMetricsLV do
 
   def render(assigns) do
     ~L"""
-      <span> <%= @log_count %> </span>
+    <h3>That's <span><%= @log_count %></span> records logged to date</h3>
+    <h3>Counting <span><%= @per_second %></span> logs per second</h3>
+
     """
   end
 
@@ -22,8 +24,10 @@ defmodule LogflareWeb.GlobalLogMetricsLV do
 
   defp put_data(socket) do
     {:ok, log_count} = AllLogsLogged.log_count(:total_logs_logged)
+    total_logs_per_second = AllLogsLogged.Poller.get_total_logs_per_second()
     log_count = number_to_delimited(log_count)
+    total_logs_per_second = number_to_delimited(total_logs_per_second)
 
-    assign(socket, log_count: log_count)
+    assign(socket, log_count: log_count, per_second: total_logs_per_second)
   end
 end
