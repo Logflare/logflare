3a20a5f07489258e9a5e3ee4dbc12a8b1c9c7693
ontofractal <ontofractal@protonmail.com>
2019-06-18 21:33:35 +0300

    add Source.EmailNotificationServer test module

---
+++ test/logflare/source/email_notification_server_test.exs
@@ -0,0 +1,28 @@
+defmodule Logflare.Source.EmailNotificationServerTest do
+  @moduledoc false
+  use Logflare.DataCase
+  alias Logflare.Source.EmailNotificationServer
+  alias Logflare.Source.RecentLogsServer, as: RLS
+  alias Logflare.LogEvent, as: LE
+  import Logflare.DummyFactory
+
+  setup do
+    u1 = insert(:user)
+    s1 = insert(:source, user_id: u1.id)
+    sid = s1.token
+    rls = %RLS{source_id: sid}
+
+    {:ok, sources: [s1], args: rls}
+  end
+
+  describe "EmailNotificationServer" do
+    test "start_link/1", %{sources: [s1 | _], args: rls} do
+      assert {:ok, _pid} = EmailNotificationServer.start_link(rls)
+    end
+
+    test "init/1", %{args: rls} do
+      EmailNotificationServer.init(rls)
+      assert_receive :check_rate, 1_100
+    end
+  end
+end
