369af200709764e283dc132ad14a394619dd98ed
chasers <chase@pagemodified.com>
2019-06-21 20:00:42 -0700

    Dont mess with observer metrics response

--- lib/logflare/system_metrics/observer/observer.ex
+++ lib/logflare/system_metrics/observer/observer.ex
@@ -2,14 +2,6 @@ defmodule Logflare.SystemMetrics.Observer do
   def get_metrics() do
     :observer_backend.sys_info()
     |> Keyword.drop([:alloc_info])
-    |> Enum.map(fn {x, y} ->
-      if is_list(y) do
-        {x, to_string(y)}
-      else
-        {x, y}
-      end
-    end)
-    |> Enum.into(%{})
   end
 
   def get_memory() do
