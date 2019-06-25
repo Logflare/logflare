009b6e3ed96873d154a9666bb1ac7e27f73cd6c6
ontofractal <ontofractal@protonmail.com>
2019-06-26 00:20:48 +0300

    add first version of search LiveView

---
+++ lib/logflare_web/live/sources/search_live.ex
@@ -0,0 +1,53 @@
+defmodule LogflareWeb.Source.SearchLV do
+  @moduledoc false
+  alias Logflare.Google.BigQuery.{GenUtils, Query}
+  alias Logflare.{Source, Logs, LogEvent}
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~L"""
+    <form class="form-group" phx-submit="search">
+      <input class="form-control" type="text" name="q" value="<%= @query %>" placeholder="Enter regex to search for matching log messages..."
+             <%= if @loading, do: "readonly" %>/>
+    </form>
+    <button class="btn btn-primary form-button" type="submit"> Search </button>
+    <%= if @log_events do %>
+    <ul id="logs-list" class="list-unstyled console-text-list">
+      <%= @log_events |> Enum.with_index |> Enum.map(fn {log, inx} -> %>
+        <li>
+          <mark class="log-datestamp" data-timestamp="<%= log.body.timestamp %>"><%= Timex.from_unix(log.body.timestamp, :microsecond) |> Timex.to_naive_datetime() %></mark>
+          <%= log.body.message %>
+          <%= if map_size(log.body.metadata) > 0 do %>
+          <a class="metadata-link" data-toggle="collapse" href="#metadata-<%= inx %>"aria-expanded="false">
+            metadata
+          </a>
+          <div class="collapse metadata" id="metadata-<%= inx %>">
+            <pre class="pre-metadata"><code><%= Jason.encode!(log.body.metadata, pretty: true) %></code></pre>
+          </div>
+          <% end %>
+        </li>
+      <% end) %>
+    </ul>
+    <% end %>
+    """
+  end
+
+  def mount(%{source: source} = session, socket) do
+    {:ok, assign(socket, query: nil, loading: false, log_events: [], source: source)}
+  end
+
+  def handle_event("search", %{"q" => query}, socket) when byte_size(query) <= 100 do
+    send(self(), {:search, query})
+    {:noreply, assign(socket, query: query, result: "Searching...", loading: true)}
+  end
+
+  def handle_info({:search, query}, socket) do
+    %Source{} = source = socket.assigns.source
+    {:ok, %{result: log_events}} = Logs.Search.utc_today(%{regex: query, source: source})
+    log_events = if log_events do
+     Enum.map(log_events, &LogEvent.make(&1, %{source: source}))
+    end
+
+    {:noreply, assign(socket, loading: false, log_events: log_events)}
+  end
+end
