37178f3a024a074b9924814c0894eea42dab07d8
ontofractal <ontofractal@protonmail.com>
2019-06-26 22:14:26 +0300

    add prune partitions filter to Logs.Search

--- lib/logflare/logs/log_event_search.ex
+++ lib/logflare/logs/log_event_search.ex
@@ -23,6 +23,7 @@ defmodule Logflare.Logs.Search do
     typedstruct do
       field :source, Source.t()
       field :regex, String.t()
+      field :partitions, {String.t, String.t}
     end
   end
 
@@ -39,21 +40,17 @@ defmodule Logflare.Logs.Search do
 
   @spec search(SearchOpts.t()) :: {:ok, SearchResult.t()} | {:error, term}
   def search(%SearchOpts{} = opts) do
-    %SearchOpts{ source: %Source{ bq_table_id: bq_table_id, token: source_id } } = opts
+    %SearchOpts{source: %Source{token: source_id}} = opts
     project_id = GenUtils.get_project_id(source_id)
 
     {sql, params} = to_sql(opts)
 
-    {:ok, result} =
-      query_pos_params(
-        project_id,
-        sql,
-        build_bq_pos_params(params)
-      )
-
-    rows = SchemaUtils.merge_rows_with_schema(result.schema, result.rows)
-
-    {:ok, %SearchResult{ rows: rows }}
+    with {:ok, result} <- query_pos_params(project_id, sql, params) do
+      rows = SchemaUtils.merge_rows_with_schema(result.schema, result.rows)
+      {:ok, %SearchResult{rows: rows}}
+    else
+      err -> err
+    end
   end
 
   def query_named_params(project_id, sql, params) do
@@ -88,29 +85,48 @@ defmodule Logflare.Logs.Search do
     )
   end
 
-  def to_sql(%SearchOpts{} = opts) do
+  def filter_by_regex_message(q, %SearchOpts{regex: regex}) do
+    where(q, [log], fragment("REGEXP_CONTAINS(?, ?)", log.event_message, ^regex))
+  end
+
+  def filter_by_regex_message(q, _), do: q
+
+  def prune_partitions(_q, %SearchOpts{partitions: {start_date, from_date}}) do
+    where(q, [log], fragment("_PARTITIONDATE BETWEEN ? and ?", log.timestamp, ^start_date, ^from_date))
+  end
+
+  def prune_partitions(q, _), do: q
+
+  def to_sql(log, %SearchOpts{} = opts) do
     import Ecto.Query
     import Ecto.Adapters.SQL, only: [to_sql: 3]
 
     q =
-      from(opts.source.bq_table_id, select: [:timestamp, :event_message])
-
-    q =
-      if opts.regex do
-        where(q, [log], fragment("REGEXP_CONTAINS(?, ?)", log.event_message, ^opts.regex))
-      else
-        q
-      end
+     opts.source.bq_table_id
+      |> from(select: [:timestamp, :event_message])
+      |> filter_message_by_regex(opts)
+      |> prune_partitions(opts)
 
     {sql, params} = to_sql(:all, Repo, q)
 
-    sql =
-      ecto_pg_sql_to_bq_sql(sql)
+    sql = ecto_pg_sql_to_bq_sql(sql)
+    params = ecto_pg_params_to_bq_params(params)
 
     {sql, params}
   end
 
-  def build_bq_pos_params(params) when is_list(params) do
+  def ecto_pg_sql_to_bq_sql({sql, params}) when is_list(params) do
+
+    sql = sql
+    # replaces PG-style to BQ-style positional parameters
+    |> String.replace(~r/\$\d/, "?")
+      # removes double quotes around the names after the dot
+    |> String.replace(~r/\."(\w+)"/, ".\\1")
+      # removes double quotes around the qualified BQ table id
+    |> String.replace(~r/FROM\s+"(.+)"/, "FROM \\1")
+  end
+
+  def ecto_pg_params_to_bq_params() do
     alias GoogleApi.BigQuery.V2.Model
     alias Model.QueryParameter, as: Param
     alias Model.QueryParameterType, as: Type
@@ -128,13 +144,4 @@ defmodule Logflare.Logs.Search do
     end
   end
 
-  def ecto_pg_sql_to_bq_sql(sql) do
-    sql
-    # replaces PG-style to BQ-style positional parameters
-    |> String.replace(~r/\$\d/, "?")
-      # removes double quotes around the names after the dot
-    |> String.replace(~r/\."(\w+)"/, ".\\1")
-      # removes double quotes around the qualified BQ table id
-    |> String.replace(~r/FROM\s+"(.+)"/, "FROM \\1")
-  end
 end
