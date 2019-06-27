d16c5f24c31e64fb03e69c5a65474e0a539d8e04
ontofractal <ontofractal@protonmail.com>
2019-06-27 18:01:08 +0300

    Logs search bugfixes and improvements

--- lib/logflare/logs/log_event_search.ex
+++ lib/logflare/logs/log_event_search.ex
@@ -9,12 +9,7 @@ defmodule Logflare.Logs.Search do
 
   alias GoogleApi.BigQuery.V2.Api
 
-  alias GoogleApi.BigQuery.V2.Model.{
-    QueryRequest,
-    QueryParameter,
-    QueryParameterType,
-    QueryParameterValue
-    }
+  alias GoogleApi.BigQuery.V2.Model.{QueryRequest}
 
   defmodule SearchOpts do
     @moduledoc """
@@ -25,7 +20,7 @@ defmodule Logflare.Logs.Search do
     typedstruct do
       field :source, Source.t()
       field :regex, String.t()
-      field :partitions, {String.t, String.t}
+      field :partitions, {String.t(), String.t()}
     end
   end
 
@@ -36,13 +31,14 @@ defmodule Logflare.Logs.Search do
     use TypedStruct
 
     typedstruct do
-      field :rows, [map()]
+      field(:rows, [map()])
     end
   end
 
   @spec search(SearchOpts.t()) :: {:ok, SearchResult.t()} | {:error, term}
   def search(%SearchOpts{} = opts) do
-    %SearchOpts{ source: %Source{ token: source_id } } = opts
+    %SearchOpts{source: %Source{token: source_id}} = opts
+
     project_id = GenUtils.get_project_id(source_id)
 
     {sql, params} = to_sql(opts)
@@ -90,7 +86,7 @@ defmodule Logflare.Logs.Search do
   def filter_by_injest_partitions(q, %SearchOpts{partitions: {start_date, end_date}} = opts) do
     if Timex.equal?(end_date, Date.utc_today()) do
       q
-      |> where([log], fragment("_PARTITIONDATE BETWEEN ? and ?", ^start_date, ^from_date))
+      |> where([log], fragment("_PARTITIONDATE BETWEEN ? and ?", ^start_date, ^end_date))
       |> or_where([log], fragment("_PARTITIONDATE IS NULL"))
     else
       prune_partitions(q, opts)
@@ -103,26 +99,28 @@ defmodule Logflare.Logs.Search do
 
   def filter_by_regex_message(q, _), do: q
 
-  def filter_by_streaming_filter(q, _), do: where(q, [l], fragment("_PARTITIONDATE IS NULL", l.timestamp))
-  def filter_by_streaming_filter(q, _), do: q
+  def filter_by_streaming_filter(q, _), do: where(q, [l], fragment("_PARTITIONDATE IS NULL"))
 
-  def prune_partitions(_q, %SearchOpts{partitions: {start_date, from_date}}) do
-    where(q, [log], fragment("_PARTITIONDATE BETWEEN ? and ?", log.timestamp, ^start_date, ^from_date))
+  def prune_partitions(q, %SearchOpts{partitions: {start_date, from_date}}) do
+    where(
+      q,
+      [log],
+      fragment("_PARTITIONDATE BETWEEN ? and ?", ^start_date, ^from_date)
+    )
   end
 
   def prune_partitions(q, _), do: q
 
-  def to_sql(log, %SearchOpts{} = opts) do
+  def to_sql(%SearchOpts{} = opts) do
     import Ecto.Query
     import Ecto.Adapters.SQL, only: [to_sql: 3]
 
     query =
       opts.source.bq_table_id
       |> from(select: [:timestamp, :event_message])
-      |> filter_message_by_regex(opts)
+      |> filter_by_regex_message(opts)
 
-
-    {sql, params} = to_sql(:all, Repo, q)
+    {sql, params} = to_sql(:all, Repo, query)
 
     sql = ecto_pg_sql_to_bq_sql(sql)
     params = ecto_pg_params_to_bq_params(params)
@@ -130,17 +128,17 @@ defmodule Logflare.Logs.Search do
     {sql, params}
   end
 
-  def ecto_pg_sql_to_bq_sql({sql, params}) when is_list(params) do
+  def ecto_pg_sql_to_bq_sql(sql) do
     sql
-     # replaces PG-style to BQ-style positional parameters
-     |> String.replace(~r/\$\d/, "?")
-     # removes double quotes around the names after the dot
-     |> String.replace(~r/\."(\w+)"/, ".\\1")
-     # removes double quotes around the qualified BQ table id
-     |> String.replace(~r/FROM\s+"(.+)"/, "FROM \\1")
+    # replaces PG-style to BQ-style positional parameters
+    |> String.replace(~r/\$\d/, "?")
+    # removes double quotes around the names after the dot
+    |> String.replace(~r/\."(\w+)"/, ".\\1")
+    # removes double quotes around the qualified BQ table id
+    |> String.replace(~r/FROM\s+"(.+)"/, "FROM \\1")
   end
 
-  def ecto_pg_params_to_bq_params() do
+  def ecto_pg_params_to_bq_params(params) do
     alias GoogleApi.BigQuery.V2.Model
     alias Model.QueryParameter, as: Param
     alias Model.QueryParameterType, as: Type
@@ -157,5 +155,4 @@ defmodule Logflare.Logs.Search do
       }
     end
   end
-
 end
