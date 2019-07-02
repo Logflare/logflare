7065de2e2a4a69077385aa24be28f3e2376470bd
ontofractal <ontofractal@protonmail.com>
2019-07-02 23:23:36 +0300

    add test case for EctoQueryBQ with multiple nested record values eq op

--- test/logflare/bigquery/ecto_query_bq_test.exs
+++ test/logflare/bigquery/ecto_query_bq_test.exs
@@ -5,6 +5,7 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
   use Logflare.DataCase
   import Ecto.Query
   import Logflare.DummyFactory
+  alias Logflare.EctoQueryBQ.NestedPath
 
   setup do
     u = insert(:user, email: System.get_env("LOGFLARE_TEST_USER_WITH_SET_IAM"))
@@ -51,7 +52,7 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
       sql = EctoQueryBQ.ecto_pg_sql_to_bq_sql(sql)
 
       assert sql ==
-               ~s|SELECT t0.timestamp, t0.metadata FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE INNER JOIN UNNEST(t0.user) AS f2 ON TRUE WHERE (f2.id = ?)|
+               ~s|SELECT t0.timestamp, t0.metadata FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE INNER JOIN UNNEST(f1.user) AS f2 ON TRUE WHERE (f2.id = ?)|
 
       assert params == [5]
 
@@ -68,10 +69,74 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
       sql = EctoQueryBQ.ecto_pg_sql_to_bq_sql(sql)
 
       assert sql ==
-               ~s|SELECT t0.timestamp, t0.metadata FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE INNER JOIN UNNEST(t0.user) AS f2 ON TRUE INNER JOIN UNNEST(t0.address) AS f3 ON TRUE WHERE (f3.country = ?)|
+               ~s|SELECT t0.timestamp, t0.metadata FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE INNER JOIN UNNEST(f1.user) AS f2 ON TRUE INNER JOIN UNNEST(f2.address) AS f3 ON TRUE WHERE (f3.country = ?)|
 
       assert params == ["AQ"]
     end
+
+    test "multiple paths with various depth levels", %{sources: [source | _], users: [user | _]} do
+      bq_table_id = System.get_env("LOGFLARE_DEV_BQ_TABLE_ID_FOR_TESTING")
+
+      pathvalues = [
+        %{
+          path: "metadata.user.address.country",
+          value: "AQ"
+        },
+        %{
+          path: "metadata.user.address.city",
+          value: "Aboa"
+        },
+        %{
+          path: "metadata.datacenter",
+          value: "AWS"
+        },
+        %{
+          path: "metadata.context.pid",
+          value: "<0.255.0>"
+        },
+        %{
+          path: "metadata.context.file",
+          value: "lib/bigquery.ex"
+        }
+      ]
+
+      q =
+        from(bq_table_id)
+        |> select([:timestamp, :metadata])
+        |> EctoQueryBQ.where_nested_eqs(pathvalues)
+
+      {sql, params} = Ecto.Adapters.SQL.to_sql(:all, Repo, q)
+
+      sql = EctoQueryBQ.ecto_pg_sql_to_bq_sql(sql)
+
+      sql =
+        Enum.reduce(params, sql, fn param, sql ->
+          String.replace(sql, ~S"?", ~s|"#{param}"|, global: false)
+        end)
+
+      path = Application.app_dir(:logflare) <> "/generated.sql"
+      File.write!(path, sql)
+
+      assert sql ==
+               ~s|
+               SELECT t0.timestamp, t0.metadata
+               FROM #{bq_table_id} AS t0
+               INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE
+               INNER JOIN UNNEST(f1.context) AS f2 ON TRUE
+               INNER JOIN UNNEST(f2.user) AS f3 ON TRUE
+               INNER JOIN UNNEST(f3.address) AS f4 ON TRUE
+           WHERE (f1.datacenter = "AWS")
+           AND (f2.file = "lib/bigquery.ex")
+           AND (f2.pid = "<0.255.0>")
+           AND (f4.city = "Aboa")
+           AND (f4.country = "AQ")
+               |
+               |> String.replace(~r/\s+/, " ")
+               |> String.trim()
+
+      assert params == ["AWS", "lib/bigquery.ex", "<0.255.0>", "Aboa", "AQ"]
+    end
+  end
 
   describe "NestedPath" do
     test "to_map" do
