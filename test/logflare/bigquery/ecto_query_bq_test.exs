4ce0af7cf6407009fc7fedf65fded8319eeca678
ontofractal <ontofractal@protonmail.com>
2019-07-02 14:13:06 +0300

    add EctoQueryBQ test cases for nested structs with 2 and 3 level deep

--- test/logflare/bigquery/ecto_query_bq_test.exs
+++ test/logflare/bigquery/ecto_query_bq_test.exs
@@ -34,5 +34,43 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
 
       assert params == ["ne-1"]
     end
+
+    test "2 and 3 level deep", %{sources: [source | _], users: [user | _]} do
+      bq_table_id = System.get_env("LOGFLARE_DEV_BQ_TABLE_ID_FOR_TESTING")
+
+      path = "metadata.user.id"
+      value = 5
+
+      q =
+        from(bq_table_id)
+        |> select([:timestamp, :metadata])
+        |> EctoQueryBQ.where_nested_eq(path, value)
+
+      {sql, params} = Ecto.Adapters.SQL.to_sql(:all, Repo, q)
+
+      sql = EctoQueryBQ.ecto_pg_sql_to_bq_sql(sql)
+
+      assert sql ==
+               ~s|SELECT t0.timestamp, t0.metadata FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE INNER JOIN UNNEST(t0.user) AS f2 ON TRUE WHERE (f2.id = ?)|
+
+      assert params == [5]
+
+      path = "metadata.user.address.country"
+      value = "AQ"
+
+      q =
+        from(bq_table_id)
+        |> select([:timestamp, :metadata])
+        |> EctoQueryBQ.where_nested_eq(path, value)
+
+      {sql, params} = Ecto.Adapters.SQL.to_sql(:all, Repo, q)
+
+      sql = EctoQueryBQ.ecto_pg_sql_to_bq_sql(sql)
+
+      assert sql ==
+               ~s|SELECT t0.timestamp, t0.metadata FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE INNER JOIN UNNEST(t0.user) AS f2 ON TRUE INNER JOIN UNNEST(t0.address) AS f3 ON TRUE WHERE (f3.country = ?)|
+
+      assert params == ["AQ"]
+    end
   end
 end
