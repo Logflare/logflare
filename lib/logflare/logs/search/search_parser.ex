2847012efc5910da31d090046212ba4b3f7aa7aa
ontofractal <ontofractal@protonmail.com>
2019-07-03 23:23:58 +0300

    teach Logs.Search.Parser to handle field nested filters in search query

--- lib/logflare/logs/search/search_parser.ex
+++ lib/logflare/logs/search/search_parser.ex
@@ -4,10 +4,69 @@ defmodule Logflare.Logs.Search.Parser do
     result =
       searchq
       |> extract_quoted_strings()
+      |> extract_fields_filter()
       |> build_message_clauses()
       |> Map.get(:clauses)
 
     {:ok, result}
+  end
+
+  def extract_fields_filter(parsemap) do
+    regex = ~r/(metadata\.[\w\.]+:[\d\w\.><=~]+)/
+    fields_strings = Regex.scan(regex, parsemap.searchq, capture: :all_but_first)
+    searchq = Regex.replace(regex, parsemap.searchq, "")
+
+    clauses =
+      for [fs] <- fields_strings do
+        case String.split(fs, ":") do
+          [column, ">=" <> val] ->
+            %{
+              path: column,
+              value: val,
+              operator: ">="
+            }
+
+          [column, ">" <> val] ->
+            %{
+              path: column,
+              value: val,
+              operator: ">"
+            }
+
+          [column, "<=" <> val] ->
+            %{
+              path: column,
+              value: val,
+              operator: "<="
+            }
+
+          [column, "<" <> val] ->
+            %{
+              path: column,
+              value: val,
+              operator: "<"
+            }
+
+          [column, "~" <> val] ->
+            %{
+              path: column,
+              value: val,
+              operator: "~"
+            }
+
+          [column, val] ->
+            %{
+              path: column,
+              value: val,
+              operator: "="
+            }
+        end
+      end
+
+    %{
+      searchq: searchq,
+      clauses: parsemap.clauses ++ clauses
+    }
   end
 
   def build_message_clauses(parsemap) do
@@ -17,7 +76,7 @@ defmodule Logflare.Logs.Search.Parser do
 
   def build_message_clause(word) do
     %{
-      path: "metadata.message",
+      path: "message",
       value: word,
       operator: "~"
     }
--- test/logflare/logs/search_parser_test.exs
+++ test/logflare/logs/search_parser_test.exs
@@ -9,9 +9,9 @@ defmodule Logflare.Logs.Search.ParserTest do
       {:ok, result} = Parser.parse(str)
 
       assert result == [
-               %{operator: "~", path: "metadata.message", value: "user"},
-               %{operator: "~", path: "metadata.message", value: "sign"},
-               %{operator: "~", path: "metadata.message", value: "up"}
+               %{operator: "~", path: "message", value: "user"},
+               %{operator: "~", path: "message", value: "sign"},
+               %{operator: "~", path: "message", value: "up"}
              ]
     end
 
@@ -20,12 +20,31 @@ defmodule Logflare.Logs.Search.ParserTest do
       {:ok, result} = Parser.parse(str)
 
       assert result == [
-               %{operator: "~", path: "metadata.message", value: "user sign up"},
-               [
-                 %{operator: "~", path: "metadata.message", value: "user sign up"},
-                 %{operator: "~", path: "metadata.message", value: "new"},
-                 %{operator: "~", path: "metadata.message", value: "server"}
-               ]
+               %{operator: "~", path: "message", value: "user sign up"},
+               %{operator: "~", path: "message", value: "new"},
+               %{operator: "~", path: "message", value: "server"}
+             ]
+    end
+
+    test "nested fields filter" do
+      str = ~S|
+        metadata.user.type:paid
+        metadata.user.id:<1
+        metadata.user.views:<=1
+        metadata.users.source_count:>100
+        metadata.context.error_count:>=100
+        metadata.user.about:~referrall
+      |
+
+      {:ok, result} = Parser.parse(str)
+
+      assert result == [
+               %{operator: "=", path: "metadata.user.type", value: "paid"},
+               %{operator: "<", path: "metadata.user.id", value: "1"},
+               %{operator: "<=", path: "metadata.user.views", value: "1"},
+               %{operator: ">", path: "metadata.users.source_count", value: "100"},
+               %{operator: ">=", path: "metadata.context.error_count", value: "100"},
+               %{operator: "~", path: "metadata.user.about", value: "referrall"}
              ]
     end
   end
