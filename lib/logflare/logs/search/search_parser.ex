4391424e0bb4fa4815bf05a2d177c99b14d92530
ontofractal <ontofractal@protonmail.com>
2019-07-03 21:05:06 +0300

    teach Logs.Search.Parser to handle quoted strings in search query

--- lib/logflare/logs/search/search_parser.ex
+++ lib/logflare/logs/search/search_parser.ex
@@ -1,17 +1,43 @@
 defmodule Logflare.Logs.Search.Parser do
   @moduledoc false
   def parse(searchq) do
-    result = build_message_clauses(searchq)
+    result =
+      searchq
+      |> extract_quoted_strings()
+      |> build_message_clauses()
+      |> Map.get(:clauses)
+
     {:ok, result}
   end
 
-  def build_message_clauses(searchq) do
-    for word <- String.split(searchq) do
-      %{
-        path: "metadata.message",
-        value: word,
-        operator: "~"
-      }
-    end
+  def build_message_clauses(parsemap) do
+    clauses = for word <- String.split(parsemap.searchq), do: build_message_clause(word)
+    %{parsemap | clauses: parsemap.clauses ++ clauses}
+  end
+
+  def build_message_clause(word) do
+    %{
+      path: "metadata.message",
+      value: word,
+      operator: "~"
+    }
+  end
+
+  def extract_quoted_strings(searchq) do
+    regex = ~r/"(.+)"/
+    quoted_strings = Regex.run(regex, searchq, capture: :all_but_first)
+    searchq = Regex.replace(regex, searchq, "")
+
+    clauses =
+      if quoted_strings do
+        for(qs <- quoted_strings, do: build_message_clause(qs))
+      else
+        []
+      end
+
+    %{
+      searchq: searchq,
+      clauses: clauses
+    }
   end
 end
--- test/logflare/logs/search_parser_test.exs
+++ test/logflare/logs/search_parser_test.exs
@@ -14,5 +14,19 @@ defmodule Logflare.Logs.Search.ParserTest do
                %{operator: "~", path: "metadata.message", value: "up"}
              ]
     end
+
+    test "quoted message search string" do
+      str = ~S|new "user sign up" server|
+      {:ok, result} = Parser.parse(str)
+
+      assert result == [
+               %{operator: "~", path: "metadata.message", value: "user sign up"},
+               [
+                 %{operator: "~", path: "metadata.message", value: "user sign up"},
+                 %{operator: "~", path: "metadata.message", value: "new"},
+                 %{operator: "~", path: "metadata.message", value: "server"}
+               ]
+             ]
+    end
   end
 end
