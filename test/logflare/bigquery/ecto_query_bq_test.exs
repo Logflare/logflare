bec548b4cb447f3b3af01483954fab62951aeb56
ontofractal <ontofractal@protonmail.com>
2019-07-05 23:38:47 +0300

    update EctoQueryBQ test to match latest module updates

--- test/logflare/bigquery/ecto_query_bq_test.exs
+++ test/logflare/bigquery/ecto_query_bq_test.exs
@@ -1,11 +1,10 @@
-defmodule Logflare.BigQuery.EctoQueryBQ do
+defmodule Logflare.BigQuery.EctoQueryBQTest do
   @moduledoc false
   alias Logflare.Sources
   alias Logflare.EctoQueryBQ
   use Logflare.DataCase
   import Ecto.Query
   import Logflare.DummyFactory
-  alias Logflare.EctoQueryBQ.NestedPath
 
   setup do
     u = insert(:user, email: System.get_env("LOGFLARE_TEST_USER_WITH_SET_IAM"))
@@ -15,7 +14,7 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
   end
 
   describe "where_nesteds" do
-    test "1 level deep", %{sources: [source | _], users: [user | _]} do
+    test "1 level deep" do
       bq_table_id = System.get_env("LOGFLARE_DEV_BQ_TABLE_ID_FOR_TESTING")
 
       pathvalops = [
@@ -26,24 +25,26 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
         }
       ]
 
-      pathvalops = NestedPath.to_map(pathvalops)
-
       q =
         from(bq_table_id)
         |> select([:timestamp, :metadata])
         |> EctoQueryBQ.where_nesteds(pathvalops)
 
-      {sql, params} = Ecto.Adapters.SQL.to_sql(:all, Repo, q)
-
-      sql = EctoQueryBQ.ecto_pg_sql_to_bq_sql(sql)
+      {sql, params} = EctoQueryBQ.SQL.to_sql(q)
 
       assert sql ==
-               ~s|SELECT t0.timestamp, t0.metadata FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE WHERE (f1.datacenter = ?)|
+               ~s|
+               SELECT t0.timestamp, t0.metadata
+               FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE
+               WHERE (f1.datacenter = ?)|
+               |> String.replace(~r/[\s]+/, " ")
+               |> String.trim()
 
-      assert params == ["ne-1"]
+      assert params == [%GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "STRING"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: "ne-1"}}]
+
     end
 
-    test "2 and 3 level deep", %{sources: [source | _], users: [user | _]} do
+    test "2 and 3 level deep" do
       bq_table_id = System.get_env("LOGFLARE_DEV_BQ_TABLE_ID_FOR_TESTING")
 
       pathvalops = [
@@ -51,24 +52,61 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
           path: "metadata.user.id",
           operator: "=",
           value: 5
+        },
+        %{
+          path: "metadata.datacenter",
+          value: "ne-1",
+          operator: "="
         }
       ]
-
-      pathvalops = NestedPath.to_map(pathvalops)
 
       q =
         from(bq_table_id)
         |> select([:timestamp, :metadata])
         |> EctoQueryBQ.where_nesteds(pathvalops)
 
-      {sql, params} = Ecto.Adapters.SQL.to_sql(:all, Repo, q)
-
-      sql = EctoQueryBQ.ecto_pg_sql_to_bq_sql(sql)
+      {sql, params} = EctoQueryBQ.SQL.to_sql(q)
 
       assert sql ==
-               ~s|SELECT t0.timestamp, t0.metadata FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE INNER JOIN UNNEST(f1.user) AS f2 ON TRUE WHERE (f2.id = ?)|
+               ~s|
+               SELECT t0.timestamp, t0.metadata
+               FROM `logflare-dev-238720`.1_dev.6efefdc5_e6fa_4193_864a_9e9daa6924d7 AS t0
+               INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE
+               INNER JOIN UNNEST(t0.metadata) AS f2 ON TRUE
+               INNER JOIN UNNEST(f2.user) AS f3 ON TRUE
+               WHERE (f1.datacenter = ?) AND (f3.id = ?)
+               |
+               |> String.replace(~r/[\s]+/, " ")
+               |> String.trim()
 
-      assert params == [5]
+      assert params == [
+               %GoogleApi.BigQuery.V2.Model.QueryParameter{
+                 name: nil,
+                 parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{
+                   arrayType: nil,
+                   structTypes: nil,
+                   type: "STRING"
+                 },
+                 parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{
+                   arrayValues: nil,
+                   structValues: nil,
+                   value: "ne-1"
+                 }
+               },
+               %GoogleApi.BigQuery.V2.Model.QueryParameter{
+                 name: nil,
+                 parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{
+                   arrayType: nil,
+                   structTypes: nil,
+                   type: "INTEGER"
+                 },
+                 parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{
+                   arrayValues: nil,
+                   structValues: nil,
+                   value: 5
+                 }
+               }
+             ]
 
       pathvalops = [
         %{
@@ -78,24 +116,43 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
         }
       ]
 
-      pathvalops = NestedPath.to_map(pathvalops)
-
       q =
         from(bq_table_id)
         |> select([:timestamp, :metadata])
         |> EctoQueryBQ.where_nesteds(pathvalops)
 
-      {sql, params} = Ecto.Adapters.SQL.to_sql(:all, Repo, q)
-
-      sql = EctoQueryBQ.ecto_pg_sql_to_bq_sql(sql)
+      {sql, params} = EctoQueryBQ.SQL.to_sql(q)
 
       assert sql ==
-               ~s|SELECT t0.timestamp, t0.metadata FROM #{bq_table_id} AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE INNER JOIN UNNEST(f1.user) AS f2 ON TRUE INNER JOIN UNNEST(f2.address) AS f3 ON TRUE WHERE (f3.country = ?)|
+               ~s|
+               SELECT t0.timestamp, t0.metadata
+               FROM `logflare-dev-238720`.1_dev.6efefdc5_e6fa_4193_864a_9e9daa6924d7 AS t0
+               INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE
+               INNER JOIN UNNEST(f1.user) AS f2 ON TRUE
+               INNER JOIN UNNEST(f2.address) AS f3 ON TRUE
+               WHERE (f3.country = ?)
+               |
+               |> String.replace(~r/[\s]+/, " ")
+               |> String.trim()
 
-      assert params == ["AQ"]
+      assert params == [
+               %GoogleApi.BigQuery.V2.Model.QueryParameter{
+                 name: nil,
+                 parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{
+                   arrayType: nil,
+                   structTypes: nil,
+                   type: "STRING"
+                 },
+                 parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{
+                   arrayValues: nil,
+                   structValues: nil,
+                   value: "AQ"
+                 }
+               }
+             ]
     end
 
-    test "multiple paths with various depth levels", %{sources: [source | _], users: [user | _]} do
+    test "multiple paths with various depth levels" do
       bq_table_id = System.get_env("LOGFLARE_DEV_BQ_TABLE_ID_FOR_TESTING")
 
       pathvalops = [
@@ -115,9 +172,9 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
           value: 100
         },
         %{
-          path: "metadata.user.source_count",
-          operator: "<",
-          value: 10
+          path: "metadata.context.pid",
+          operator: "=",
+          value: "<0.255.0>"
         },
         %{
           path: "metadata.user.exceptions",
@@ -140,9 +197,9 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
           value: "AWS"
         },
         %{
-          path: "metadata.context.pid",
-          operator: "=",
-          value: "<0.255.0>"
+          path: "metadata.user.source_count",
+          operator: "<",
+          value: 10
         },
         %{
           path: "metadata.context.file",
@@ -150,99 +207,40 @@ defmodule Logflare.BigQuery.EctoQueryBQ do
           value: "lib/bigquery.ex"
         }
       ]
-
-      pathvalops = NestedPath.to_map(pathvalops)
 
       q =
         from(bq_table_id)
         |> select([:timestamp, :metadata])
         |> EctoQueryBQ.where_nesteds(pathvalops)
 
-      {sql, params} = Ecto.Adapters.SQL.to_sql(:all, Repo, q)
-
-      sql = EctoQueryBQ.ecto_pg_sql_to_bq_sql(sql)
-
-      sql =
-        Enum.reduce(params, sql, fn param, sql ->
-          replacement =
-            if is_binary(param) and param not in ~w(>= <= > <) do
-              ~s|"#{param}"|
-            else
-              ~s|#{param}|
-            end
-
-          String.replace(sql, ~r/\?\d?/, replacement, global: false)
-        end)
-
-      path = Application.app_dir(:logflare) <> "/generated.sql"
-      File.write!(path, sql)
+      {sql, params} = EctoQueryBQ.SQL.to_sql(q)
 
       assert sql ==
                ~s|
-               SELECT
-               t0.timestamp,
-               t0.metadata
-           FROM
-               `logflare-dev-238720`.1_dev.6efefdc5_e6fa_4193_864a_9e9daa6924d7 AS t0
-               INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE
-               INNER JOIN UNNEST(f1.context) AS f2 ON TRUE
-               INNER JOIN UNNEST(f2.user) AS f3 ON TRUE
-               INNER JOIN UNNEST(f3.address) AS f4 ON TRUE
-           WHERE (f1.datacenter = "AWS")
-           AND (f2.file = "lib/bigquery.ex")
-           AND (f2.pid = "<0.255.0>")
-           AND (f3.exceptions >= 0)
-           AND (REGEXP_CONTAINS(f3.name, "Neo"))
-           AND (f3.rating > 100)
-           AND (f3.source_count < 10)
-           AND (f3.variables <= 2)
-           AND (f4.city = "Aboa")
-           AND (f4.country = "AQ")
-               |
-               |> String.replace(~r/\s+/, " ")
+               SELECT t0.timestamp, t0.metadata
+               FROM `logflare-dev-238720`.1_dev.6efefdc5_e6fa_4193_864a_9e9daa6924d7 AS t0 INNER JOIN UNNEST(t0.metadata) AS f1 ON TRUE
+               INNER JOIN UNNEST(t0.metadata) AS f2 ON TRUE
+               INNER JOIN UNNEST(f2.context) AS f3 ON TRUE
+               INNER JOIN UNNEST(t0.metadata) AS f4 ON TRUE
+               INNER JOIN UNNEST(f4.user) AS f5 ON TRUE
+               INNER JOIN UNNEST(t0.metadata) AS f6 ON TRUE
+               INNER JOIN UNNEST(f6.user) AS f7 ON TRUE
+               INNER JOIN UNNEST(f7.address) AS f8 ON TRUE
+               WHERE (f1.datacenter = ?)
+               AND (f3.pid = ?)
+               AND (f3.file = ?)
+               AND (f5.rating > ?)
+               AND (f5.exceptions >= ?)
+               AND (f5.variables <= ?)
+               AND (REGEXP_CONTAINS(f5.name, ?))
+               AND (f5.source_count < ?)
+               AND (f8.country = ?)
+               AND (f8.city = ?)|
+               |> String.replace(~r/[\s]+/, " ")
                |> String.trim()
 
-      assert params == ["AWS", "lib/bigquery.ex", "<0.255.0>", 0, "Neo", 100, 10, 2, "Aboa", "AQ"]
-    end
-  end
+      assert params == [%GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "STRING"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: "AWS"}}, %GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "STRING"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: "<0.255.0>"}}, %GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "STRING"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: "lib/bigquery.ex"}}, %GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "INTEGER"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: 100}}, %GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "INTEGER"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: 0}}, %GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "INTEGER"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: 2}}, %GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "STRING"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: "Neo"}}, %GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "INTEGER"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: 10}}, %GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "STRING"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: "AQ"}}, %GoogleApi.BigQuery.V2.Model.QueryParameter{name: nil, parameterType: %GoogleApi.BigQuery.V2.Model.QueryParameterType{arrayType: nil, structTypes: nil, type: "STRING"}, parameterValue: %GoogleApi.BigQuery.V2.Model.QueryParameterValue{arrayValues: nil, structValues: nil, value: "Aboa"}}]
 
-  describe "NestedPath" do
-    test "to_map" do
-      pathvalues = [
-        %{
-          path: "metadata.user.address.country",
-          operator: "=",
-          value: "AQ"
-        },
-        %{
-          path: "metadata.user.address.city",
-          operator: "=",
-          value: "Aboa"
-        },
-        %{
-          path: "metadata.datacenter",
-          operator: "=",
-          value: "AWS"
-        },
-        %{
-          path: "metadata.context.pid",
-          operator: "=",
-          value: "<0.255.0>"
-        },
-        %{
-          path: "metadata.context.file",
-          operator: "=",
-          value: "lib/bigquery.ex"
-        }
-      ]
-
-      assert EctoQueryBQ.NestedPath.to_map(pathvalues) == %{
-               metadata: %{
-                 context: %{file: {"=", "lib/bigquery.ex"}, pid: {"=", "<0.255.0>"}},
-                 datacenter: {"=", "AWS"},
-                 user: %{address: %{city: {"=", "Aboa"}, country: {"=", "AQ"}}}
-               }
-             }
     end
   end
 end
