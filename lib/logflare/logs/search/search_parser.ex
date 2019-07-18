45eee255faec33bc0e4352ebb231e1934c32cdc1
ontofractal <ontofractal@protonmail.com>
2019-07-18 14:47:28 +0300

    Logs.Search.Parser: code improvements

--- lib/logflare/logs/search/search_parser.ex
+++ lib/logflare/logs/search/search_parser.ex
@@ -19,51 +19,48 @@ defmodule Logflare.Logs.Search.Parser do
     end
   end
 
-  def extract_fields_filter(parsemap) do
+  defp extract_fields_filter(parsemap) do
     # uses non-capturing groups to match
     # either double-quoted values with default equality operator
     # or values following the specified operator
     regex = ~r/(metadata\.[\w\.]+:(?:(?:[\d\w\.~=><]+)|(?:".+")))/
 
-    fields_strings =
-      regex |> Regex.scan(parsemap.searchq, capture: :all_but_first) |> List.flatten()
-
-    searchq = Regex.replace(regex, parsemap.searchq, "")
-
     clauses =
-      for fs <- fields_strings do
-        [column, operatorvalue] = String.split(fs, ":")
+      regex
+      |> Regex.scan(parsemap.searchq, capture: :all_but_first)
+      |> List.flatten()
+      |> Enum.map(fn string ->
+        [column, operatorvalue] = String.split(string, ":")
         op_regex = ~r/<=|>=|<|>|~/
         maybe_op = Regex.run(op_regex, operatorvalue)
         [op_val] = maybe_op || ["="]
 
-        value = operatorvalue |> String.replace(op_regex, "") |> String.replace(~S|"|, "")
+        value =
+          operatorvalue
+          |> String.replace(op_regex, "")
+          |> String.replace(~S|"|, "")
 
         %{
           path: column,
           value: value,
           operator: op_val
         }
-      end
+      end)
 
     %{
-      searchq: searchq,
+      searchq: Regex.replace(regex, parsemap.searchq, ""),
       clauses: parsemap.clauses ++ clauses
     }
   end
 
-  def extract_timestamp_filter(parsemap) do
+  defp extract_timestamp_filter(parsemap) do
     r_iso8601_date = ~S/(?:[0-9]{4})(?:-?)(?:1[0-2]|0[1-9])(?:-?)(?:3[01]|0[1-9]|[12][0-9])/
 
     r_iso8601_datetime =
       ~S/(?:-?(?:[1-9][0-9]*)?[0-9]{4})-(?:1[0-2]|0[1-9])-(?:3[01]|0[1-9]|[12][0-9])T(?:2[0-3]|[01][0-9]):(?:[0-5][0-9]):(?:[0-5][0-9])(?:\.[0-9]+)?(?:Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?/
 
-    # rdate = ~S|\d\d\d\d\-\d\d\-\d\d|
-
-    # rdatetime =
-    #   ~S/(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?/
-
     roperator = ~S/>=|<=|<|>/
+
     regex = ~r/timestamp:(#{roperator})(#{r_iso8601_datetime}|#{r_iso8601_date})/
 
     matches = Regex.scan(regex, parsemap.searchq, capture: :all_but_first)
@@ -92,7 +89,7 @@ defmodule Logflare.Logs.Search.Parser do
     }
   end
 
-  def build_message_clauses(parsemap) do
+  defp build_message_clauses(parsemap) do
     clauses =
       parsemap.searchq
       |> String.split()
@@ -101,7 +98,7 @@ defmodule Logflare.Logs.Search.Parser do
     %{parsemap | clauses: parsemap.clauses ++ clauses}
   end
 
-  def build_message_clause(word) do
+  defp build_message_clause(word) do
     %{
       path: "event_message",
       value: word,
@@ -109,8 +106,8 @@ defmodule Logflare.Logs.Search.Parser do
     }
   end
 
-  def maybe_cast_value(%{operator: op, value: sourcevalue} = c)
-      when op in @arithmetic_operators and is_binary(sourcevalue) do
+  defp maybe_cast_value(%{operator: op, value: sourcevalue} = c)
+       when op in @arithmetic_operators and is_binary(sourcevalue) do
     value =
       with :error <- Integer.parse(c.value),
            :error <- Float.parse(c.value) do
@@ -123,9 +120,9 @@ defmodule Logflare.Logs.Search.Parser do
     %{c | value: value}
   end
 
-  def maybe_cast_value(c), do: c
+  defp maybe_cast_value(c), do: c
 
-  def extract_quoted_strings(parsemap) do
+  defp extract_quoted_strings(parsemap) do
     searchq = parsemap.searchq
 
     # uses negative lookbehind to find double quoted strings that are not preceded with :
@@ -137,10 +134,8 @@ defmodule Logflare.Logs.Search.Parser do
       |> List.flatten()
       |> Enum.map(&build_message_clause/1)
 
-    searchq = Regex.replace(regex, searchq, "")
-
     %{
-      searchq: searchq,
+      searchq: Regex.replace(regex, searchq, ""),
       clauses: clauses ++ parsemap.clauses
     }
   end
