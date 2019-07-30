dfa332e72b7ba0290bc7d5658318e21a67a37381
chasers <chase@pagemodified.com>
2019-07-30 10:29:34 -0700

    Version bump for search

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Logflare.Mixfile do
   def project do
     [
       app: :logflare,
-      version: "0.13.3",
+      version: "0.14.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
