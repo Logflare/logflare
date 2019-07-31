59d019678fd2d0bfc30f47cc0efa6b80082f01aa
chasers <chase@pagemodified.com>
2019-07-31 14:14:14 -0700

    Search UX progress

--- lib/logflare_web/live/sources/logs_search_lv.ex
+++ lib/logflare_web/live/sources/logs_search_lv.ex
@@ -173,6 +173,7 @@ defmodule LogflareWeb.Source.TailSearchLV do
       |> Enum.uniq_by(& &1.body.timestamp)
       |> Enum.sort_by(& &1.body.timestamp, &>=/2)
       |> Enum.take(@max_user_logs)
+      |> Enum.reverse()
 
     tailing? = socket.assigns.tailing?
     querystring = socket.assigns.querystring
