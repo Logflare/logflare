a6d9c70e227a899ae1f48db245be5b3e1bd4e1ff
chasers <chase@pagemodified.com>
2019-08-05 13:54:51 -0700

    Cleanup

--- lib/logflare_web/controllers/admin_controller.ex
+++ lib/logflare_web/controllers/admin_controller.ex
@@ -8,30 +8,30 @@ defmodule LogflareWeb.AdminController do
   @default_sort_by :latest
 
   def dashboard(conn, params) do
-    sorted_sources = sorted_sources(params)
     sort_options = [:schema_fields, :latest, :rejected]
+    sorted_sources = sorted_sources(params)
 
     render(conn, "dashboard.html", sources: sorted_sources, sort_options: sort_options)
   end
 
-  defp sorted_sources(%{"page" => page, "sort_by" => sort_by} = params) do
-    query
+  defp sorted_sources(%{"page" => page, "sort_by" => sort_by} = _params) do
+    query()
     |> Repo.all()
     |> Enum.map(&Sources.preload_defaults/1)
     |> Enum.sort_by(&Map.fetch(&1.metrics, String.to_atom(sort_by)), &>=/2)
     |> Repo.paginate(%{page_size: @page_size, page: page})
   end
 
-  defp sorted_sources(%{"sort_by" => sort_by} = params) do
-    query
+  defp sorted_sources(%{"sort_by" => sort_by} = _params) do
+    query()
     |> Repo.all()
     |> Enum.map(&Sources.preload_defaults/1)
     |> Enum.sort_by(&Map.fetch(&1.metrics, String.to_atom(sort_by)), &>=/2)
     |> Repo.paginate(%{page_size: @page_size, page: 1})
   end
 
-  defp sorted_sources(params) do
-    query
+  defp sorted_sources(_params) do
+    query()
     |> Repo.all()
     |> Enum.map(&Sources.preload_defaults/1)
     |> Enum.sort_by(&Map.fetch(&1.metrics, @default_sort_by), &>=/2)
