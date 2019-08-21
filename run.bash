705a2ab2c875c507c7dc2896a223268af0b68803
ontofractal <ontofractal@protonmail.com>
2019-08-21 23:54:47 +0300

    use ecto.migrate for now

--- run.bash
+++ run.bash
@@ -1,3 +1,4 @@
 #! /bin/bash
-/logflare/_build/staging/rel/logflare/bin/logflare eval "Logflare.Tasks.ReleaseTasks.setup()"
+# /logflare/_build/staging/rel/logflare/bin/logflare eval "Logflare.Tasks.ReleaseTasks.setup()" && \
+mix ecto.migrate && \
 /logflare/_build/staging/rel/logflare/bin/logflare start
