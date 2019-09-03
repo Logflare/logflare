552ace98103facdf54c2a57f4076a7554bd2c649
ontofractal <ontofractal@protonmail.com>
2019-09-03 22:34:45 +0300

    SigTermHandler: improve logging

--- lib/sig_term_handler.ex
+++ lib/sig_term_handler.ex
@@ -8,19 +8,20 @@ defmodule Logflare.SigtermHandler do
 
   @impl true
   def init(_) do
+    Logger.info("#{__MODULE__} is being initialized...")
     {:ok, %{}}
   end
 
   @impl true
   def handle_info(:proceed_with_sigterm, state) do
-    Logger.warn("Shutdown grace period reacher, stopping the app...")
+    Logger.warn("#{__MODULE__}: shutdown grace period reached, stopping the app...")
     :init.stop()
     {:ok, state}
   end
 
   @impl true
   def handle_event(:sigterm, state) do
-    Logger.warn("SIGTERM received: waiting for #{@grace_period / 1_000} seconds")
+    Logger.warn("#{__MODULE__}: SIGTERM received: waiting for #{@grace_period / 1_000} seconds")
     Process.send_after(self(), :proceed_with_sigterm, @grace_period)
 
     {:ok, state}
@@ -28,12 +29,13 @@ defmodule Logflare.SigtermHandler do
 
   @impl true
   def handle_event(ev, _state) do
+    Logger.warn("#{__MODULE__}: has received a system signal: #{ev} and redirected it to :erl_signal_server")
     :gen_event.notify(:erl_signal_server, ev)
   end
 
   @impl true
   def handle_call(msg, state) do
-    Logger.warn("#{__MODULE__} has received an expected call: #{inspect(msg)}")
+    Logger.warn("#{__MODULE__} has received an unexpected call: #{inspect(msg)}")
     {:ok, :ok, state}
   end
 end
