67bb8f408c47bbc597a37985220f31cdf4b0424d
ontofractal <ontofractal@protonmail.com>
2019-10-16 01:27:28 +0300

    configure GOOGLE_APPLICATION_CREDENTIALS env in run.bash

--- run.bash
+++ run.bash
@@ -1,4 +1,6 @@
 #! /bin/bash
 # /logflare/_build/staging/rel/logflare/bin/logflare eval "Logflare.Tasks.ReleaseTasks.setup()" && \
+export GOOGLE_APPLICATION_CREDENTIALS=/logflare/gcloud.json
+
 mix ecto.migrate && \
 /logflare/_build/prod/rel/logflare/bin/logflare start
