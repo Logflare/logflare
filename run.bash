9ee6bdde555adde4d73450c3f652fc6fca4cd484
ontofractal <ontofractal@protonmail.com>
2019-10-16 00:45:06 +0300

    update run.bash

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 # /logflare/_build/staging/rel/logflare/bin/logflare eval "Logflare.Tasks.ReleaseTasks.setup()" && \
 mix ecto.migrate && \
-/logflare/_build/staging/rel/logflare/bin/logflare start
+/logflare/_build/prod/rel/logflare/bin/logflare start
