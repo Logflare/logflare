8866de10aabe1d5229dd1d22e1503955bc8634c9
Chase Granberry <chase@logflare.app>
2019-10-30 01:43:26 -0700

    Not as terrible

--- lib/logflare/source/recent_logs_server.ex
+++ lib/logflare/source/recent_logs_server.ex
@@ -55,9 +55,6 @@ defmodule Logflare.Source.RecentLogsServer do
   ## Server
 
   def handle_continue(:boot, %__MODULE__{source_id: source_id} = rls) when is_atom(source_id) do
-    # Terrible
-    Enum.random(0..60_000) |> Process.sleep()
-
     %{
       user_id: user_id,
       bigquery_table_ttl: bigquery_table_ttl,
