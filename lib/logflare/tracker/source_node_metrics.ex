2c03bf1d4b173e40834d6a4008b911df2238c032
Chase Granberry <chase@logflare.app>
2019-11-01 07:05:35 -0700

    Pull out rates

--- lib/logflare/tracker/source_node_metrics.ex
+++ lib/logflare/tracker/source_node_metrics.ex
@@ -10,7 +10,6 @@ defmodule Logflare.Tracker.SourceNodeMetrics do
   use GenServer
 
   @check_buffers_every 1_000
-  @check_rates_every 1_000
   @check_total_inserts_every 250
 
   def start_link() do
@@ -25,7 +24,6 @@ defmodule Logflare.Tracker.SourceNodeMetrics do
     init_trackers()
     check_buffers()
     check_total_inserts()
-    check_rates()
     {:ok, state}
   end
 
@@ -82,42 +80,6 @@ defmodule Logflare.Tracker.SourceNodeMetrics do
     {:noreply, state}
   end
 
-  def handle_info(:check_rates, state) do
-    query =
-      from(s in "sources",
-        select: %{
-          source_id: s.token
-        }
-      )
-
-    sources =
-      Repo.all(query)
-      |> Stream.map(fn x ->
-        {:ok, source_id} = Ecto.UUID.load(x.source_id)
-        source_id_atom = String.to_atom(source_id)
-        avg_rate = Source.Data.get_avg_rate(source_id_atom)
-        last_rate = Source.Data.get_rate(source_id_atom)
-        max_rate = Source.Data.get_max_rate(source_id_atom)
-
-        limiter_metrics = Source.RateCounterServer.get_rate_metrics(source_id_atom)
-
-        {source_id,
-         %{
-           average_rate: avg_rate,
-           last_rate: last_rate,
-           max_rate: max_rate,
-           limiter_metrics: limiter_metrics
-         }}
-      end)
-      |> Enum.into(%{})
-
-    update_tracker(sources, "rates")
-    Tracker.Cache.cache_cluster_rates()
-
-    check_rates()
-    {:noreply, state}
-  end
-
   defp update_tracker(sources, type) do
     Logflare.Tracker.update(Logflare.Tracker, self(), type, Node.self(), sources)
   end
@@ -138,22 +100,10 @@ defmodule Logflare.Tracker.SourceNodeMetrics do
       Node.self(),
       %{}
     )
-
-    Logflare.Tracker.track(
-      Logflare.Tracker,
-      self(),
-      "rates",
-      Node.self(),
-      %{}
-    )
   end
 
   defp check_buffers() do
     Process.send_after(self(), :check_buffers, @check_buffers_every)
-  end
-
-  defp check_rates() do
-    Process.send_after(self(), :check_rates, @check_rates_every)
   end
 
   defp check_total_inserts() do
