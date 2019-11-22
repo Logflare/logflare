4d8acfdac5415bf18eb25195cd4809ac915685f7
ontofractal <ontofractal@protonmail.com>
2019-11-22 19:58:12 +0200

    improve log events agg search bar chart

--- assets/js/source_log_chart.jsx
+++ assets/js/source_log_chart.jsx
@@ -1,6 +1,6 @@
 import * as _ from "lodash"
 import React from "react"
-import { Bar } from "@nivo/bar"
+import { ResponsiveBar } from "@nivo/bar"
 
 const brandLightBlack = "#1d1d1d"
 const brandGray = "#9a9a9a"
@@ -22,38 +22,37 @@ const theme = {
 }
 
 const LogEventsChart = ({ data }) => {
-  const width = innerWidth * 0.9
-  return <Bar
-    data={data}
-    width={width}
-    height={200}
-    margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
-    padding={0.3}
-    enableGridY={true}
-    indexBy={"timestamp"}
-    tooltip={(tooltipData) => {
-      const { value, color, indexValue } = tooltipData
-      return <div>
-        <strong style={{ color }}>
-          Timestamp: {indexValue}
-        </strong>
-        <br/>
-        <strong style={{ color }}>
-          Value: {value}
-        </strong>
-      </div>
-    }}
-    colors={"#5eeb8f"}
-    axisTop={null}
-    axisRight={null}
-    axisBottom={null}
-    axisLeft={null}
-    enableLabel={false}
-    animate={true}
-    motionStiffness={90}
-    motionDamping={15}
-    theme={theme}
-  />
+  return <div style={{ height: 100 }}>
+    <ResponsiveBar
+      data={data}
+      margin={{ top: 20, right: 0, bottom: 20, left: 0 }}
+      padding={0.3}
+      enableGridY={true}
+      indexBy={"timestamp"}
+      tooltip={(tooltipData) => {
+        const { value, color, indexValue } = tooltipData
+        return <div>
+          <strong style={{ color }}>
+            Timestamp: {indexValue}
+          </strong>
+          <br/>
+          <strong style={{ color }}>
+            Value: {value}
+          </strong>
+        </div>
+      }}
+      colors={"#5eeb8f"}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={null}
+      axisLeft={null}
+      enableLabel={false}
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+      theme={theme}
+    />
+  </div>
 }
 
 export { LogEventsChart }
