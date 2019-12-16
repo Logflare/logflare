61606ff7282961d2184974d5fdb171ad9ef084c6
Chase Granberry <chase@logflare.app>
2019-12-16 15:34:02 -0700

    Sign in with email

--- lib/logflare/users/users.ex
+++ lib/logflare/users/users.ex
@@ -29,4 +29,45 @@ defmodule Logflare.Users do
     %Logflare.Source{user_id: user_id} = Sources.get_by(token: source_id)
     Users.get_by_and_preload(id: user_id)
   end
+
+  def insert_or_update_user(auth_params) do
+    cond do
+      user = Repo.get_by(User, provider_uid: auth_params.provider_uid) ->
+        update_user_by_provider_id(user, auth_params)
+
+      user = Repo.get_by(User, email: auth_params.email) ->
+        update_user_by_email(user, auth_params)
+
+      true ->
+        api_key = :crypto.strong_rand_bytes(12) |> Base.url_encode64() |> binary_part(0, 12)
+        auth_params = Map.put(auth_params, :api_key, api_key)
+
+        changeset = User.changeset(%User{}, auth_params)
+        Repo.insert(changeset)
+    end
+  end
+
+  defp update_user_by_email(user, auth_params) do
+    updated_changeset = User.changeset(user, auth_params)
+
+    case Repo.update(updated_changeset) do
+      {:ok, user} ->
+        {:ok_found_user, user}
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  defp update_user_by_provider_id(user, auth_params) do
+    updated_changeset = User.changeset(user, auth_params)
+
+    case Repo.update(updated_changeset) do
+      {:ok, user} ->
+        {:ok_found_user, user}
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
 end
---
+++ lib/logflare_web/views/auth/email_view.ex
@@ -0,0 +1,3 @@
+defmodule LogflareWeb.Auth.EmailView do
+  use LogflareWeb, :view
+end
