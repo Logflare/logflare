c887df536170648c9bd4112b7761428c9b8ca9a8
ontofractal <ontofractal@protonmail.com>
2020-01-16 17:33:15 +0200

    add Ecto.Term for generic erl terms, use it for rule :lql_filters field

--- lib/logflare/sources/rule.ex
+++ lib/logflare/sources/rule.ex
@@ -2,6 +2,7 @@ defmodule Logflare.Rule do
   @moduledoc false
   use TypedEctoSchema
   alias Logflare.Source
+  alias Logflare.Sources
   alias Logflare.Lql
   import Ecto.Changeset
 
@@ -9,7 +10,7 @@ defmodule Logflare.Rule do
     field :regex, :string
     field :regex_struct, Ecto.Regex
     field :sink, Ecto.UUID.Atom
-    embeds_many :lql_filters, Lql.FilterRule
+    field :lql_filters, Ecto.Term, default: []
     field :lql_string, :string
     # TODO update sink field to be an belongs_to association
     # belongs_to :sink, Source, foreign_key: :sink_id, type: Ecto.UUID.Atom, references: :token
@@ -22,9 +23,23 @@ defmodule Logflare.Rule do
   def changeset(rule, attrs \\ %{}) do
     rule
     |> cast(attrs, [:sink, :lql_string])
-    |> cast_embed(:lql_filters)
+    |> parse_lql_string(rule.source_id)
     |> validate_required([:sink, :lql_filters, :lql_string])
     |> validate_regex_is_read_only()
+  end
+
+  def parse_lql_string(changeset, source_id) do
+    source = Sources.get_by_and_preload(id: source_id)
+      |> Sources.put_bq_table_data()
+
+    lql_string = get_change(changeset, :lql_string)
+
+    if lql_string do
+      {:ok, lql_filters} = Lql.Parser.parse(lql_string, source.bq_table_schema)
+      put_change(changeset, :lql_filters, lql_filters)
+    else
+      changeset
+    end
   end
 
   def regex_to_lql_upgrade_changeset(rule) do
@@ -32,9 +47,7 @@ defmodule Logflare.Rule do
       rule
       |> cast(
         %{lql_filters: [Lql.Utils.build_message_filter_rule_from_regex(rule.regex)]},
-        [
-          :lql_filters
-        ]
+        [ :lql_filters ]
       )
     else
       cast(rule, %{}, [])
