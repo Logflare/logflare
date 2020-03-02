37eddbdd0a8d895aaf064f27f2ebc75ad80acbba
ontofractal <ontofractal@protonmail.com>
2020-03-02 17:09:21 +0200

    minor schema builder type generation bugfix

--- lib/logflare/google/bigquery/schema_utils/schema_types.ex
+++ lib/logflare/google/bigquery/schema_utils/schema_types.ex
@@ -6,7 +6,6 @@ defmodule Logflare.BigQuery.SchemaTypes do
   def to_schema_type(value) when is_integer(value), do: "INTEGER"
   def to_schema_type(value) when is_binary(value), do: "STRING"
   def to_schema_type(value) when is_boolean(value), do: "BOOLEAN"
-  def to_schema_type(value) when is_list(value) and is_map(hd(value)), do: "ARRAY"
   def to_schema_type(value) when is_list(value), do: {"ARRAY", to_schema_type(hd(value))}
   def to_schema_type(value) when is_float(value), do: "FLOAT"
 
