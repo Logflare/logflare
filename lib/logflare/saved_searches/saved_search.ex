31f0f210be291ae7942a50ba6a847544e755d2fd
ontofractal <ontofractal@protonmail.com>
2020-04-01 14:14:28 +0300

    update saved searches context and schema to support counters

--- lib/logflare/saved_searches/saved_search.ex
+++ lib/logflare/saved_searches/saved_search.ex
@@ -5,6 +5,10 @@ defmodule Logflare.SavedSearch do
 
   typed_schema "saved_searches" do
     field :querystring, :string
+    field :saved_by_user, :boolean
+    field :count_tailing, :integer
+    field :count_non_tailing, :integer
+    field :lql, {:array, :map}
     belongs_to :source, Source
 
     timestamps()
@@ -12,8 +16,8 @@ defmodule Logflare.SavedSearch do
 
   def changeset(saved_search, attrs \\ %{}) do
     saved_search
-    |> cast(attrs, [:querystring])
-    |> validate_required([:querystring])
+    |> cast(attrs, [:querystring, :lql, :saved_by_user])
+    |> validate_required([:querystring, :lql])
     |> unique_constraint(:querystring, name: :saved_searches_querystring_source_id_index)
   end
 end
--- lib/logflare/saved_searches/saved_searches.ex
+++ lib/logflare/saved_searches/saved_searches.ex
@@ -1,16 +1,48 @@
 defmodule Logflare.SavedSearches do
+  @moduledoc false
+  import Ecto.Query
   alias Logflare.{SavedSearch, Repo}
+  alias Logflare.Source
+  alias Logflare.Lql.{FilterRule, ChartRule}
 
-  def insert(querystring, source) do
-    changeset =
-      source
-      |> Ecto.build_assoc(:saved_searches)
-      |> SavedSearch.changeset(%{querystring: querystring})
-
-    Repo.insert(changeset)
+  @spec insert(String.t(), [FilterRule.t() | ChartRule.t()], Source.t()) :: :ok | {:error, term}
+  def insert(querystring, lql_rules, %Source{} = source) do
+    source
+    |> Ecto.build_assoc(:saved_searches)
+    |> SavedSearch.changeset(%{querystring: querystring, lql: lql_rules})
+    |> Repo.insert()
   end
 
   def delete(search) do
     Repo.delete(search)
+  end
+
+  def save_by_user(querystring, lql_rules, source) do
+    search = get_by(querystring, source)
+
+    if search do
+      Repo.update(search, saved_by_user: true)
+    else
+      insert(querystring, lql_rules, source)
+    end
+  end
+
+  def inc(search_id, tailing?: tailing?) do
+    search = Repo.get(SavedSearch, search_id)
+
+    cond do
+      search && tailing? ->
+        Repo.update(search, update: [inc: :count_tailing])
+
+      search ->
+        Repo.update(search, update: [inc: :count_non_tailing])
+    end
+  end
+
+  def get_by(querystring, source) do
+    SavedSearch
+    |> where([s], s.querystring == ^querystring)
+    |> where([s], s.source_id == ^source.id)
+    |> Repo.one()
   end
 end
