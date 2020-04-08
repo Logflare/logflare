9bd033ff4649a6c42c530a1421fdce6eeb897906
Chase Granberry <chase@logflare.app>
2020-04-08 10:04:52 -0700

    Working pretty well now.

--- lib/logflare_web/controllers/billing_controller.ex
+++ lib/logflare_web/controllers/billing_controller.ex
@@ -8,7 +8,7 @@ defmodule LogflareWeb.BillingController do
   plug LogflareWeb.Plugs.AuthMustBeOwner
 
   @stripe_publishable_key Application.get_env(:stripity_stripe, :publishable_key)
-  @default_error_message "Try that again! If this continues please contact support."
+  @default_error_message "Something went wrong. Try that again! If this continues please contact support."
 
   def create(%{assigns: %{user: user}} = conn, _params) do
     with {:ok, customer} <- Stripe.create_customer(user),
@@ -27,14 +27,19 @@ defmodule LogflareWeb.BillingController do
     end
   end
 
+  def edit(%{assigns: %{user: %{billing_account: nil}} = user} = conn, _params) do
+    conn
+    |> put_flash(:error, "Please creaete a billing account first!")
+    |> redirect(to: Routes.user_path(conn, :edit) <> "#billing-account")
+  end
+
   def edit(%{assigns: %{user: user}} = conn, _params) do
     conn
     |> render("edit.html")
   end
 
-  def delete(%{assigns: %{user: user}} = conn, _params) do
-    with billing_account <- Billing.get_billing_account_by(user_id: user.id),
-         {:ok, _response} <- Stripe.delete_customer(billing_account.stripe_customer),
+  def delete(%{assigns: %{user: %{billing_account: billing_account}} = user} = conn, _params) do
+    with {:ok, _response} <- Stripe.delete_customer(billing_account.stripe_customer),
          {:ok, _response} <- Billing.delete_billing_account(billing_account) do
       conn
       |> put_flash(:info, "Billing account deleted!")
@@ -52,52 +57,68 @@ defmodule LogflareWeb.BillingController do
     |> render("start.html")
   end
 
-  def preview_subscription(%{assigns: %{user: user}} = conn, %{"plan_id" => plan_id}) do
+  def preview_subscription(
+        %{assigns: %{user: %{billing_account: billing_account}} = user} = conn,
+        %{
+          "plan_id" => plan_id
+        }
+      ) do
     with plan <- Plans.get_plan!(plan_id),
-         billing_account <- Billing.get_billing_account_by(user_id: user.id),
-         true <- is_nil(billing_account.stripe_subscriptions),
+         {:error, :not_found} <- get_billing_account_subscription(billing_account),
          {:ok, session} <- Stripe.create_customer_session(billing_account, plan) do
       conn
       |> put_session(:stripe_session, session)
       |> render("confirm.html", stripe_key: @stripe_publishable_key, stripe_session: session)
     else
-      _err -> error_and_redirect(conn, @default_error_message)
-    end
-  end
+      {:ok, _subscription} ->
+        error_and_redirect(conn, "Please delete your current subscription first!")
 
-  def change_subscription(%{assigns: %{user: user}} = conn, _params) do
-    with billing_account <- Billing.get_billing_account_by(user_id: user.id),
-         false <- is_nil(billing_account.stripe_subscriptions),
-         {:ok, session} <- Stripe.create_add_credit_card_session(billing_account) do
-      conn
-      |> put_session(:stripe_session, session)
-      |> render("confirm.html", stripe_key: @stripe_publishable_key, stripe_session: session)
-    else
       _err ->
         error_and_redirect(conn, @default_error_message)
     end
   end
 
-  def unsubscribe(%{assigns: %{user: user}} = conn, _params) do
-    with billing_account <- Billing.get_billing_account_by(user_id: user.id),
-         # we only support one subscription currently
-         [subscription] <- billing_account.stripe_subscriptions["data"],
+  def change_subscription(
+        %{assigns: %{user: %{billing_account: billing_account}} = user} = conn,
+        _params
+      ) do
+    with {:ok, subscription} <- get_billing_account_subscription(billing_account),
+         {:ok, session} <- Stripe.create_add_credit_card_session(billing_account) do
+      conn
+      |> put_session(:stripe_session, session)
+      |> render("confirm.html", stripe_key: @stripe_publishable_key, stripe_session: session)
+    else
+      {:error, :not_found} ->
+        error_and_redirect(conn, "Please subscribe first!")
+
+      _err ->
+        error_and_redirect(conn, @default_error_message)
+    end
+  end
+
+  def unsubscribe(%{assigns: %{user: %{billing_account: billing_account}} = user} = conn, _params) do
+    with {:ok, subscription} <- get_billing_account_subscription(billing_account),
          {:ok, _response} <- Stripe.delete_subscription(subscription["id"]),
          {:ok, _billing_account} <-
            Billing.update_billing_account(billing_account, %{stripe_subscriptions: nil}) do
       success_and_redirect(conn, "Subscription deleted!")
     else
-      _err -> error_and_redirect(conn)
+      {:error, :not_found} ->
+        error_and_redirect(conn, "Subscription not found.")
+
+      _err ->
+        error_and_redirect(conn)
     end
   end
 
-  def update_credit_card_success(%{assigns: %{user: user}} = conn, _params) do
+  def update_credit_card_success(
+        %{assigns: %{user: %{billing_account: billing_account}} = user} = conn,
+        _params
+      ) do
     stripe_session = get_session(conn, :stripe_session)
     billing_params = %{latest_successful_stripe_session: stripe_session}
 
-    with billing_account <-
-           Billing.get_billing_account_by(user_id: user.id),
-         {:ok, stripe_session} <-
+    with {:ok, stripe_session} <-
            Stripe.find_completed_session(stripe_session.id),
          {:ok, setup_intent} <-
            Stripe.get_setup_intent(stripe_session.setup_intent),
@@ -105,8 +126,8 @@ defmodule LogflareWeb.BillingController do
            Stripe.update_customer(billing_account.stripe_customer, %{
              invoice_settings: %{default_payment_method: setup_intent.payment_method}
            }),
-         [subscription] <-
-           billing_account.stripe_subscriptions["data"],
+         {:ok, subscription} <-
+           get_billing_account_subscription(billing_account),
          {:ok, _response} <-
            Stripe.update_subscription(subscription["id"], %{
              default_payment_method: setup_intent.payment_method
@@ -120,13 +141,11 @@ defmodule LogflareWeb.BillingController do
     end
   end
 
-  def success(%{assigns: %{user: user}} = conn, _params) do
+  def success(%{assigns: %{user: %{billing_account: billing_account}} = user} = conn, _params) do
     stripe_session = get_session(conn, :stripe_session)
     billing_params = %{latest_successful_stripe_session: stripe_session}
 
-    with billing_account <-
-           Billing.get_billing_account_by(user_id: user.id),
-         {:ok, stripe_session} <-
+    with {:ok, stripe_session} <-
            Stripe.find_completed_session(stripe_session.id),
          {:ok, _billing_account} <-
            Billing.sync_billing_account(billing_account, billing_params) do
@@ -151,10 +170,21 @@ defmodule LogflareWeb.BillingController do
 
   defp error_and_redirect(conn, message \\ @default_error_message) do
     conn
-    |> put_flash(
-      :error,
-      "Something went wrong. #{message}"
-    )
+    |> put_flash(:error, message)
     |> redirect(to: Routes.billing_path(conn, :edit))
+  end
+
+  defp get_billing_account_subscription(billing_account) do
+    # we only support one subscription currently
+    case billing_account.stripe_subscriptions["data"] do
+      nil ->
+        {:error, :not_found}
+
+      [subscription] ->
+        {:ok, subscription}
+
+      _else ->
+        {:error, :uknown_error}
+    end
   end
 end
