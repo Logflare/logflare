c0cebf67b0d7d88b577c7e403972d3a52799fb90
Chase Granberry <chase@logflare.app>
2020-04-10 08:44:06 -0700

    Update metadata and timestamp refs to m. and t:

--- lib/logflare_web/live/search/logs_search_utils.ex
+++ lib/logflare_web/live/search/logs_search_utils.ex
@@ -16,12 +16,12 @@ defmodule Logflare.Logs.Search.Utils do
   def gen_search_tip() do
     tips = [
       "Search is case sensitive.",
-      "Exact match an integer (e.g. `metadata.response.status_code:500`).",
-      "Integers support greater and less than symobols (e.g. `metadata.response.origin_time:<1000`).",
-      ~s|Exact match a string in a field (e.g. `metadata.response.cf-ray:"505c16f9a752cec8-IAD"`).|,
-      "Timestamps support greater and less than symbols (e.g. `timestamp:>=2019-07-01`).",
-      ~s|Match a field with regex (e.g. `metadata.browser:~"Firefox 5\\d"`).|,
-      "Search between times with multiple fields (e.g. `timestamp:>=2019-07-01 timestamp:<=2019-07-02`).",
+      "Exact match an integer (e.g. `m.response.status_code:500`).",
+      "Integers support greater and less than symobols (e.g. `m.response.origin_time:<1000`).",
+      ~s|Exact match a string in a field (e.g. `m.response.cf-ray:"505c16f9a752cec8-IAD"`).|,
+      "Timestamps support greater and less than symbols (e.g. `t:>=2019-07-01`).",
+      ~s|Match a field with regex (e.g. `m.browser:~"Firefox 5\\d"`).|,
+      "Search between times with multiple fields (e.g. `t:>=2019-07-01 t:<=2019-07-02`).",
       "Default behavoir is to search the log message field (e.g. `error`).",
       "Turn off Live Search to search the full history of this source."
     ]
