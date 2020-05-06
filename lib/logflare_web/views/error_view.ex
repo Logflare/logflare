244d27af1dae524e75237350d8a1f1e4857e4224
ontofractal <v@ontofractal.com>
2020-05-06 15:30:40 +0300

    add phoenix error pages

--- lib/logflare_web/views/error_view.ex
+++ lib/logflare_web/views/error_view.ex
@@ -1,15 +1,22 @@
 defmodule LogflareWeb.ErrorView do
   use LogflareWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
+  def render("401.html", assigns) do
+    render "401_page.html", assigns
+  end
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
+  def render("403.html", assigns) do
+    render "403_page.html", assigns
+  end
+
+  def render("404.html", assigns) do
+    render "404_page.html", assigns
+  end
+
+  def render("500.html", assigns) do
+    render "500_page.html", assigns
+  end
+
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
