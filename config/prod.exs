f432b9e2702df5c9c33a9c450e698925ab515b40
chasers <chase@logflare.app>
2020-05-06 07:55:56 -0700

    New IP for new PG DB

--- config/prod.exs
+++ config/prod.exs
@@ -28,7 +28,7 @@ config :logflare, Logflare.Repo,
   timeout: 30_000,
   queue_target: 5_000,
   database: "logflare",
-  hostname: "10.9.240.17"
+  hostname: "10.9.240.20"
 
 config :logflare, Logflare.Google,
   # gcloud services enable cloudbuild.googleapis.com container.googleapis.com dataproc.googleapis.com redis.googleapis.com cloudfunctions.googleapis.com run.googleapis.com servicenetworking.googleapis.com sourcerepo.googleapis.com
