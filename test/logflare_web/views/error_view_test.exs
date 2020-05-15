41389f38f033ff7c2217ade6fc811209faa9a893
ontofractal <v@ontofractal.com>
2020-05-15 16:30:23 +0300

    minor test updates

--- test/logflare_web/views/error_view_test.exs
+++ test/logflare_web/views/error_view_test.exs
@@ -1,7 +1,6 @@
 defmodule LogflareWeb.ErrorViewTest do
   use LogflareWeb.ConnCase, async: true
   alias LogflareWeb.ErrorView
-  @endpint LogflareWeb.Endpoint
 
   import Phoenix.View
 
@@ -13,10 +12,5 @@ defmodule LogflareWeb.ErrorViewTest do
   test "renders 401.html", %{conn: conn} do
     assert render_to_string(ErrorView, "404_page.html", conn: conn) =~ "404"
     assert render_to_string(ErrorView, "404_page.html", conn: conn) =~ "not found"
-  end
-
-  test "renders 500.html", %{conn: conn} do
-    assert render_to_string(ErrorView, "500_page.html", conn: conn) == "500"
-    assert render_to_string(ErrorView, "500_page.html", conn: conn) == "Server Error"
   end
 end
