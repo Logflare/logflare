74f232c56036a2d56f188fcceaa8ee736cbbc38c
chasers <chase@logflare.app>
2020-09-08 15:54:49 -0700

    Broadcast rates on RCS boot

--- lib/logflare/source/rate_counter_server.ex
+++ lib/logflare/source/rate_counter_server.ex
@@ -63,6 +63,9 @@ defmodule Logflare.Source.RateCounterServer do
     bigquery_project_id = GenUtils.get_project_id(source_id)
     init_counters(source_id, bigquery_project_id)
 
+    get_data_from_ets(source_id)
+    |> broadcast()
+
     {:noreply, source_id}
   end
 
