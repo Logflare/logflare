235a11937d1d2372259f5c22897cee61eb1de3d0
chasers <chase@logflare.app>
2020-09-14 14:04:00 -0700

    Put back custom title for source views

--- lib/logflare_web/views/helpers/page_title.ex
+++ lib/logflare_web/views/helpers/page_title.ex
@@ -2,22 +2,22 @@ defmodule LogflareWeb.Helpers.PageTitle do
   @moduledoc """
   Sets page titles.
   """
-  alias LogflareWeb.{SourceView}
+  alias LogflareWeb.LayoutView
 
   # alias LogflareWeb.{SourceView, MarketingView}
 
   @suffix "Logflare | Cloudflare, Vercel & Elixir Logging"
 
-  def page_title(assigns), do: assigns |> get |> put_suffix
+  def page_title(conn), do: conn |> get |> put_suffix
 
   defp put_suffix(nil), do: @suffix
   defp put_suffix(title), do: title <> " | " <> @suffix
 
   # defp get(%{view_module: MarketingView}), do: "Works"
 
-  defp get(%{view_module: SourceView, view_template: "show.html", source: source}) do
+  defp get(%{view_module: LayoutView, view_template: "root.html", source: source}) do
     source.name
   end
 
-  defp get(_), do: nil
+  defp get(_conn), do: nil
 end
