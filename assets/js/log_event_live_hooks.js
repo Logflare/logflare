628b50660123340d044a3e99aa6f4af421343f93
ontofractal <v@ontofractal.com>
2020-09-30 22:25:49 +0300

    add copy metadata json nav link to log event viewer

--- assets/js/log_event_live_hooks.js
+++ assets/js/log_event_live_hooks.js
@@ -1,9 +1,17 @@
 import JSONFormatter from "json-formatter-js"
+import {activateClipboardForSelector} from "./utils"
+import $ from "jquery"
 
 let hooks = {}
 
 hooks.MetadataJsonViewer = {
   mounted() {
+    $(".logflare-tooltip").tooltip({delay: {show: 100, hide: 200}})
+    activateClipboardForSelector("#copy-metadata-raw", {
+      text: () => {
+        return $("#metadata-raw-json-code").text()
+      },
+    })
     const json = JSON.parse(this.el.innerText)
     const formatter = new JSONFormatter(json, 3, {theme: "logflare"})
     document.getElementById("metadata-viewer").appendChild(formatter.render())
