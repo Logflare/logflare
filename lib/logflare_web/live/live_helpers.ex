4e19a4c04d0a81520490efd1db0abfa64b5c65f9
ontofractal <v@ontofractal.com>
2020-12-11 14:23:51 +0200

    add live_modal component

--- lib/logflare_web/live/live_helpers.ex
+++ lib/logflare_web/live/live_helpers.ex
@@ -1,9 +1,19 @@
 defmodule LogflareWeb.LiveHelpers do
+  @moduledoc false
   import Phoenix.LiveView.Helpers
 
   def live_modal(socket, component, opts) do
     path = Keyword.fetch!(opts, :return_to)
-    modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
+    title = Keyword.fetch!(opts, :title)
+    modal_opts = [id: :modal, return_to: path, component: component, opts: opts, title: title]
     live_component(socket, LogflareWeb.ModalComponent, modal_opts)
+  end
+
+  defmacro self_path(socket, extra \\ []) do
+    Routes.live_path(socket, __MODULE__, Enum.into(extra, socket.assign.params))
+  end
+
+  defmacro self_path(socket, action, extra) do
+    Routes.live_path(socket, __MODULE__, action, Enum.into(extra, socket.assign.params))
   end
 end
---
+++ lib/logflare_web/live/modal_component.ex
@@ -0,0 +1,41 @@
+defmodule LogflareWeb.ModalComponent do
+  use LogflareWeb, :live_component
+
+  @impl true
+  def render(assigns) do
+    ~L"""
+    <div id="<%= @id %>" class="modal fade show"
+      phx-capture-click="close"
+      phx-window-keydown="close"
+      phx-key="escape"
+      phx-target="#<%= @id %>"
+      phx-page-loading
+      style="display: block;"
+      >
+
+      <div class="modal-dialog">
+      <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title"><%= @title %> </h5>
+        <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
+      </div>
+      <div class="modal-body">
+        <div class="container">
+           <%= live_component @socket, @component, @opts %>
+        </div>
+      </div>
+      </div>
+      </div>
+    </div>
+    """
+  end
+
+  @impl true
+  def handle_event("close", _, socket) do
+    socket =
+      socket
+      |> push_patch(to: socket.assigns.return_to)
+
+    {:noreply, socket}
+  end
+end
