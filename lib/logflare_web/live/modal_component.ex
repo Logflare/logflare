41052df9d812760b2d8d8254b80af0991394c9ab
ontofractal <v@ontofractal.com>
2020-12-16 19:31:08 +0200

    minor styling fix

--- lib/logflare_web/live/modal_component.ex
+++ lib/logflare_web/live/modal_component.ex
@@ -17,9 +17,7 @@ defmodule LogflareWeb.ModalComponent do
           <div class="modal-content">
             <div class="modal-header lf-modal-header">
               <h5 class="modal-title"><%= @title %> </h5>
-              <button type="button" class="close" aria-label="Close">
-                <span> <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %> </span>
-              </button>
+              <span> <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %> </span>
             </div>
             <div class="modal-body">
               <div class="container">
