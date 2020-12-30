b4c3ec51fb8e25ba99078850391e79f1ec8e22eb
chasers <chase@logflare.app>
2020-12-30 12:44:33 -0700

    Work with and without billing_account

--- lib/logflare_web/live/billingaccount_live/edit.ex
+++ lib/logflare_web/live/billingaccount_live/edit.ex
@@ -6,13 +6,8 @@ defmodule LogflareWeb.BillingAccountLive do
   use LogflareWeb, :live_view
 
   alias LogflareWeb.BillingAccountView
-  alias Logflare.{Sources, Users, Plans}
-
-  @impl true
-  def render(assigns) do
-    IO.inspect(assigns)
-    BillingAccountView.render("edit.html", assigns)
-  end
+  alias Logflare.{Users, Plans}
+  alias LogflareWeb.Router.Helpers, as: Routes
 
   @impl true
   def mount(_params, %{"user_id" => user_id}, socket) do
@@ -21,20 +16,39 @@ defmodule LogflareWeb.BillingAccountLive do
       |> Users.preload_sources()
       |> Users.preload_billing_account()
 
-    plan = Plans.get_plan_by_user(user)
+    case user.billing_account do
+      nil ->
+        socket =
+          socket
+          |> put_flash(
+            :error,
+            "Create a billing account first!"
+          )
+          |> redirect(to: Routes.user_path(socket, :edit))
 
-    socket =
-      socket
-      |> assign(:period, "month")
-      |> assign(:plans, Plans.list_plans())
-      |> assign(:plan, plan)
-      |> assign(:user, user)
+        {:ok, socket}
 
-    {:ok, socket}
+      _billing_account ->
+        plan = Plans.get_plan_by_user(user)
+
+        socket =
+          socket
+          |> assign(:period, "month")
+          |> assign(:plans, Plans.list_plans())
+          |> assign(:plan, plan)
+          |> assign(:user, user)
+
+        {:ok, socket}
+    end
   end
 
   @impl true
   def handle_params(_params, _uri, socket) do
     {:noreply, socket}
+  end
+
+  @impl true
+  def render(assigns) do
+    BillingAccountView.render("edit.html", assigns)
   end
 end
