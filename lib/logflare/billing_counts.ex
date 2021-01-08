3cfdb5cd62636a67934e47775d52a1cb62006d66
chasers <chase@logflare.app>
2021-01-08 10:27:06 -0700

    Rendering my chart now

--- lib/logflare/billing_counts.ex
+++ lib/logflare/billing_counts.ex
@@ -7,7 +7,38 @@ defmodule Logflare.BillingCounts do
 
   import Ecto.Query, warn: false
   alias Logflare.Repo
+  alias Logflare.User
   alias Logflare.BillingCounts.BillingCount
+
+  def timeseries(%User{id: user_id}) do
+    days = :timer.hours(24 * 30)
+    end_date = DateTime.utc_now()
+    start_date = DateTime.add(end_date, -days, :millisecond)
+
+    from(c in BillingCount,
+      right_join:
+        range in fragment(
+          "select generate_series(date(?), date(?), '1 day') AS day, true as is_zero",
+          ^start_date,
+          ^end_date
+        ),
+      on: fragment("date(?)", range.day) == fragment("date(?)", c.inserted_at),
+      where: range.day >= ^start_date and range.day <= ^end_date,
+      where: c.user_id == ^user_id or range.is_zero,
+      group_by: range.day,
+      order_by: [desc: range.day],
+      select: [
+        range.day,
+        coalesce(sum(c.count), 0),
+        "Log Events"
+      ]
+    )
+    |> Repo.all()
+  end
+
+  def timeseries_to_ext(timeseries) do
+    Enum.map(timeseries, fn [x, y, z] -> [Date.to_string(DateTime.to_date(x)), y, z] end)
+  end
 
   def list_by(kv) do
     BillingCount
--- lib/logflare_web/live/billingaccount_live/chart_component.ex
+++ lib/logflare_web/live/billingaccount_live/chart_component.ex
@@ -6,7 +6,9 @@ defmodule LogflareWeb.BillingAccountLive.ChartComponent do
   use LogflareWeb, :live_component
   use Phoenix.HTML
 
-  alias Contex.{BarChart, Plot, Dataset}
+  alias Contex.{Plot, Dataset, BarChart}
+  alias Logflare.BillingCounts
+  alias Logflare.User
 
   require Logger
 
@@ -15,31 +17,19 @@ defmodule LogflareWeb.BillingAccountLive.ChartComponent do
   end
 
   def mount(socket) do
-    IO.inspect(socket)
     {:ok, socket}
   end
 
   def update(%{counter: counter} = _assigns, socket) do
     socket =
       socket
-      |> assign(
-        chart_options: %{
-          categories: 10,
-          series: 4,
-          type: :stacked,
-          orientation: :vertical,
-          show_selected: "no",
-          title: nil,
-          colour_scheme: "themed"
-        }
-      )
       |> assign(counter: counter)
-      |> make_test_data()
+      |> assign(chart_data: timeseries())
 
     socket =
       case connected?(socket) do
         true ->
-          Process.send_after(self(), {:chart_tick, counter + 1}, 100)
+          # Process.send_after(self(), {:chart_tick, counter + 1}, 100)
           assign(socket, show_chart: true)
 
         false ->
@@ -52,105 +42,44 @@ defmodule LogflareWeb.BillingAccountLive.ChartComponent do
   def render(assigns) do
     ~L"""
     <div id="billing-chart">
-      <%= basic_plot(@chart_data, @chart_options) %>
+      <%= make_chart(@chart_data) %>
     </div>
     """
   end
 
-  def basic_plot(test_data, chart_options) do
-    options = [
-      mapping: %{category_col: "Category", value_cols: chart_options.series_columns},
-      type: chart_options.type,
-      orientation: chart_options.orientation,
-      colour_palette: :default
+  def make_chart(data) do
+    options = plot_options()
+
+    content =
+      Dataset.new(data)
+      |> BarChart.new()
+      |> BarChart.data_labels(false)
+
+    Plot.new(400, 125, content)
+    |> Plot.axis_labels("", "")
+    |> Plot.titles("", "")
+    |> Plot.to_svg()
+  end
+
+  defp plot_options() do
+    [
+      axis_label_rotation: nil,
+      colour_palette: :default,
+      type: :line,
+      series: 1,
+      title: "Blah"
+      # dataset: term(),
+      # fill_scale: term(),
+      # height: term(),
+      # mapping: term(),
+      # transforms: term(),
+      # width: term(),
+      # x_scale: term(),
+      # y_scale: term()
     ]
-
-    plot_content =
-      BarChart.new(test_data, options)
-      |> BarChart.force_value_range({0, chart_options.series * 2.0})
-
-    plot =
-      Plot.new(400, 125, plot_content)
-      |> Plot.titles(chart_options.title, nil)
-
-    Plot.to_svg(plot)
   end
 
-  defp make_test_data(socket) do
-    options = socket.assigns.chart_options
-    series = options.series
-    categories = options.categories
-    counter = socket.assigns.counter
-
-    data =
-      1..categories
-      |> Enum.map(fn cat ->
-        series_data =
-          for s <- 1..series do
-            abs(1 + :math.sin((counter + cat + s) / 5.0))
-          end
-
-        ["Category #{cat}" | series_data]
-      end)
-
-    series_cols =
-      for i <- 1..series do
-        "Series #{i}"
-      end
-
-    test_data = Dataset.new(data, ["Category" | series_cols])
-
-    options = Map.put(options, :series_columns, series_cols)
-
-    assign(socket, chart_data: test_data, chart_options: options)
-  end
-
-  def make_test_data_for_preload() do
-    options = default_chart_options()
-    series = options.series
-    categories = options.categories
-    counter = 0
-
-    data =
-      1..categories
-      |> Enum.map(fn cat ->
-        series_data =
-          for s <- 1..series do
-            abs(1 + :math.sin((counter + cat + s) / 5.0))
-          end
-
-        ["Category #{cat}" | series_data]
-      end)
-
-    series_cols =
-      for i <- 1..series do
-        "Series #{i}"
-      end
-
-    Dataset.new(data, ["Category" | series_cols])
-  end
-
-  def make_options() do
-    options = default_chart_options()
-    series = options.series
-
-    series_cols =
-      for i <- 1..series do
-        "Series #{i}"
-      end
-
-    Map.put(options, :series_columns, series_cols)
-  end
-
-  defp default_chart_options() do
-    %{
-      categories: 10,
-      series: 4,
-      type: :stacked,
-      orientation: :vertical,
-      show_selected: "no",
-      title: nil,
-      colour_scheme: "themed"
-    }
+  defp timeseries() do
+    BillingCounts.timeseries(%User{id: 36}) |> BillingCounts.timeseries_to_ext() |> Enum.reverse()
   end
 end
