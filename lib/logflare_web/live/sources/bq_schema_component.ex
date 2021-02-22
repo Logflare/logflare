0d43a41bcb000a6486d8c34a05af33f71a78b8e5
ontofractal <v@ontofractal.com>
2021-02-22 14:37:28 +0200

    improve logs search schema viewer by preventing stale bq schema data

--- lib/logflare_web/live/sources/bq_schema_component.ex
+++ lib/logflare_web/live/sources/bq_schema_component.ex
@@ -1,10 +1,15 @@
 defmodule LogflareWeb.SourceBqSchemaComponent do
   use LogflareWeb, :live_component
+  use Logflare.Commons
   alias LogflareWeb.Helpers.BqSchema
-  alias Logflare.Sources
+
+  def update(assigns, socket) do
+    source_schema = SourceSchemas.get_source_schema_by(source_id: assigns.source.id)
+    {:ok, assign(socket, %{source_schema: source_schema})}
+  end
 
   @impl true
-  def render(%{source: source}) do
-    BqSchema.format_bq_schema(source.source_schema.bigquery_schema)
+  def render(%{source_schema: source_schema}) do
+    BqSchema.format_bq_schema(source_schema.bigquery_schema)
   end
 end
