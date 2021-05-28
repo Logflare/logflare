790f763720faf3ee775335e672d5e7da85db9fba
chasers <chase@logflare.app>
2021-05-28 09:49:20 -0700

    No need to do this

--- lib/logflare/sources/source_routing.ex
+++ lib/logflare/sources/source_routing.ex
@@ -70,7 +70,7 @@ defmodule Logflare.Logs.SourceRouting do
                   apply(Kernel, :==, [le_value, value])
 
                 operator == :"~" ->
-                  apply(Kernel, :=~, [le_value, ~r/#{value}/ui])
+                  apply(Kernel, :=~, [le_value, ~r/#{value}/u])
 
                 operator in [:<=, :<, :>=, :>] ->
                   apply(Kernel, operator, [le_value, value])
