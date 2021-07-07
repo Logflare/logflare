648bbcbcef45840eb8ed6f09886b908874b85eb3
Yurii Rashkovskii <yrashk@gmail.com>
2021-07-07 10:50:58 -0700

    Problem: can't build assets on Apple Silicon
    node-sass build fails:
    
    ```
    .node-gyp/16.0.0/include/node/v8-internal.h:452:38: error: no template named
    'remove_cv_t' in namespace 'std'; did you mean 'remove_cv'?
    ```
    
    Solution: upgrade node-sass and everything that's required
    
    Turns out, the version of node-sass used does not work on Node.js 16:
    
    https://stackoverflow.com/questions/67241196/error-no-template-named-remove-cv-t-in-namespace-std-did-you-mean-remove
    
    Surely, an earlier version can be used, however it may come with a bit
    more hassle since Node.js did not ship Apple Silicon binaries prior to v16:
    
    https://github.com/asdf-vm/asdf-nodejs/issues/189#issuecomment-848432339
    
    There's a pull request for ASDF to fall back to source code builds,
    but it hasn't been merged yet.
    
    https://github.com/asdf-vm/asdf-nodejs/pull/207
    
    So, to get around all of this, I've decided to upgrade node-sass
    to a version that supports Node.js 16 instead.
    
    That forced me to upgrade to Webpack 5 and update some of the plugins
    for it. So far everything seems to have worked. Had to update webpack
    watcher command line argument (https://github.com/phoenixframework/phoenix/issues/4020#issuecomment-719562055)

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :logflare, LogflareWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
