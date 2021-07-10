bf463fc0fa1a19cd43a58ccb14e36a2b7be295aa
chasers <chase@logflare.app>
2021-07-10 08:39:20 -0700

    More informative type mismatch error message

--- lib/logflare/logs/validators/bq_schema_change_validator.ex
+++ lib/logflare/logs/validators/bq_schema_change_validator.ex
@@ -2,53 +2,44 @@ defmodule Logflare.Logs.Validators.BigQuerySchemaChange do
   @moduledoc false
   alias Logflare.LogEvent, as: LE
   alias Logflare.{Source, Sources}
-  import Logflare.Google.BigQuery.SchemaUtils, only: [to_typemap: 1, to_typemap: 2]
+  alias Logflare.Source.BigQuery.SchemaBuilder
+
+  import Logflare.Google.BigQuery.SchemaUtils,
+    only: [to_typemap: 1, to_typemap: 2, bq_schema_to_flat_typemap: 1]
 
   @spec validate(LE.t()) :: :ok | {:error, String.t()}
   def validate(%LE{body: body, source: %Source{} = source}) do
     schema = Sources.Cache.get_bq_schema(source)
 
-    if valid?(body.metadata, schema) do
-      :ok
-    else
-      {:error, message()}
-    end
-  end
+    schema_flatmap =
+      schema
+      |> bq_schema_to_flat_typemap()
 
-  def valid?(nil, _), do: true
-  def valid?(_, nil), do: true
-  def valid?(m, _) when m === %{}, do: true
-
-  def valid?(metadata, existing_schema) do
-    existing_typemap = to_typemap(existing_schema, from: :bigquery_schema)
-
-    existing_metadata_typemap =
-      case existing_typemap do
-        %{metadata: %{fields: _}} -> existing_typemap.metadata.fields
-        _ -> %{}
-      end
-
-    new_metadata_typemap = to_typemap(metadata)
+    new_schema_flatmap =
+      SchemaBuilder.build_table_schema(body.metadata)
+      |> bq_schema_to_flat_typemap()
 
     try do
-      DeepMerge.deep_merge(new_metadata_typemap, existing_metadata_typemap, &resolver/3)
+      merge_flat_typemaps(schema_flatmap, new_schema_flatmap)
+      :ok
     rescue
-      _e -> false
-    else
-      _ -> true
+      e ->
+        {:error, e.message}
     end
   end
 
-  def resolver(_, original, override)
-      when (is_atom(original) or is_tuple(original)) and (is_atom(override) or is_tuple(override)) do
-    if original != override, do: raise("type_error")
-  end
+  def merge_flat_typemaps(nil, new), do: new
+  def merge_flat_typemaps(original, nil), do: original
+  def merge_flat_typemaps(_, new) when new === %{}, do: new
 
-  def resolver(_, _original, _override) do
-    DeepMerge.continue_deep_merge()
-  end
-
-  def message() do
-    "Incoming metadata is not compatible with existing schema"
+  def merge_flat_typemaps(original, new) do
+    Map.merge(original, new, fn k, v1, v2 ->
+      if v1 != v2,
+        do:
+          raise(
+            "Type error! Field `#{k}` has type of `#{v1}`. Incoming metadata has type of `#{v2}`."
+          ),
+        else: v2
+    end)
   end
 end
