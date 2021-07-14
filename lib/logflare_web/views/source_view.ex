5004492480829399355754d7b73b06e9c805d69d
chasers <chase@logflare.app>
2021-07-14 15:30:51 -0700

    Include API in subdomain of modal examples

--- lib/logflare_web/views/source_view.ex
+++ lib/logflare_web/views/source_view.ex
@@ -1,4 +1,18 @@
 defmodule LogflareWeb.SourceView do
   import LogflareWeb.Helpers.Forms
+  alias LogflareWeb.Router.Helpers, as: Routes
   use LogflareWeb, :view
+
+  def log_url(route) do
+    url = Routes.log_url(LogflareWeb.Endpoint, route) |> URI.parse()
+
+    case url do
+      %URI{authority: "logflare.app"} ->
+        Map.put(url, :authority, "api.logflare.app")
+
+      _ ->
+        url
+    end
+    |> URI.to_string()
+  end
 end
