1ec39e25b32604c4fc709ad55425812a9f16a2bd
Yurii Rashkovskii <yrashk@gmail.com>
2021-07-14 08:00:45 -0700

    Problem: General SQL Parser comes with a lot of third-party jars
    Those are open source libraries, easily available as Maven dependencies.
    
    Solution: use Maven dependencies to fetch them

--- sql/build.gradle.kts
+++ sql/build.gradle.kts
@@ -21,12 +21,21 @@ dependencies {
     implementation(platform("com.google.cloud:libraries-bom:20.7.0"))
     implementation("com.google.cloud:google-cloud-bigquery")
     implementation(fileTree("lib") { include("*.jar") })
+
     implementation(fileTree("gsp") { include("*.jar") })
+    implementation("tk.pratanumandal:expr4j:0.0.3")
     implementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    implementation("com.alibaba:fastjson:1.2.76")
+    implementation("org.asm-labs:junrar:0.8")
+    implementation("org.simpleframework:simple-xml:2.7.1")
+    implementation("jdom:jdom:1.1")
+    implementation("net.robyf:gradle-jarinjar-plugin:1.0.9")
+
     implementation(group = "com.impossibl.pgjdbc-ng", name = "pgjdbc-ng", version = "0.8.9")
     implementation(group = "com.zaxxer", name = "HikariCP", version = "4.0.3")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("ch.qos.logback:logback-core:1.2.3")
+    implementation("commons-logging:commons-logging:1.2")
 
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
