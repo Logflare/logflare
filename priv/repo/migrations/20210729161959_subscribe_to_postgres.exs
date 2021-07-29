6754430f1d5c3c66b20a8bfff7e35296f3851526
chasers <chase@logflare.app>
2021-07-29 14:16:46 -0700

    Alter migration for CloudSQL prod and staging

--- priv/repo/migrations/20210729161959_subscribe_to_postgres.exs
+++ priv/repo/migrations/20210729161959_subscribe_to_postgres.exs
@@ -4,12 +4,26 @@ defmodule Logflare.Repo.Migrations.SubscribeToPostgres do
   @disable_migration_lock true
 
   def up do
-    execute("ALTER SYSTEM SET wal_level = 'logical';")
+    # For Google Cloud SQL set cloudsql.logical_decoding to `on`
+
+    env = Application.get_env(:logflare, :env)
+
+    if env in [:dev, :test] do
+      execute("ALTER SYSTEM SET wal_level = 'logical';")
+    end
+
     execute("CREATE PUBLICATION logflare_pub FOR ALL TABLES;")
   end
 
   def down do
-    execute("ALTER SYSTEM SET wal_level = 'minimal';")
+    # For Google Cloud SQL set cloudsql.logical_decoding to `off`
+
+    env = Application.get_env(:logflare, :env)
+
+    if env in [:dev, :test] do
+      execute("ALTER SYSTEM SET wal_level = 'minimal';")
+    end
+
     execute("DROP PUBLICATION logflare_pub;")
   end
 end
