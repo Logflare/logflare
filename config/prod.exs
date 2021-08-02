d3e5cae44d0cd4f92c4d26dc563fdb3a6d781fde
chasers <chase@logflare.app>
2021-08-02 15:31:49 -0700

    Try the small postgres

--- config/prod.exs
+++ config/prod.exs
@@ -37,7 +37,7 @@ config :logflare, Logflare.Repo,
   timeout: 30_000,
   queue_target: 5_000,
   database: "logflare",
-  hostname: "10.11.144.14"
+  hostname: "10.11.144.17"
 
 config :logflare, Logflare.Google,
   # gcloud services enable cloudbuild.googleapis.com container.googleapis.com dataproc.googleapis.com redis.googleapis.com cloudfunctions.googleapis.com run.googleapis.com servicenetworking.googleapis.com sourcerepo.googleapis.com
