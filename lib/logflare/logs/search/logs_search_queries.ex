8b49b051fe900b1d51ecf29d7aabcd8e5764d928
chasers <chase@logflare.app>
2021-10-27 10:47:33 -0700

    Streaming buffer query to one minute

--- lib/logflare/logs/search/logs_search_queries.ex
+++ lib/logflare/logs/search/logs_search_queries.ex
@@ -263,12 +263,11 @@ defmodule Logflare.Logs.SearchQueries do
     |> where(in_streaming_buffer())
   end
 
-  def source_table_last_5_minutes(bq_table_id) when is_binary(bq_table_id) do
+  def source_table_last_1_minutes(bq_table_id) when is_binary(bq_table_id) do
     from(bq_table_id)
     |> select([:id, :metadata, :timestamp, :event_message])
-    |> where([t], t.timestamp >= ^Timex.shift(DateTime.utc_now(), seconds: -300))
+    |> where([t], t.timestamp >= ^Timex.shift(DateTime.utc_now(), seconds: -60))
   end
-
 
   def where_log_id(q, id) do
     where(q, [t], t.id == ^id)
