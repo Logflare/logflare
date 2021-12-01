f96b5beb1dbbace0f766f27ac74fbdbef99fb1d2
chasers <chase@logflare.app>
2021-12-01 06:18:27 -0700

    Change Endpoint caching defaults

--- priv/repo/migrations/20211130201505_configurable_endpoint_caching.exs
+++ priv/repo/migrations/20211130201505_configurable_endpoint_caching.exs
@@ -3,8 +3,8 @@ defmodule Logflare.Repo.Migrations.ConfigurableEndpointCaching do
 
   def change do
     alter table(:endpoint_queries) do
-      add :cache_duration_seconds, :integer, default: 1800
-      add :proactive_requerying_seconds, :integer, default: 1800
+      add :cache_duration_seconds, :integer, default: 3_600
+      add :proactive_requerying_seconds, :integer, default: 1_800
     end
   end
 end
