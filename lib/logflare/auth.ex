32e35e660cc59f93bdc0f5b887faa96fecf92f90
TzeYiing <ty@tzeyiing.com>
2022-05-16 16:46:54 +0100

    feat: added oauth access token generation logic and specs

--- lib/logflare/auth.ex
+++ lib/logflare/auth.ex
@@ -2,8 +2,12 @@ defmodule Logflare.Auth do
   @max_age_default 86_400
 
   alias Phoenix.Token
+  alias Logflare.User
+  alias Logflare.Teams.Team
+  alias Logflare.OauthAccessTokens.OauthAccessToken
 
   @salt Application.get_env(:logflare, LogflareWeb.Endpoint)[:secret_key_base]
+  @oauth_config Application.get_env(:logflare, ExOauth2Provider)
 
   def gen_email_token(email) when is_binary(email) do
     Token.sign(LogflareWeb.Endpoint, @salt, email)
@@ -26,5 +30,52 @@ defmodule Logflare.Auth do
   def gen_gravatar_link(email) do
     hash = :crypto.hash(:md5, String.trim(email)) |> Base.encode16(case: :lower)
     "https://www.gravatar.com/avatar/" <> hash
+  end
+
+  @doc """
+  Creates an Oauth access token with no expiry, linked to the given user or team's user.
+  """
+  @spec create_access_token(%Team{} | %User{}) :: {:ok, %OauthAccessToken{}} | {:error, term()}
+  def create_access_token(%Team{user_id: user_id}) do
+    user_id
+    |> Logflare.Users.get()
+    |> create_access_token()
+  end
+
+  def create_access_token(%User{} = user) do
+    ExOauth2Provider.AccessTokens.create_token(user, %{}, @oauth_config)
+  end
+
+  @doc """
+  Verifies a  `%OauthAccessToken{}` or string access token. If valid, returns the token's associated user.
+  """
+  @spec verify_access_token(%OauthAccessToken{} | binary()) :: {:ok, %User{}} | {:error, term()}
+  def verify_access_token(%OauthAccessToken{token: str_token}) do
+    verify_access_token(str_token)
+  end
+
+  def verify_access_token(str_token) when is_binary(str_token) do
+    case ExOauth2Provider.authenticate_token(str_token, @oauth_config) do
+      {:ok, %{resource_owner_id: user_id}} ->
+        user = Logflare.Users.get(user_id)
+        {:ok, user}
+
+      {:error, _reason} = err ->
+        err
+    end
+  end
+
+  @doc """
+  Revokes a given access token.
+  """
+  @spec revoke_access_token(%OauthAccessToken{} | binary()) :: :ok | {:error, term()}
+  def revoke_access_token(token) when is_binary(token) do
+    ExOauth2Provider.AccessTokens.get_by_token(token)
+    |> revoke_access_token()
+  end
+  def revoke_access_token(%OauthAccessToken{} = token) do
+    with {:ok, _token} = ExOauth2Provider.AccessTokens.revoke(token, @oauth_config) do
+      :ok
+    end
   end
 end
--- test/logflare/auth_test.exs
+++ test/logflare/auth_test.exs
@@ -1,35 +1,35 @@
 defmodule Logflare.AuthTest do
   use Logflare.DataCase
   alias Logflare.Auth
-  alias Logflare.Auth.ApiKey
+  alias Logflare.OauthAccessTokens.OauthAccessToken
   alias Logflare.Factory
-  alias Logflare.Teams.Team
-  alias Logflare.TeamUsers.TeamUser
   alias Logflare.User
+
   setup do
-    user  = Factory.insert(:user)
-    [user: user, team: Factory.insert(:team, user: user) ]
+    user = Factory.insert(:user)
+    [user: user, team: Factory.insert(:team, user: user)]
   end
 
   describe "api access tokens" do
     test "can create api key", %{user: user, team: team} do
-      {:ok, %ApiKey{}} = Auth.create_access_token(user)
-      {:ok, %ApiKey{}} = Auth.create_access_token(team)
+      {:ok, %OauthAccessToken{}} = Auth.create_access_token(user)
+      {:ok, %OauthAccessToken{}} = Auth.create_access_token(team)
     end
+
     test "can revoke access tokens", %{user: user} do
       key = access_token_fixture(user)
       :ok = Auth.revoke_access_token(key)
     end
-    test "verify access tokens", %{user: %{id: user_id} = user, team: %{id: team_id} = team} do
-      # api key without a team
-      key = access_token_fixture(user)
-      {:ok, %User{}, nil} = Auth.verify_access_token(key)
 
-      # api key with a team
-      key = access_token_fixture(team)
-      {:ok, %User{}, %Team{}} = Auth.verify_access_token(key)
+    test "verify access tokens", %{user: user} do
+      key = access_token_fixture(user)
+      {:ok, %User{}} = Auth.verify_access_token(key)
+
+      # string token
+      {:ok, _} = Auth.verify_access_token(key.token)
     end
   end
+
   defp access_token_fixture(user_or_team) do
     {:ok, key} = Auth.create_access_token(user_or_team)
     key
