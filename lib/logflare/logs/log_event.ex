709975d8facc4b4f484bfcfa0455cf18eae10ffb
TzeYiing <ty@tzeyiing.com>
2022-12-01 01:20:45 +0800

    chore: remove clustering keys hack

--- lib/logflare/logs/log_event.ex
+++ lib/logflare/logs/log_event.ex
@@ -125,7 +125,6 @@ defmodule Logflare.LogEvent do
         other ->
           other
       end
-      |> put_clustering_keys(source)
 
     %{
       "body" => body,
@@ -162,45 +161,6 @@ defmodule Logflare.LogEvent do
       joined_errors = inspect(v)
       "#{acc}#{k}: #{joined_errors}\n"
     end)
-  end
-
-  # hack for adding top level keys to payload
-  defp put_clustering_keys(params, source) do
-    top_level_project = Map.get(params, "project")
-
-    case source.token do
-      # dev
-      :"83e59828-b6ee-408c-92a8-c17bc523e6e0" ->
-        key = Kernel.get_in(params, ["metadata", "level"])
-
-        params |> Map.put("level", key)
-
-      # prod Postgres logs
-      :"74c7911a-4671-46b7-9c7f-440a18bc6bad" when is_nil(top_level_project) ->
-        key = Kernel.get_in(params, ["metadata", "project"])
-
-        if is_nil(key),
-          do: Logger.warn("Clustering key not found for Postgres.", error_string: inspect(params))
-
-        params |> Map.put("project", key)
-
-      # prod Cloudflare
-      :"7b5df630-a551-4c79-ae17-042650b37a3e" when is_nil(top_level_project) ->
-        host = Kernel.get_in(params, ["metadata", "request", "host"])
-
-        unless is_nil(host) do
-          project = String.split(host, ".") |> Enum.at(0)
-
-          params |> Map.put("project", project)
-        else
-          Logger.warn("Clustering key not found for Cloudflare.", error_string: inspect(params))
-
-          params
-        end
-
-      _ ->
-        params
-    end
   end
 
   @doc """
