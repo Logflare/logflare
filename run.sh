de43dc6e9d3608e3469feb9f5b398e7740fcf9ca
Filipe Cabaço <filipe@supabase.io>
2022-12-22 20:42:29 +0000

    Make output from export be silent (#1259)
    Makes the output of the export be silent

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 if [ -f /tmp/.secrets.env ]; then
     while read -r line; do
-        export $line
+        export $line > /dev/null
     done < /tmp/.secrets.env
 fi
+
 ./logflare eval Logflare.Release.migrate
 ./logflare start --sname logflare
