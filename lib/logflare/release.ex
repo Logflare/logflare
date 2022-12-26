046a26a1f971b838f71f1ce260a937e943d2c568
Filipe Cabaco <filipe@supabase.io>
2022-12-26 13:02:05 +0000

    Explicit :ssl library startup during migration
    For some reason SSL is not starting as expected, this will try to force
    that startup during the migration stage

--- lib/logflare/release.ex
+++ lib/logflare/release.ex
@@ -4,8 +4,8 @@ defmodule Logflare.Release do
 
   def migrate do
     Logger.info("Starting migration")
+    Application.ensure_all_started(:ssl)
     Application.ensure_all_started(@app)
-    Application.ensure_started(:ssl)
 
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
