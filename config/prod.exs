590edb0278b5da5c65373f7eb3e17550c91aa6b4
Filipe Cabaco <filipe@supabase.io>
2023-01-16 18:28:42 +0000

    Get Production pipeline changes ready
    Replicate the changes made in staging onto our production env.

--- config/prod.exs
+++ config/prod.exs
@@ -59,10 +59,3 @@ config :libcluster,
 config :logflare, Logflare.Tracker, pool_size: 5
 
 import_config "telemetry.exs"
-
-# if File.exists?("config/prod.secret.exs") do
-#   Logger.info("prod.secret.exs found, importing..")
-#   import_config("prod.secret.exs")
-# else
-#   Logger.warn("prod.secret.exs doesn't exist")
-# end
