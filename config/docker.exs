7c920600dfb26cdf35f9f5d11238cb88c66be893
TzeYiing <ty@tzeyiing.com>
2023-03-15 09:42:22 +0800

    chore: remove PHX_STATIC_PATH and remove unused compile time conifgs

--- config/docker.exs
+++ config/docker.exs
@@ -4,22 +4,8 @@ config :logflare, env: :dev, supabase_mode: true
 
 config :logflare, LogflareWeb.Endpoint,
   server: true,
-  http: [
-    port: System.get_env("PORT") || 4000,
-    transport_options: [
-      max_connections: 16_384,
-      num_acceptors: 100,
-      socket_opts: [{:raw, 0xFFFF, 0x0200, <<1::32-native>>}]
-    ],
-    protocol_options: [max_keepalive: 1_000],
-    compress: true
-  ],
-  live_view: [
-    signing_salt: "eVpFFmpN+OHPrilThexLilWnF+a8zBLbCtdH/OzAayShcm1B3OHOyGiadM6qOezp"
-  ],
   debug_errors: true,
   code_reloader: false,
-  check_origin: false,
   watchers: [
     node: [
       "node_modules/webpack/bin/webpack.js",
@@ -48,13 +34,3 @@ config :logger, :console,
   level: :debug
 
 config :phoenix, :stacktrace_depth, 20
-
-config :logflare, Logflare.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "logflare_docker",
-  hostname: "localhost",
-  port: 5432,
-  pool_size: 10,
-  prepare: :unnamed,
-  log: false
