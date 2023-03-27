08badbbe7f9aa12fa38e53d13a150bc502103bce
TzeYiing <ty@tzeyiing.com>
2023-03-27 22:24:39 +0800

    fix: remove alter user from migrations, shift it to conditional scope.

--- priv/setup.sql
+++ priv/setup.sql
@@ -1,2 +1,9 @@
-ALTER SYSTEM SET wal_level = 'logical'; -- for wal
-create schema if not exists analytics; -- for custom schema, cli integration
+-- for wal
+ALTER SYSTEM SET wal_level = 'logical'; 
+
+-- allow user access to replication
+-- originally from priv/repo/migrations/20210729161959_subscribe_to_postgres.exs
+ALTER USER postgres WITH REPLICATION;
+
+ -- create a custom db schema, mainly for testing cli integration
+create schema if not exists analytics;
