5c1e7824c0ef0e7033949c8f4c77332431c22d6d
TzeYiing <ty@tzeyiing.com>
2023-04-04 00:55:51 +0800

    fix: missing if/do

--- lib/logflare/application.ex
+++ lib/logflare/application.ex
@@ -180,12 +180,13 @@ defmodule Logflare.Application do
       Logger.info("Ensuring single tenant user is seeded...")
       SingleTenant.create_default_plan()
       SingleTenant.create_default_user()
-      SingleTenant.supabase_mode?()
-      SingleTenant.create_supabase_sources()
-      SingleTenant.create_supabase_endpoints()
-      # wait for all sources to init and create tables, takes really long
-      :timer.sleep(15_000)
-      SingleTenant.update_supabase_source_schemas()
+      if SingleTenant.supabase_mode?() do
+        SingleTenant.create_supabase_sources()
+        SingleTenant.create_supabase_endpoints()
+        # wait for all sources to init and create tables, takes really long
+        :timer.sleep(15_000)
+        SingleTenant.update_supabase_source_schemas()
+      end
     end
   end
 end
