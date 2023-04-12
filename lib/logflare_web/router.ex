f1f2c1bfdbd27ca9eb48c27442eebd4fc4b18671
TzeYiing <ty@tzeyiing.com>
2023-04-12 19:49:16 +0900

    fix: access tokens path scope

--- lib/logflare_web/router.ex
+++ lib/logflare_web/router.ex
@@ -155,6 +155,7 @@ defmodule LogflareWeb.Router do
   scope "/", LogflareWeb do
     pipe_through([:browser, :require_auth])
     get("/dashboard", SourceController, :dashboard)
+    live("/access-tokens", AccessTokensLive, :index)
   end
 
   scope "/endpoints", LogflareWeb do
@@ -235,6 +236,7 @@ defmodule LogflareWeb.Router do
     get("/", TeamUserController, :change_team)
   end
 
+
   scope "/account", LogflareWeb do
     pipe_through([:browser, :require_auth])
 
@@ -249,9 +251,6 @@ defmodule LogflareWeb.Router do
     delete("/", UserController, :delete)
     get("/edit/api-key", UserController, :new_api_key)
     put("/edit/owner", UserController, :change_owner)
-
-    # access token management
-    live("/access-tokens", AccessTokensLive, :index)
   end
 
   scope "/integrations", LogflareWeb do
