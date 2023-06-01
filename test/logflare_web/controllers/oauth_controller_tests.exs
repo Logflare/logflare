18af33584df843a3f7d719f08ec57345b8b10ed6
TzeYiing <ty@tzeyiing.com>
2023-06-02 02:22:40 +0800

    fix: oauth2 callback redirect fix

--- test/logflare_web/controllers/oauth_controller_tests.exs
+++ test/logflare_web/controllers/oauth_controller_tests.exs
@@ -12,4 +12,27 @@ defmodule LogflareWeb.OAuthControllerTests do
       assert get_flash(conn)["error"] =~ "Authentication error"
     end
   end
+
+  test "oauth2 callback redirect work correctly post-sign in", %{conn: conn} do
+    insert(:plan)
+    user = insert(:user)
+    conn = login_user(conn, user)
+
+    search =
+      URI.encode_query(%{
+        scope: "read write",
+        response_type: "code",
+        redirect_uri: "www.cloudflare.com/apps/oauth/",
+        client_id: "my-client-id",
+        # additional app metadata should be forwarded
+        "user.email": "test@logflare.app",
+        "site.name": "test.logflare.app"
+      })
+
+    uri = "/oauth/authorize?#{search}"
+    conn = get(conn, uri, %{})
+    assert html_response(conn, 302)
+    assert get_flash(conn)["error"] == nil
+    assert redirected_to(conn) =~ "www.cloudflare.com"
+  end
 end
