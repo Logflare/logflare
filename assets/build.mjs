2dd51d26df7bb62c49d9b6dfc1cbd38c326855fe
TzeYiing <ty@tzeyiing.com>
2023-06-20 16:09:51 +0800

    chore: improve file watching for assets building

--- assets/build.mjs
+++ assets/build.mjs
@@ -1,15 +1,17 @@
-import * as esbuild from "esbuild"
-import {sassPlugin} from "esbuild-sass-plugin"
-import * as path from "path"
-import {copy} from "esbuild-plugin-copy"
-import postcss from "postcss"
-import autoprefixer from "autoprefixer"
-import tailwindcss from "tailwindcss"
+import * as esbuild from "esbuild";
+import { sassPlugin } from "esbuild-sass-plugin";
+import * as path from "path";
+import { copy } from "esbuild-plugin-copy";
+import postcss from "postcss";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import * as chokidar from "chokidar";
+import tailwindConfig from "./tailwind.config.js";
 
-const watch = process.argv[2] ? true : false
+const watch = process.argv[2] ? true : false;
 
 if (watch) {
-  console.log("[ESBUILD] Initiating watch mode...")
+  console.log("[ESBUILD] Initiating watch mode...");
 }
 
 let externalizeCssImages = {
@@ -18,14 +20,14 @@ let externalizeCssImages = {
     // Intercept import paths called "env" so esbuild doesn't attempt
     // to map them to a file system location. Tag them with the "env-ns"
     // namespace to reserve them for this plugin.
-    build.onResolve({filter: /\.png$/}, (args) => {
+    build.onResolve({ filter: /\.png$/ }, (args) => {
       return {
         path: args.path,
         external: true,
-      }
-    })
+      };
+    });
   },
-}
+};
 let copyStatic = copy({
   // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
   // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
@@ -42,26 +44,37 @@ let copyStatic = copy({
       to: ["../priv/static/css"],
     },
   ],
-})
+});
 
 let sassPostcssPlugin = sassPlugin({
   async transform(source, resolveDir) {
-    const {css} = await postcss([autoprefixer, tailwindcss]).process(source)
-    return css
+    const { css } = await postcss([autoprefixer, tailwindcss]).process(source);
+    return css;
   },
-})
+});
 
-await esbuild.build({
-  logLevel: "info",
-  watch,
-  entryPoints: ["js/app.js"],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  loader: {".svg": "file", ".png": "file"},
-  outfile: "../priv/static/js/app.js",
-  plugins: [sassPostcssPlugin, externalizeCssImages, copyStatic],
-  jsx: "automatic",
-  treeShaking: true,
-  nodePaths: ["node_modules"],
-})
+const build = async () => {
+  await esbuild.build({
+    logLevel: "info",
+    entryPoints: ["js/app.js"],
+    bundle: true,
+    minify: watch ? false : true,
+    sourcemap: true,
+    loader: { ".svg": "file", ".png": "file" },
+    outfile: "../priv/static/js/app.js",
+    plugins: [sassPostcssPlugin, externalizeCssImages, copyStatic],
+    jsx: "automatic",
+    treeShaking: watch ? false : true,
+    nodePaths: ["node_modules"],
+  });
+};
+
+console.log(`[ESBUILD] Running build...`)
+await build();
+if (watch) {
+  console.log('[CHOKIDAR] Watching content: ', tailwindConfig.content)
+  await chokidar.watch(tailwindConfig.content).on("change", async (event, path) => {
+    console.log(`[CHOKIDAR] File change detected, triggering rebuild...`)
+    await build();
+  });
+}
