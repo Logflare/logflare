c5acd6227c463fd50577d3bc5752d4bc73a05c32
TzeYiing <ty@tzeyiing.com>
2023-07-13 04:21:43 +0800

    chore: formatting

--- config/runtime.exs
+++ config/runtime.exs
@@ -187,7 +187,6 @@ config :stripity_stripe,
 
 if config_env() != :test do
   config :goth, json: File.read!("gcloud.json")
-
   config :grpc, port: System.get_env("LOGFLARE_GRPC_PORT", "50051") |> String.to_integer()
 end
 
