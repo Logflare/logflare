1b66983fbb6f7a8d4f924e4310153ccdb1fabf19
TzeYiing <ty@tzeyiing.com>
2023-07-14 13:18:51 +0800

    fix: add adaptor genserver name

--- lib/logflare/backends/adaptor/postgres_adaptor.ex
+++ lib/logflare/backends/adaptor/postgres_adaptor.ex
@@ -28,7 +28,9 @@ defmodule Logflare.Backends.Adaptor.PostgresAdaptor do
   end
 
   def start_link(%SourceBackend{} = source_backend) do
-    GenServer.start_link(__MODULE__, source_backend)
+    GenServer.start_link(__MODULE__, source_backend,
+      name: Backends.via_source_backend(source_backend, __MODULE__)
+    )
   end
 
   @impl true
