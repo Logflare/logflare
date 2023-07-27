4625f6f2946d39c4f1675804c8c21043eeb39fda
TzeYiing <ty@tzeyiing.com>
2023-07-27 19:18:33 +0800

    chore: fix inline migration docs

--- priv/repo/migrations/20230727111150_update_endpoint_query_language_default_value.exs
+++ priv/repo/migrations/20230727111150_update_endpoint_query_language_default_value.exs
@@ -2,7 +2,7 @@ defmodule Logflare.Repo.Migrations.UpdateEndpointQueryLanguageDefaultValue do
   use Ecto.Migration
 
   def up do
-    # set all current values
+    # set all current existing endpoints
     Logflare.Repo.update_all("endpoint_queries", set: [language: :bq_sql])
   end
 end
