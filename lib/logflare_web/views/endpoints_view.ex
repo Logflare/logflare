a48e3a60145158d726d5207eaa691b6410750161
TzeYiing <ty@tzeyiing.com>
2023-08-05 23:27:23 +0800

    chore: remove unused code

--- lib/logflare_web/views/endpoints_view.ex
+++ lib/logflare_web/views/endpoints_view.ex
@@ -2,17 +2,6 @@ defmodule LogflareWeb.EndpointsView do
   use LogflareWeb, :view
   alias Logflare.SingleTenant
   def render("query.json", %{result: data}) do
-  # if SingleTenant.supabase_mode?() and SingleTenant.postgres_backend?() do
-  #   data = Enum.map(fn
-  #     %{"event_message"=> _, "timestamp"=> ts, "id"=> _} = datum->
-  #       %{datum | "timestamp" => }
-  #     datum ->datum
-  #   end)
-  #   %{result: data}
-
-  # else
-  #   %{result: data}
-  # end
     %{result: data}
   end
 
