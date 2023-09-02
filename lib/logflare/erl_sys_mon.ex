02449c96d19a0033e676b195d6135f9b867f9411
Chase Granberry <chase@logflare.app>
2023-09-02 09:04:54 -0700

    fix: log Erlang system monitor events

---
+++ lib/logflare/erl_sys_mon.ex
@@ -0,0 +1,26 @@
+defmodule Logflare.ErlSysMon do
+  @moduledoc """
+  Logs Erlang System Monitor events.
+  """
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(_args) do
+    :erlang.system_monitor(self(), [
+      :busy_dist_port,
+      :busy_port,
+      {:long_gc, 100},
+      {:long_schedule, 100}
+    ])
+
+    {:ok, []}
+  end
+
+  def handle_info(msg, state) do
+    Logger.warning("#{__MODULE__} message: " <> msg)
+
+    {:noreply, state}
+  end
+end
