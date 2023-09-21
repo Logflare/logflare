5bb2d40ad988b83e1f064ee219541d8aecccacb2
TzeYiing <ty@tzeyiing.com>
2023-09-21 23:29:49 +0800

    chore: remove github compilation warning

--- priv/repo/migrations/20230727111150_update_endpoint_query_language_default_value.exs
+++ priv/repo/migrations/20230727111150_update_endpoint_query_language_default_value.exs
@@ -5,7 +5,9 @@ defmodule Logflare.Repo.Migrations.UpdateEndpointQueryLanguageDefaultValue do
     execute(fn ->
       # set all current existing endpoints
       Logflare.Repo.update_all("endpoint_queries", set: [language: "bq_sql"])
-    end, fn -> end)
+    end, fn ->
+      nil
+    end)
 
     alter table("endpoint_queries") do
       modify :language, :string, null: false, from: {:string, null: true}
