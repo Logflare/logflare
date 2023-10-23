e029de0995bee3dd9c852bc16d840cc5871d047d
TzeYiing <ty@tzeyiing.com>
2023-10-24 01:17:38 +0800

    chore: remove :nimble_options

--- mix.exs
+++ mix.exs
@@ -122,7 +122,6 @@ defmodule Logflare.Mixfile do
 
       # Concurrency and pipelines
       {:broadway, "~> 1.0.6"},
-      {:nimble_options, "~>0.4.0"},
 
       # Test
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
