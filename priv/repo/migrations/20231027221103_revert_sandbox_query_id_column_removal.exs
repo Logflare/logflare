e7c8de13e241d321b7efdd3ccfdc350ce9c0255c
TzeYiing <ty@tzeyiing.com>
2023-11-02 03:28:13 +0800

    chore: update to add_if_not_exists

--- priv/repo/migrations/20231027221103_revert_sandbox_query_id_column_removal.exs
+++ priv/repo/migrations/20231027221103_revert_sandbox_query_id_column_removal.exs
@@ -1,10 +1,12 @@
 defmodule Logflare.Repo.Migrations.RevertSandboxQueryIdColumnRemoval do
   use Ecto.Migration
 
-  def change do
-
+  def up do
     alter table(:endpoint_queries) do
-      add :sandbox_query_id, references(:endpoint_queries, on_delete: :nothing)
+      add_if_not_exists :sandbox_query_id, references(:endpoint_queries, on_delete: :nothing)
     end
+  end
+  def down do
+
   end
 end
