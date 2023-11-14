5b0008130f355908c110d33e7f1db7da1b1da94d
TzeYiing <ty@tzeyiing.com>
2023-11-14 17:11:43 +0800

    chore: add specs, minor refactoring of Logflare.Cluster.Utils

--- lib/logflare/cluster/utils.ex
+++ lib/logflare/cluster/utils.ex
@@ -2,27 +2,31 @@ defmodule Logflare.Cluster.Utils do
   @moduledoc false
   require Logger
 
-  defp env_min_cluster_size, do: Application.get_env(:logflare, __MODULE__)[:min_cluster_size]
-
+  @spec node_list_all() :: [Node.t()]
   def node_list_all() do
     [Node.self() | Node.list()]
   end
 
+  @spec cluster_size() :: non_neg_integer()
   def cluster_size() do
-    lib_cluster_size = node_list_all() |> Enum.count()
+    lib_cluster_size = actual_cluster_size()
+    min_size = env_min_cluster_size()
 
-    if lib_cluster_size >= env_min_cluster_size() do
+    if lib_cluster_size >= min_size do
       lib_cluster_size
     else
-      Logger.warning("Cluster size is #{lib_cluster_size} but expected #{env_min_cluster_size()}",
+      Logger.warning("Cluster size is #{lib_cluster_size} but expected #{min_size}",
         cluster_size: lib_cluster_size
       )
 
-      env_min_cluster_size()
+      min_size
     end
   end
 
+  @spec actual_cluster_size() :: non_neg_integer()
   def actual_cluster_size() do
     Enum.count(node_list_all())
   end
+
+  defp env_min_cluster_size, do: Application.get_env(:logflare, __MODULE__)[:min_cluster_size]
 end
