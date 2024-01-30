73ce63f13700d1c7792ce4ee40c06e5eb23aa888
Ziinc <Ziinc@users.noreply.github.com>
2024-01-31 05:00:06 +0700

    chore: remove regex rules logic (#1943)
    * chore: remove regex-related fields from Repo.Schema

--- lib/logflare/sources/rule.ex
+++ lib/logflare/sources/rule.ex
@@ -2,17 +2,13 @@ defmodule Logflare.Rule do
   @moduledoc false
   use TypedEctoSchema
   alias Logflare.Source
-  alias Logflare.Lql
   import Ecto.Changeset
 
   typed_schema "rules" do
-    field :regex, :string
-    field :regex_struct, Ecto.Regex
+
     field :sink, Ecto.UUID.Atom
     field :lql_filters, Ecto.Term, default: []
     field :lql_string, :string
-    # TODO update sink field to be an belongs_to association
-    # belongs_to :sink, Source, foreign_key: :sink_id, type: Ecto.UUID.Atom, references: :token
     belongs_to :source, Source
 
     timestamps()
@@ -25,33 +21,6 @@ defmodule Logflare.Rule do
     |> validate_required([:sink, :lql_filters, :lql_string])
     |> validate_length(:lql_filters, min: 1)
     |> foreign_key_constraint(:source_id)
-  end
-
-  def regex_changeset(rule, attrs \\ %{}) do
-    rule
-    |> cast(attrs, [:sink, :regex, :regex_struct])
-    |> validate_required([:sink, :regex, :regex_struct])
-    |> foreign_key_constraint(:source_id)
-  end
-
-  @doc "Delete when all source rules are upgraded to LQL"
-  @spec regex_to_lql_upgrade_changeset(Rule.t()) :: Ecto.Changeset.t()
-  def regex_to_lql_upgrade_changeset(rule) do
-    if rule.regex do
-      lql_string = ~s|"#{rule.regex}"|
-      {:ok, lql_filters} = Lql.build_message_filter_from_regex(lql_string)
-
-      rule
-      |> cast(
-        %{
-          lql_filters: lql_filters,
-          lql_string: lql_string
-        },
-        [:lql_filters, :lql_string]
-      )
-    else
-      cast(rule, %{}, [])
-    end
   end
 
   def changeset_error_to_string(changeset) do
--- test/logflare_web/live_views/source_rules_lv_test.exs
+++ test/logflare_web/live_views/source_rules_lv_test.exs
@@ -4,10 +4,7 @@ defmodule LogflareWeb.Source.RulesLqlTest do
   @endpoint LogflareWeb.Endpoint
   import Phoenix.LiveViewTest
   alias Logflare.Sources
-  alias Logflare.Repo
-  # alias Logflare.Source.BigQuery.SchemaBuilder
   alias Logflare.Source.RecentLogsServer, as: RLS
-  alias Logflare.Rule
   alias Logflare.Users
   import Logflare.Factory
   alias Logflare.Billing
@@ -133,117 +130,6 @@ defmodule LogflareWeb.Source.RulesLqlTest do
         |> get("/sources/#{s.id + 1000}/rules")
 
       assert html_response(conn, 404) =~ "404"
-    end
-  end
-
-  describe "Rule regex to LQL upgrade" do
-    setup do
-      user = Users.get_by(email: System.get_env("LOGFLARE_TEST_USER_WITH_SET_IAM"))
-
-      source = params_for(:source)
-
-      {:ok, source} = Sources.create_source(source, user)
-
-      {:ok, sink} =
-        :source
-        |> params_for(name: "Sink Source 1")
-        |> Sources.create_source(user)
-
-      Repo.insert(%Rule{
-        regex: "100",
-        regex_struct: Regex.compile!("100"),
-        source_id: source.id,
-        sink: sink.token
-      })
-
-      {:ok, sink2} =
-        :source
-        |> params_for(name: "Sink Source 2")
-        |> Sources.create_source(user)
-
-      Repo.insert(%Rule{
-        regex: ~S"\d\d",
-        regex_struct: Regex.compile!(~S"\d\d"),
-        source_id: source.id,
-        sink: sink2.token
-      })
-
-      {:ok, sink3} =
-        :source
-        |> params_for(name: "Sink Source 3")
-        |> Sources.create_source(user)
-
-      Repo.insert(%Rule{
-        regex: ~S"\w+",
-        regex_struct: Regex.compile!(~S"\w+"),
-        source_id: source.id,
-        sink: sink3.token
-      })
-
-      %{sources: [source, sink, sink2, sink3], user: [user]}
-    end
-
-    @tag :failing
-    test "is successfull", %{sources: [source | _], user: [user], conn: conn} do
-      conn =
-        conn
-        |> Plug.Test.init_test_session(%{user_id: user.id})
-        |> assign(:user, user)
-        |> get("/sources/#{source.id}/rules")
-
-      assert {:ok, view, html} = live(conn)
-
-      assert html =~ ~S|id="lql-rules-container"|
-
-      assert html =~ "#has_regex_rules"
-
-      html = render_click(view, "upgrade_rules", %{})
-
-      assert [
-               %Logflare.Rule{
-                 lql_filters: [
-                   %Logflare.Lql.FilterRule{
-                     modifiers: %{quoted_string: true},
-                     operator: :string_contains,
-                     path: "event_message",
-                     value: "100",
-                     shorthand: nil,
-                     values: nil
-                   }
-                 ],
-                 lql_string: ~S|"100"|,
-                 regex: "100",
-                 regex_struct: ~r/100/
-               },
-               %Logflare.Rule{
-                 lql_filters: [
-                   %Logflare.Lql.FilterRule{
-                     modifiers: %{},
-                     operator: :string_contains,
-                     path: "event_message",
-                     value: ~S"\d\d"
-                   }
-                 ],
-                 lql_string: ~S|"\d\d"|,
-                 regex: ~S"\d\d",
-                 regex_struct: ~r/\d\d/
-               },
-               %Logflare.Rule{
-                 lql_filters: [
-                   %Logflare.Lql.FilterRule{
-                     modifiers: %{},
-                     operator: :string_contains,
-                     path: "event_message",
-                     value: ~S"\w+"
-                   }
-                 ],
-                 lql_string: ~S|"\w+"|,
-                 regex: ~S"\w+",
-                 regex_struct: ~r/\w+/
-               }
-             ] = Sources.get_by_and_preload(id: source.id).rules
-
-      refute html =~ "#has_regex_rules"
     end
   end
 end
