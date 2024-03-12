48bf2746ffbe19bb1e84e39c51c6145bb432b0fe
TzeYiing <ty@tzeyiing.com>
2024-03-12 09:02:32 +0800

    feat: add/remove of source backends, secrets redaction

--- lib/logflare_web/controllers/api/source_controller.ex
+++ lib/logflare_web/controllers/api/source_controller.ex
@@ -3,6 +3,7 @@ defmodule LogflareWeb.Api.SourceController do
   use OpenApiSpex.ControllerSpecs
 
   alias Logflare.Sources
+  alias Logflare.Backends
   alias LogflareWeb.OpenApi.Accepted
   alias LogflareWeb.OpenApi.Created
   alias LogflareWeb.OpenApi.List
@@ -35,7 +36,7 @@ defmodule LogflareWeb.Api.SourceController do
 
   def show(%{assigns: %{user: user}} = conn, %{"token" => token}) do
     with source when not is_nil(source) <- Sources.get_by(token: token, user_id: user.id),
-         [source] <- Sources.preload_for_dashboard([source]) do
+         source <- Sources.preload_defaults(source) do
       json(conn, source)
     end
   end
@@ -51,6 +52,8 @@ defmodule LogflareWeb.Api.SourceController do
 
   def create(%{assigns: %{user: user}} = conn, params) do
     with {:ok, source} <- Sources.create_source(params, user) do
+      source = Sources.preload_defaults(source)
+
       conn
       |> put_status(201)
       |> json(source)
@@ -70,6 +73,8 @@ defmodule LogflareWeb.Api.SourceController do
   def update(%{assigns: %{user: user}} = conn, %{"token" => token} = params) do
     with source when not is_nil(source) <- Sources.get_by(token: token, user_id: user.id),
          {:ok, source} <- Sources.update_source_by_user(source, params) do
+      source = Sources.preload_defaults(source)
+
       conn
       |> put_status(204)
       |> json(source)
@@ -91,6 +96,53 @@ defmodule LogflareWeb.Api.SourceController do
       conn
       |> Plug.Conn.send_resp(204, [])
       |> Plug.Conn.halt()
+    end
+  end
+
+  operation(:add_backend,
+    summary: "Add source backend",
+    parameters: [
+      source_token: [in: :path, description: "Source Token", type: :string],
+      backend_token: [in: :path, description: "Backend Token", type: :string]
+    ],
+    request_body: nil,
+    responses: %{
+      201 => Source.response(),
+      404 => NotFound.response()
+    }
+  )
+
+  def add_backend(conn, %{"source_token" => token, "backend_token" => backend_token}) do
+    with {:ok, backend} <- Backends.fetch_backend_by(token: backend_token),
+         {:ok, source} <- Sources.fetch_source_by(token: token),
+         source <- Sources.preload_backends(source),
+         {:ok, source} <- Backends.update_source_backends(source, [backend | source.backends]) do
+      conn
+      |> put_status(201)
+      |> json(source)
+    end
+  end
+
+  operation(:removebackend,
+    summary: "Remove source backend",
+    parameters: [
+      source_token: [in: :path, description: "Source Token", type: :string],
+      backend_token: [in: :path, description: "Backend Token", type: :string]
+    ],
+    responses: %{
+      200 => Source.response(),
+      404 => NotFound.response()
+    }
+  )
+
+  def remove_backend(conn, %{"source_token" => token, "backend_token" => backend_token}) do
+    with {:ok, source} <- Sources.fetch_source_by(token: token),
+         source <- Sources.preload_backends(source),
+         filtered <- Enum.filter(source.backends, &(&1.token != backend_token)),
+         {:ok, source} <- Backends.update_source_backends(source, filtered) do
+      conn
+      |> put_status(200)
+      |> json(source)
     end
   end
 end
