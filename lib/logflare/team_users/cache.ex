7d478cb094d4c16faa95d44d9e79cd005a4bba31
TzeYiing <ty@tzeyiing.com>
2024-04-05 15:05:19 +0800

    chore: fix tests

--- lib/logflare/team_users/cache.ex
+++ lib/logflare/team_users/cache.ex
@@ -1,9 +1,9 @@
 defmodule Logflare.TeamUsers.Cache do
   @moduledoc """
-  Cache for users.
+  Cache for TeamUsers.
   """
 
-  alias Logflare.Users
+  alias Logflare.TeamUsers
   alias Logflare.Utils
 
   def child_spec(_) do
@@ -17,13 +17,13 @@ defmodule Logflare.TeamUsers.Cache do
     }
   end
 
-  def get_team_user(id), do: apply_repo_fun(:get_team_user, [id])
+  def get_team_user(id), do: apply_repo_fun({:get_team_user, 1}, [id])
   def get_team_user!(id), do: apply_repo_fun(:get_team_user!, [id])
   def get_team_user_and_preload(id), do: apply_repo_fun(:get_team_user_and_preload, [id])
   def preload_defaults(team_user), do: apply_repo_fun(:preload_defaults, [team_user])
   def get_team_user_by(keyword), do: apply_repo_fun(:get_team_user_by, [keyword])
 
   defp apply_repo_fun(arg1, arg2) do
-    Logflare.ContextCache.apply_fun(Users, arg1, arg2)
+    Logflare.ContextCache.apply_fun(TeamUsers, arg1, arg2)
   end
 end
