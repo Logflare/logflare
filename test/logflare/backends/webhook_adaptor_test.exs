cc8772f9a855e22837f63d308d7f8f76ffa5e7ff
Ziinc <Ziinc@users.noreply.github.com>
2024-04-16 07:46:37 +0800

    feat: webhook adaptor batching & benchmarks (#2039)

--- test/logflare/backends/webhook_adaptor_test.exs
+++ test/logflare/backends/webhook_adaptor_test.exs
@@ -6,32 +6,36 @@ defmodule Logflare.Backends.WebhookAdaptorTest do
 
   @subject Logflare.Backends.Adaptor.WebhookAdaptor
 
-  setup do
-    user = insert(:user)
-    source = insert(:source, user: user)
+  describe "ingestion tests" do
+    setup do
+      user = insert(:user)
+      source = insert(:source, user: user)
 
-    backend =
-      insert(:backend, type: :webhook, sources: [source], config: %{url: "https://example.com"})
+      backend =
+        insert(:backend, type: :webhook, sources: [source], config: %{url: "https://example.com"})
 
-    pid = start_supervised!({@subject, {source, backend}})
-    :timer.sleep(500)
-    [pid: pid, backend: backend, source: source]
-  end
+      pid = start_supervised!({@subject, {source, backend}})
+      :timer.sleep(500)
+      [pid: pid, backend: backend, source: source]
+    end
 
-  test "ingest/2", %{pid: pid, source: source, backend: backend} do
-    this = self()
-    ref = make_ref()
+    test "ingest/2", %{pid: pid, source: source, backend: backend} do
+      this = self()
+      ref = make_ref()
 
-    @subject.Client
-    |> expect(:send, fn _ ->
-      send(this, ref)
-      %Tesla.Env{}
-    end)
+      @subject.Client
+      |> expect(:send, fn req ->
+        body = req[:body]
+        assert is_list(body)
+        send(this, ref)
+        %Tesla.Env{}
+      end)
 
-    le = build(:log_event, source: source)
+      le = build(:log_event, source: source)
 
-    assert :ok == @subject.ingest(pid, [le], source_id: source.id, backend_id: backend.id)
-    assert_receive ^ref, 2000
+      assert :ok == @subject.ingest(pid, [le], source_id: source.id, backend_id: backend.id)
+      assert_receive ^ref, 2000
+    end
   end
 
   test "cast_and_validate_config/1" do
@@ -50,6 +54,83 @@ defmodule Logflare.Backends.WebhookAdaptorTest do
         ] do
       assert %Ecto.Changeset{valid?: false} = Adaptor.cast_and_validate_config(@subject, invalid),
              "invalid: #{inspect(invalid)}"
+    end
+  end
+
+  @tag :benchmark
+  describe "benchmark" do
+    setup do
+      start_supervised!(BencheeAsync.Reporter)
+
+      @subject.Client
+      |> stub(:send, fn batch ->
+        body = batch[:body]
+        n = Enum.count(body)
+        BencheeAsync.Reporter.record(n)
+        # simulate latency
+        :timer.sleep(100)
+        %Tesla.Env{}
+      end)
+
+      user = insert(:user)
+      source = insert(:source, user_id: user.id)
+
+      backend =
+        insert(:backend, type: :webhook, sources: [source], config: %{url: "https://example.com"})
+
+      pid = start_supervised!({@subject, {source, backend}})
+
+      [backend: backend, pid: pid, source: source]
+    end
+
+    test "defaults", %{pid: pid, backend: backend, source: source} do
+      le = build(:log_event, source: source)
+      batch_1 = [le]
+
+      batch_10 =
+        for _i <- 1..10 do
+          le
+        end
+
+      batch_100 =
+        for _i <- 1..100 do
+          le
+        end
+
+      batch_250 =
+        for _i <- 1..250 do
+          le
+        end
+
+      batch_500 =
+        for _i <- 1..500 do
+          le
+        end
+
+      BencheeAsync.run(
+        %{
+          "batch-1" => fn ->
+            @subject.ingest(pid, batch_1, source_id: source.id, backend_id: backend.id)
+          end,
+          "batch-10" => fn ->
+            @subject.ingest(pid, batch_10, source_id: source.id, backend_id: backend.id)
+          end,
+          "batch-100" => fn ->
+            @subject.ingest(pid, batch_100, source_id: source.id, backend_id: backend.id)
+          end,
+          "batch-250" => fn ->
+            @subject.ingest(pid, batch_250, source_id: source.id, backend_id: backend.id)
+          end,
+          "batch-500" => fn ->
+            @subject.ingest(pid, batch_500, source_id: source.id, backend_id: backend.id)
+          end
+        },
+        time: 3,
+        warmup: 1,
+        print: [configuration: false],
+        # use extended_statistics to view units of work done
+        formatters: [{Benchee.Formatters.Console, extended_statistics: true}]
+      )
     end
   end
 end
