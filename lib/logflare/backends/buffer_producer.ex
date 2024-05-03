df519d405df780e63a308989cf3bb3a207b40ece
TzeYiing <ty@tzeyiing.com>
2024-05-03 11:39:18 +0800

    feat: tweak buffer broadcast interval

--- lib/logflare/backends/buffer_producer.ex
+++ lib/logflare/backends/buffer_producer.ex
@@ -8,7 +8,7 @@ defmodule Logflare.Backends.BufferProducer do
   alias Logflare.Source
   alias Logflare.PubSubRates
 
-  @default_broadcast_interval 2_000
+  @default_broadcast_interval 5_000
 
   def start_link(opts) when is_list(opts) do
     GenStage.start_link(__MODULE__, opts)
