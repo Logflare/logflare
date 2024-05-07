6150609ee8d69f41e663ceb571d0cb7d224b2c3c
TzeYiing <ty@tzeyiing.com>
2024-05-07 15:12:40 +0800

    chore: add tsting for delete backend with rules

--- test/logflare/backends_test.exs
+++ test/logflare/backends_test.exs
@@ -49,6 +49,14 @@ defmodule Logflare.BackendsTest do
       assert {:ok, %Backend{}} = Backends.delete_backend(backend)
       assert Backends.get_backend(backend.id) == nil
     end
+    test "delete backend with rules" do
+      user = insert(:user)
+      source = insert(:source, user: user)
+      insert(:rule, source: source)
+      backend = insert(:backend, user: user)
+      assert {:ok, %Backend{}} = Backends.delete_backend(backend)
+      assert Backends.get_backend(backend.id) == nil
+    end
 
     test "can attach multiple backends to a source", %{source: source} do
       [backend1, backend2] = insert_pair(:backend)
