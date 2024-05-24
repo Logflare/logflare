12e6333de0742d89a453262e5b9033de49079b37
Ziinc <Ziinc@users.noreply.github.com>
2024-05-24 17:07:01 +0800

    fix: add guard for empty email form submissions on email login (#2071)

---
+++ test/logflare_web/controllers/email_controller_tests.exs
@@ -0,0 +1,31 @@
+defmodule LogflareWeb.EmailControllerTests do
+  @moduledoc false
+  use LogflareWeb.ConnCase
+
+  test "GET email login", %{conn: conn} do
+    conn =
+      conn
+      |> get(~p"/auth/login/email")
+
+    assert html_response(conn, 200) =~ "email"
+    assert conn.assigns.flash["error"] == nil
+  end
+
+  test "POST email login", %{conn: conn} do
+    conn =
+      conn
+      |> post(~p"/auth/login/email", %{email: "some-email@logflare.app"})
+
+    assert html_response(conn, 302)
+    assert conn.assigns.flash["error"] == nil
+  end
+
+  test "POST email login with empty email", %{conn: conn} do
+    conn =
+      conn
+      |> post(~p"/auth/login/email", %{email: ""})
+
+    assert html_response(conn, 200)
+    assert conn.assigns.flash["error"] =~ "cannot be empty"
+  end
+end
