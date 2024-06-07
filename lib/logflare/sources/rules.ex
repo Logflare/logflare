2b528890c4fd272ab6ce048e23ecce81f5f7fa97
TzeYiing <ty@tzeyiing.com>
2024-06-07 20:22:52 +0800

    fix: use :rpc.multicall to propagate rules creation/deletion across cluster

--- lib/logflare/sources/rules.ex
+++ lib/logflare/sources/rules.ex
@@ -11,7 +11,6 @@ defmodule Logflare.Rules do
   alias Logflare.SourceSchemas
   alias Logflare.Backends.Backend
   alias Logflare.Backends.SourceSup
-  alias Logflare.Backends
 
   @doc """
   Lists rules for a given Source or Backend
@@ -38,7 +37,9 @@ defmodule Logflare.Rules do
     |> Repo.insert()
     |> case do
       {:ok, %Rule{backend_id: backend_id} = rule} = result when backend_id != nil ->
-        if Backends.source_sup_started?(rule.source_id), do: SourceSup.start_rule_child(rule)
+        # expected to fail on nodes where SourceSup is not started
+        :rpc.multicall(SourceSup, :start_rule_child, [rule])
+
         result
 
       other ->
@@ -84,7 +85,8 @@ defmodule Logflare.Rules do
   @spec delete_rule(Rule.t()) :: {:ok, Rule.t()}
   def delete_rule(rule) do
     res = Repo.delete(rule)
-    if Backends.source_sup_started?(rule.source_id), do: SourceSup.stop_rule_child(rule)
+    :rpc.multicall(SourceSup, :stop_rule_child, [rule])
+
     res
   end
 
