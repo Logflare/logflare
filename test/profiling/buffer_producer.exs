a9729021fe3efafad9c3ebf13e48b2e7732511bf
TzeYiing <ty@tzeyiing.com>
2024-07-05 14:38:52 +0800

    chore: udpate buffer_producer script

--- test/profiling/buffer_producer.exs
+++ test/profiling/buffer_producer.exs
@@ -6,7 +6,6 @@ Mimic.stub(Broadway, :push_messages, fn _, _ -> :ok end)
 
 arg1 = System.argv() |> Enum.at(0)
 source = Sources.get(:"f74e843a-e09d-42e1-b2bc-1915e75b53c5")
-# Logflare.Backends.BufferProducer.start_link(backend: nil, source: source)
 
 :timer.sleep(100)
 
@@ -19,16 +18,9 @@ batch =
 Logflare.Backends.IngestEventQueue.upsert_tid({source, nil})
 
 for _ <- 1..1000 do
-  # send(pid, {:add_to_buffer, batch})
   :ok = Logflare.Backends.IngestEventQueue.add_to_table({source, nil}, batch)
 end
 
-# Logflare.Backends.IngestEventQueue.get_table_info({source, bac})
-# :timer.sleep(5_000)
-
-# Current: 2024-06-02 bef
-# CNT    ACC (ms)    OWN (ms)
-# 37976   60007.640     195.062
-
 # 2024-07-05 after
-# 214,337
+# CNT    ACC (ms)    OWN (ms)
+# 214,375     434.933     325.246
