2f28db592912611f6e26aaa8980b8b649dd46071
TzeYiing <ty@tzeyiing.com>
2024-07-05 14:03:11 +0800

    pef: update profiling script

--- test/profiling/buffer_producer.exs
+++ test/profiling/buffer_producer.exs
@@ -6,23 +6,28 @@ Mimic.stub(Broadway, :push_messages, fn _, _ -> :ok end)
 
 arg1 = System.argv() |> Enum.at(0)
 source = Sources.get(:"f74e843a-e09d-42e1-b2bc-1915e75b53c5")
-Logflare.Backends.BufferProducer.start_link(backend_token: nil, source_token: source.token)
+# Logflare.Backends.BufferProducer.start_link(backend: nil, source: source)
 
 :timer.sleep(100)
 
+# add 1k batches of 100
 batch =
-  for _ <- 1..1000 do
-    %{id: "123", message: :something}
+  for _ <- 1..100 do
+    %Logflare.LogEvent{id: "123", message: :something}
   end
 
-for _ <- 1..600 do
+Logflare.Backends.IngestEventQueue.upsert_tid({source, nil})
+for _ <- 1..1000 do
   # send(pid, {:add_to_buffer, batch})
-  :ok = Logflare.Backends.IngestEventQueue.add_to_table(source, batch)
+  :ok = Logflare.Backends.IngestEventQueue.add_to_table({source, nil}, batch)
 end
 
-Logflare.Backends.IngestEventQueue.get_table_info(source)
-:timer.sleep(5_000)
+# Logflare.Backends.IngestEventQueue.get_table_info({source, bac})
+# :timer.sleep(5_000)
 
 # Current: 2024-06-02 bef
 # CNT    ACC (ms)    OWN (ms)
 # 37976   60007.640     195.062
+
+# 2024-07-05 after
+# 214,337
