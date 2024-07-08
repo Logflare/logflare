9f8990514a7ac8c08569e74754e470ea11dee576
TzeYiing <ty@tzeyiing.com>
2024-07-08 10:55:10 +0800

    fix: recreate missing publication tables

---
+++ priv/repo/migrations/20240708025030_recreate_publication_for_tables.exs
@@ -0,0 +1,20 @@
+defmodule Logflare.Repo.Migrations.RecreatePublicationForTables do
+  use Ecto.Migration
+  use Ecto.Migration
+
+  @publications Application.get_env(:logflare, Logflare.CacheBuster)[:publications]
+  @publication_tables Application.get_env(:logflare, Logflare.CacheBuster)[:publication_tables]
+
+  def up do
+    for p <- @publications, do: execute("DROP PUBLICATION #{p};")
+    for p <- @publications do
+      tables = Enum.join(@publication_tables, ", ")
+      execute("CREATE PUBLICATION #{p} FOR TABLE #{tables};")
+    end
+  end
+  def down do
+    for p <- @publications, do: execute("DROP PUBLICATION #{p};")
+  end
+
+
+end
