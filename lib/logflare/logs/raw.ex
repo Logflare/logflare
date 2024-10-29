85ce99781867bcd1c16d1d971f539f6589040053
Chase Granberry <chase@logflare.app>
2024-10-29 11:55:02 -0700

    fix: put JSON message if no message exists in log event

--- lib/logflare/logs/raw.ex
+++ lib/logflare/logs/raw.ex
@@ -1,5 +1,26 @@
 defmodule Logflare.Logs.Raw do
   @behaviour Logflare.Logs.Processor
 
-  def handle_batch(data, _source), do: data
+  @spec handle_batch(maybe_improper_list(), any()) :: list()
+  def handle_batch(data, _source) when is_list(data) do
+    for event <- data do
+      handle_event(event)
+    end
+  end
+
+  @spec handle_event(map()) :: map()
+  def handle_event(%{"message" => _messsage} = event) do
+    event
+  end
+
+  def handle_event(%{"event_message" => _messsage} = event) do
+    event
+  end
+
+  def handle_event(event) do
+    case Jason.encode(event) do
+      {:ok, json} -> Map.put(event, "message", json)
+      {:error, _error} -> Map.put(event, "message", "JSONDecodeError")
+    end
+  end
 end
