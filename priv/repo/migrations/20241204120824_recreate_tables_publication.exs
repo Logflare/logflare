0f39aaaad68eb7725857990c06be553c07167d2c
TzeYiing <ty@tzeyiing.com>
2024-12-05 16:40:32 +0800

    chore: correct alter publication syntax

--- priv/repo/migrations/20241204120824_recreate_tables_publication.exs
+++ priv/repo/migrations/20241204120824_recreate_tables_publication.exs
@@ -3,29 +3,17 @@ defmodule Logflare.Repo.Migrations.RecreateTablesPublication do
   use Ecto.Migration
 
   @publications Application.get_env(:logflare, Logflare.ContextCache.CacheBuster)[:publications]
-  @prev_publication_tables [
-    "billing_accounts",
-    "plans",
-    "rules",
-    "source_schemas",
-    "sources",
-    "users",
-    "backends",
-    "team_users"
-  ]
-  @new_publication_tables @prev_publication_tables ++ ["oauth_access_tokens"]
+  @table "oauth_access_tokens"
 
   def up do
     for p <- @publications do
-      tables = Enum.join(@new_publication_tables, ", ")
-      execute("ALTER PUBLICATION #{p} FOR TABLE #{tables};")
+      execute("ALTER PUBLICATION #{p} ADD TABLE #{@table};")
     end
   end
 
   def down do
     for p <- @publications do
-      tables = Enum.join(@prev_publication_tables, ", ")
-      execute("ALTER PUBLICATION #{p} FOR TABLE #{tables};")
+      execute("ALTER PUBLICATION #{p} DROP TABLE #{@table};")
     end
   end
 end
