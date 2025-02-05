916de18d00a2297d6dbc32d5b0692f95c85ee249
TzeYiing <ty@tzeyiing.com>
2025-02-05 17:36:26 +0800

    chore: fix migration

--- priv/repo/migrations/20250203131456_create_index_for_users_partners_upgraded.exs
+++ priv/repo/migrations/20250203131456_create_index_for_users_partners_upgraded.exs
@@ -2,6 +2,6 @@ defmodule Logflare.Repo.Migrations.CreateIndexForUsersPartnersUpgraded do
   use Ecto.Migration
 
   def change do
-    create index(:partner_users_upgraded,[:partner_id, :user_id, :upgraded])
+    create index(:partner_users,[:partner_id, :user_id, :upgraded])
   end
 end
