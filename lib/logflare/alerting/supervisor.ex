734ffe6b9a6a79f0ca138af9c3b5b4f190cf40d1
TzeYiing <ty@tzeyiing.com>
2025-05-06 16:03:51 +0800

    chore: remove compilation error typo

--- lib/logflare/alerting/supervisor.ex
+++ lib/logflare/alerting/supervisor.ex
@@ -31,7 +31,7 @@ defmodule Logflare.Alerting.Supervisor do
           Logger.info("Started alerts scheduler on #{inspect(Node.self())}, pid: #{inspect(pid)}")
           :syn.join(:alerting, :scheduler, pid)
 
-        {:error, {:already_started, pid}} ->m
+        {:error, {:already_started, pid}} ->
           Logger.debug("Alerts scheduler already started on #{inspect(node(pid))}")
       end
     else
