d55bb1bf4ca63cf2ea9f112cf2f495a665245762
Ziinc <Ziinc@users.noreply.github.com>
2025-05-13 18:44:41 +0800

    feat: add support for arrays for otel traces (#2395)
    * feat: add support for arrays for otel traces
    
    * chore: PR review and failing tests
    
    * chore: separate out extract_value/1 for better clarity

--- lib/logflare/logs/otel_trace.ex
+++ lib/logflare/logs/otel_trace.ex
@@ -14,7 +14,8 @@ defmodule Logflare.Logs.OtelTrace do
 
   alias Opentelemetry.Proto.Trace.V1.ResourceSpans
   alias Opentelemetry.Proto.Common.V1.KeyValue
-
+  alias Opentelemetry.Proto.Common.V1.AnyValue
+  alias Opentelemetry.Proto.Common.V1.ArrayValue
   @behaviour Logflare.Logs.Processor
 
   def handle_batch(resource_spans, _source) when is_list(resource_spans) do
@@ -95,7 +96,18 @@ defmodule Logflare.Logs.OtelTrace do
 
   defp extract_key_value(%KeyValue{key: key, value: nil}), do: {key, nil}
 
-  defp extract_key_value(%KeyValue{key: key, value: %{value: {_, value}}}) do
-    {key, value}
+  defp extract_key_value(%KeyValue{
+         key: key,
+         value: value
+       }) do
+    {key, extract_value(value)}
   end
+
+  defp extract_value(%AnyValue{value: {:array_value, %ArrayValue{values: values}}}) do
+    Enum.map(values, &extract_value/1)
+  end
+
+  defp extract_value(%_{value: {_type, value}}), do: value
+  defp extract_value(nil), do: nil
+  defp extract_value(value), do: value
 end
--- test/support/test_utils_grpc.ex
+++ test/support/test_utils_grpc.ex
@@ -3,6 +3,7 @@ defmodule Logflare.TestUtilsGrpc do
 
   alias Opentelemetry.Proto.Collector.Trace.V1.ExportTraceServiceRequest
   alias Opentelemetry.Proto.Common.V1.AnyValue
+  alias Opentelemetry.Proto.Common.V1.ArrayValue
   alias Opentelemetry.Proto.Common.V1.InstrumentationScope
   alias Opentelemetry.Proto.Common.V1.KeyValue
   alias Opentelemetry.Proto.Resource.V1.Resource
@@ -10,6 +11,7 @@ defmodule Logflare.TestUtilsGrpc do
   alias Opentelemetry.Proto.Trace.V1.Span
   alias Opentelemetry.Proto.Trace.V1.Span.Event
   alias Opentelemetry.Proto.Trace.V1.ResourceSpans
+  alias Opentelemetry.Proto.Common.V1.KeyValueList
 
   @doc """
   Generates a ExportTraceServiceRequest message which contains a Span and an Event in it
@@ -26,8 +28,10 @@ defmodule Logflare.TestUtilsGrpc do
   def random_resource_span do
     [
       %ResourceSpans{
-        resource: %Resource{},
-        scope_spans: random_scope_span()
+        resource: %Resource{
+          attributes: random_attributes()
+        },
+        scope_spans: [random_scope_span()]
       }
     ]
   end
@@ -44,7 +48,7 @@ defmodule Logflare.TestUtilsGrpc do
       spans: random_span()
     }
 
-    [scope_spans]
+    scope_spans
   end
 
   defp random_span do
@@ -56,7 +60,8 @@ defmodule Logflare.TestUtilsGrpc do
         trace_id: :crypto.strong_rand_bytes(16),
         start_time_unix_nano: DateTime.utc_now() |> DateTime.to_unix(:nanosecond),
         end_time_unix_nano: DateTime.utc_now() |> DateTime.to_unix(:nanosecond),
-        events: random_event()
+        events: random_event(),
+        attributes: random_attributes()
       }
     ]
   end
@@ -75,8 +80,108 @@ defmodule Logflare.TestUtilsGrpc do
     boolean = random_key_value(:boolean)
     integer = random_key_value(:integer)
     double = random_key_value(:double)
+    array_of_strings = random_key_value(:array_of_strings)
+    array_of_booleans = random_key_value(:array_of_booleans)
+    array_of_integers = random_key_value(:array_of_integers)
+    array_of_doubles = random_key_value(:array_of_doubles)
 
-    [string, boolean, integer, double]
+    [
+      string,
+      boolean,
+      integer,
+      double,
+      array_of_strings,
+      array_of_booleans,
+      array_of_integers,
+      array_of_doubles
+    ]
+  end
+
+  # this is not supported by traces/metrics, only logs
+  # https://github.com/open-telemetry/opentelemetry-specification/pull/2888
+  # https://github.com/open-telemetry/opentelemetry-specification/issues/376
+  defp random_key_value(:array_of_kv) do
+    %KeyValue{
+      key: "random_array_#{TestUtils.random_string()}",
+      value: %AnyValue{
+        value:
+          {:kvlist_value,
+           %KeyValueList{
+             values: [
+               random_key_value(:string),
+               random_key_value(:boolean),
+               random_key_value(:integer),
+               random_key_value(:double)
+             ]
+           }}
+      }
+    }
+  end
+
+  defp random_key_value(:array_of_strings) do
+    %KeyValue{
+      key: "random_array_#{TestUtils.random_string()}",
+      value: %AnyValue{
+        value:
+          {:array_value,
+           %ArrayValue{
+             values: [
+               random_key_value(:string).value,
+               random_key_value(:string).value,
+               random_key_value(:string).value
+             ]
+           }}
+      }
+    }
+  end
+
+  defp random_key_value(:array_of_booleans) do
+    %KeyValue{
+      key: "random_array_#{TestUtils.random_string()}",
+      value: %AnyValue{
+        value:
+          {:array_value,
+           %ArrayValue{
+             values: [
+               random_key_value(:boolean).value,
+               random_key_value(:boolean).value,
+               random_key_value(:boolean).value
+             ]
+           }}
+      }
+    }
+  end
+
+  defp random_key_value(:array_of_doubles) do
+    %KeyValue{
+      key: "random_array_#{TestUtils.random_string()}",
+      value: %AnyValue{
+        value:
+          {:array_value,
+           %ArrayValue{
+             values: [
+               random_key_value(:double).value,
+               random_key_value(:double).value
+             ]
+           }}
+      }
+    }
+  end
+
+  defp random_key_value(:array_of_integers) do
+    %KeyValue{
+      key: "random_array_#{TestUtils.random_string()}",
+      value: %AnyValue{
+        value:
+          {:array_value,
+           %ArrayValue{
+             values: [
+               random_key_value(:integer).value,
+               random_key_value(:integer).value
+             ]
+           }}
+      }
+    }
   end
 
   defp random_key_value(:string) do
