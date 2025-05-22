93658d95b800d3ec35dde7573eedae5000af93f9
Adam Mokan <amokan@gmail.com>
2025-05-22 15:09:17 -0700

    add some basic tests for query template functions

--- lib/logflare/backends/adaptor/clickhouse_adaptor/query_templates.ex
+++ lib/logflare/backends/adaptor/clickhouse_adaptor/query_templates.ex
@@ -5,18 +5,29 @@ defmodule Logflare.Backends.Adaptor.ClickhouseAdaptor.QueryTemplates do
 
   import Logflare.Guards
 
+  @default_ttl_days 90
+
   @doc """
   Generates a ClickHouse query statement to provision an ingest table for logs.
 
   ###Options
 
   - `:database` - (Optional) Will produce a fully qualified `<database>.<table>` string when provided with a value. Defaults to `nil`.
+  - `:ttl_days` - (Optional) Will add a TTL statement to the table creation query. Defaults to `90`. `nil` will disable the TTL.
 
   """
   @spec create_log_ingest_table_statement(table :: String.t(), opts :: Keyword.t()) :: String.t()
   def create_log_ingest_table_statement(table, opts \\ [])
       when is_non_empty_binary(table) and is_list(opts) do
     database = Keyword.get(opts, :database, nil)
+    ttl_days_temp = Keyword.get(opts, :ttl_days, @default_ttl_days)
+
+    ttl_days =
+      if is_pos_integer(ttl_days_temp) do
+        ttl_days_temp
+      else
+        nil
+      end
 
     db_table_string =
       if is_non_empty_binary(database) do
@@ -25,16 +36,24 @@ defmodule Logflare.Backends.Adaptor.ClickhouseAdaptor.QueryTemplates do
         "#{table}"
       end
 
-    """
-    CREATE TABLE IF NOT EXISTS #{db_table_string} (
-      "id" UUID,
-      "event_message" String,
-      "body" String,
-      "timestamp" DateTime64(6)
-    )
-    ENGINE MergeTree()
-    ORDER BY ("timestamp")
-    SETTINGS index_granularity = 8192 SETTINGS flatten_nested=0
-    """
+    Enum.join([
+      """
+      CREATE TABLE IF NOT EXISTS #{db_table_string} (
+        "id" UUID,
+        "event_message" String,
+        "body" String,
+        "timestamp" DateTime64(6)
+      )
+      ENGINE MergeTree()
+      ORDER BY ("timestamp")
+      """,
+      if is_pos_integer(ttl_days) do
+        """
+        TTL "timestamp" + INTERVAL #{ttl_days} DAY
+        """
+      end,
+      "SETTINGS index_granularity = 8192 SETTINGS flatten_nested=0"
+    ])
+    |> String.trim_trailing("\n")
   end
 end
--- test/logflare/backends/adaptor/clickhouse_adaptor/query_templates_test.exs
+++ test/logflare/backends/adaptor/clickhouse_adaptor/query_templates_test.exs
@@ -1,7 +1,36 @@
 defmodule Logflare.Backends.Adaptor.ClickhouseAdaptor.QueryTemplatesTest do
   use Logflare.DataCase, async: false
 
-  @subject Logflare.Backends.Adaptor.ClickhouseAdaptor.QueryTemplates
+  alias Logflare.Backends.Adaptor.ClickhouseAdaptor.QueryTemplates
 
-  doctest @subject
+  doctest QueryTemplates
+
+  describe "create_log_ingest_table_statement/2" do
+    test "Generates a valid statement when given a table name" do
+      table_name = "foo"
+
+      statement = QueryTemplates.create_log_ingest_table_statement(table_name)
+
+      assert statement =~ "CREATE TABLE IF NOT EXISTS #{table_name}"
+      assert statement =~ "TTL \"timestamp\" + INTERVAL 90 DAY"
+    end
+
+    test "Allows the TTL to be adjusted via opts" do
+      table_name = "foo"
+
+      statement = QueryTemplates.create_log_ingest_table_statement(table_name, ttl_days: 5)
+
+      assert statement =~ "CREATE TABLE IF NOT EXISTS #{table_name}"
+      assert statement =~ "TTL \"timestamp\" + INTERVAL 5 DAY"
+    end
+
+    test "Removes the TTL when `ttl_days` is set to nil" do
+      table_name = "foo"
+
+      statement = QueryTemplates.create_log_ingest_table_statement(table_name, ttl_days: nil)
+
+      assert statement =~ "CREATE TABLE IF NOT EXISTS #{table_name}"
+      refute statement =~ "TTL"
+    end
+  end
 end
