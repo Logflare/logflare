618211d291fe7fa6b2e3fe1ec83580687c3994b3
Adam Mokan <amokan@gmail.com>
2025-05-22 17:18:11 -0700

    unit test fix

--- test/logflare/backends/adaptor/clickhouse_adaptor/query_templates_test.exs
+++ test/logflare/backends/adaptor/clickhouse_adaptor/query_templates_test.exs
@@ -8,7 +8,6 @@ defmodule Logflare.Backends.Adaptor.ClickhouseAdaptor.QueryTemplatesTest do
   describe "create_log_ingest_table_statement/2" do
     test "Generates a valid statement when given a table name" do
       table_name = "foo"
-
       statement = QueryTemplates.create_log_ingest_table_statement(table_name)
 
       assert statement =~ "CREATE TABLE IF NOT EXISTS #{table_name}"
@@ -17,7 +16,6 @@ defmodule Logflare.Backends.Adaptor.ClickhouseAdaptor.QueryTemplatesTest do
 
     test "Allows the TTL to be adjusted via opts" do
       table_name = "foo"
-
       statement = QueryTemplates.create_log_ingest_table_statement(table_name, ttl_days: 5)
 
       assert statement =~ "CREATE TABLE IF NOT EXISTS #{table_name}"
@@ -26,7 +24,6 @@ defmodule Logflare.Backends.Adaptor.ClickhouseAdaptor.QueryTemplatesTest do
 
     test "Removes the TTL when `ttl_days` is set to nil" do
       table_name = "foo"
-
       statement = QueryTemplates.create_log_ingest_table_statement(table_name, ttl_days: nil)
 
       assert statement =~ "CREATE TABLE IF NOT EXISTS #{table_name}"
