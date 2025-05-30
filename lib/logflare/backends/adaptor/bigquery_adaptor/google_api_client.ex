5b3c7b56daccc879197070477e75797e9e583671
TzeYiing <ty@tzeyiing.com>
2025-05-30 16:58:08 +0800

    feat: correctly create ArrowData

--- lib/logflare/backends/adaptor/bigquery_adaptor/google_api_client.ex
+++ lib/logflare/backends/adaptor/bigquery_adaptor/google_api_client.ex
@@ -3,6 +3,8 @@ defmodule Logflare.Backends.Adaptor.BigQueryAdaptor.GoogleApiClient do
   alias Google.Cloud.Bigquery.Storage.V1.BigQueryWrite
   alias Google.Cloud.Bigquery.Storage.V1.AppendRowsRequest
   alias Google.Cloud.Bigquery.Storage.V1.AppendRowsRequest.ArrowData
+  alias Google.Cloud.Bigquery.Storage.V1.ArrowRecordBatch
+  alias Google.Cloud.Bigquery.Storage.V1.ArrowSchema
   require Logger
 
   def append_rows({:arrow, record_batch_emf, schema_emf}, project, dataset, table) do
@@ -16,28 +18,33 @@ defmodule Logflare.Backends.Adaptor.BigQueryAdaptor.GoogleApiClient do
     {:ok, channel} =
       GRPC.Stub.connect("bigquerystorage.googleapis.com:443",
         # adapter: GRPC.Client.Adapters.Mint,
-        # cred: cred,
+        # cred: GRPC.Credential.new([ssl: []]),
         headers: [
-# {"Content-Type", "application/x-protobuf"},
+          # {"Content-Type", "application/x-protobuf"},
           {"Authorization", "Bearer #{goth_token.token}"}
         ]
       )
 
-    request =
-    AppendRowsRequest.new(
-        write_stream: "projects/#{project}/datasets/#{dataset}/tables/#{table}/streams/_default",
-        arrow_rows: %ArrowData{
-          writer_schema: schema_emf,
-          rows: record_batch_emf
-        }
+    arrow_rows =
+      ArrowData.new(
+        writer_schema: ArrowSchema.new(serialized_schema: schema_emf),
+        rows: ArrowRecordBatch.new(serialized_record_batch: record_batch_emf)
       )
       |> dbg()
 
-    stream = channel
-    |> BigQueryWrite.Stub.append_rows()
-|> dbg()
+    request =
+      AppendRowsRequest.new(
+        write_stream: "projects/#{project}/datasets/#{dataset}/tables/#{table}/streams/_default",
+        arrow_rows: arrow_rows
+      )
+      |> dbg()
 
-    GRPC.Stub.send_request(stream, request , end_stream: true)
+    stream =
+      channel
+      |> BigQueryWrite.Stub.append_rows()
+      |> dbg()
+
+    GRPC.Stub.send_request(stream, request, end_stream: true)
     |> dbg()
 
     GRPC.Stub.recv(stream)
