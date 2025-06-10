2e35595bb66183d9c3a208b7ba315d58647e5b9b
Ziinc <Ziinc@users.noreply.github.com>
2025-06-10 16:30:10 +0800

    feat: adjust batch size buckets

--- lib/telemetry.ex
+++ lib/telemetry.ex
@@ -190,6 +190,6 @@ defmodule Logflare.Telemetry do
   end
 
   defp batch_size_reporter_opts do
-    [buckets: [0, 5, 10, 25, 50, 100, 150, 250, 350, 1000]]
+    [buckets: [0, 1, 5, 10, 50, 100, 150, 250, 500, 1000, 2000]]
   end
 end
