eb56b3ebb9fa6e87bda98593f5604eaf6e5ce00a
Adam Mokan <amokan@gmail.com>
2025-06-18 08:54:58 -0700

    set `rustler` to override as I wouldd prefer stick with latest version

--- mix.exs
+++ mix.exs
@@ -177,7 +177,7 @@ defmodule Logflare.Mixfile do
       {:floki, "~> 0.29.0", only: [:test]},
 
       # Rust NIFs
-      {:rustler, "~> 0.36.2"},
+      {:rustler, "~> 0.36.2", override: true},
 
       # Frontend
       {:phoenix_live_react, "~> 0.4"},
