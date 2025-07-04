3262d912018a293cdd9334f2541b217bfa647922
Matt Stubbs <matt@yellowrobot.com.au>
2025-07-04 10:58:15 +1000

    Avoid copying `assigns` into start_async

--- lib/logflare_web/live/log_event_live/search_log_event_viewer_component.ex
+++ lib/logflare_web/live/log_event_live/search_log_event_viewer_component.ex
@@ -12,11 +12,13 @@ defmodule LogflareWeb.Search.LogEventViewerComponent do
 
   @impl true
   def update(%{log_event: nil} = assigns, socket) do
+    params = event_params(assigns)
+
     socket =
       socket
       |> assign_defaults(assigns)
       |> start_async(:load, fn ->
-        load_event(assigns)
+        load_event(params)
       end)
 
     {:ok, socket}
@@ -34,42 +36,19 @@ defmodule LogflareWeb.Search.LogEventViewerComponent do
     %{"log-event-id" => id, "log-event-timestamp" => timestamp} = assigns.params
     d = String.to_integer(timestamp) |> DateTime.from_unix!(:microsecond) |> DateTime.to_date()
 
-    assigns = assigns |> Map.merge(%{log_event_id: id, timestamp: d})
+    params =
+      event_params(assigns)
+      |> Map.merge(%{log_event_id: id, timestamp: d})
 
     socket =
       socket
       |> assign_defaults(assigns)
       |> start_async(:load, fn ->
-        load_event(assigns)
+        load_event(params)
       end)
 
     {:ok, socket}
   end
-
-  # @impl true
-  # def update(assigns, socket) do
-  #   %{"log-event-id" => id, "log-event-timestamp" => timestamp} = assigns.params
-
-  #   d = String.to_integer(timestamp) |> Timex.from_unix(:microsecond) |> Timex.to_date()
-  #   dminus1 = Timex.shift(d, days: -1)
-  #   dplus1 = Timex.shift(d, days: +1)
-
-  #   token = assigns.source.token
-  #   le = LogEvents.Cache.get!(token, params_to_cache_key(%{uuid: id}))
-
-  #   socket =
-  #     if le do
-  #       socket
-  #       |> assign(:log_event, le)
-  #     else
-  #       start_task(uuid: id, partitions_range: [dminus1, dplus1], source: assigns.source)
-  #       socket
-  #     end
-
-  #   socket = assign_defaults(socket, assigns)
-
-  #   {:ok, socket}
-  # end
 
   def handle_async(:load, {:ok, %{} = bq_row}, socket) do
     le = LE.make_from_db(bq_row, %{source: socket.assigns.source})
@@ -102,8 +81,8 @@ defmodule LogflareWeb.Search.LogEventViewerComponent do
     {:noreply, socket}
   end
 
-  def load_event(%{log_event_id: log_id, source: source} = assigns) do
-    range = get_partitions_range(assigns)
+  def load_event(%{log_event_id: log_id, source: source} = params) do
+    range = get_partitions_range(params)
     LogEvents.fetch_event_by_id(source.token, log_id, partitions_range: range)
   end
 
@@ -147,18 +126,19 @@ defmodule LogflareWeb.Search.LogEventViewerComponent do
     |> assign(:timestamp, timestamp)
   end
 
-  defp get_partitions_range(%{source: source, user: user} = assigns) do
+  # timestamp is explicitly set, query around the range
+  defp get_partitions_range(%{timestamp: ts}) do
+    [Timex.shift(ts, days: -1), Timex.shift(ts, days: 1)]
+  end
+
+  # no timestamp is set, fallback to ttl
+  # to avoid this clause, parent should set the timestamp
+  defp get_partitions_range(%{source: source, user: user}) do
     d = Date.utc_today()
     plan = Billing.Cache.get_plan_by_user(user)
     ttl = Sources.source_ttl_to_days(source, plan)
 
     cond do
-      ts = Map.get(assigns, :timestamp) ->
-        # timestamp is explicitly set, query around the range
-        [Timex.shift(ts, days: -1), Timex.shift(ts, days: 1)]
-
-      # no timestamp is set, fallback to ttl
-      # to avoid this clause, parent should set the timestamp
       ttl <= 7 ->
         [Timex.shift(d, days: -ttl), Timex.shift(d, days: 1)]
 
@@ -167,61 +147,7 @@ defmodule LogflareWeb.Search.LogEventViewerComponent do
     end
   end
 
-  # @spec params_to_cache_key(map()) :: {String.t(), String.t()}
-  # defp params_to_cache_key(%{uuid: id}) do
-  #   {"uuid", id}
-  # end
-
-  # defp params_to_cache_key(%{path: path, value: value}) do
-  #   {path, value}
-  # end
-
-  # defp start_task(params) do
-  #   params = Enum.into(params, %{})
-  #   source = params.source
-
-  #   pid = self()
-
-  #   Task.start(fn ->
-  #     case params do
-  #       %{uuid: id} ->
-  #         LogEvents.fetch_event_by_id(source.token, id, partitions_range: params.partitions_range)
-
-  #       %{id: id} ->
-  #         LogEvents.fetch_event_by_id(source.token, id, partitions_range: params.partitions_range)
-
-  #       %{path: "uuid", value: id} ->
-  #         LogEvents.fetch_event_by_id(source.token, id, partitions_range: params.partitions_range)
-
-  #       %{path: path, value: value} ->
-  #         LogEvents.fetch_event_by_path(source.token, path, value)
-  #     end
-  #     |> case do
-  #       %{} = bq_row ->
-  #         dbg("make from db running")
-
-  #         le = LE.make_from_db(bq_row, %{source: source}) |> dbg()
-
-  #         LogEvents.Cache.put(
-  #           source.token,
-  #           params_to_cache_key(params) |> dbg(),
-  #           le
-  #         )
-
-  #         send_update(pid, __MODULE__, log_event: le, id: :log_event_viewer) |> dbg()
-
-  #       {:error, %Tesla.Env{} = err} ->
-  #         Logger.warning("Tesla Error when fetching log event | status=#{err.status}",
-  #           tesla_response: inspect(err)
-  #         )
-
-  #       {:error, error} ->
-  #         error =
-  #           case error do
-  #             :not_found ->
-
-  #             e ->
-  #     end
-  #   end)
-  # end
+  defp event_params(assigns) do
+    assigns |> Map.take([:user, :source, :log_event_id, :timestamp])
+  end
 end
