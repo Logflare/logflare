bc8f225485d2c27b85edecb1db9df8615577e0bf
TzeYiing <ty@tzeyiing.com>
2025-07-23 05:25:28 +0800

    chore: fix failing test

--- test/logflare/log_events_test.exs
+++ test/logflare/log_events_test.exs
@@ -3,30 +3,6 @@ defmodule Logflare.LogEventsTest do
   use Logflare.DataCase
   alias Logflare.Logs.LogEvents
 
-  test "fetch_event_by_id/3 with no partition" do
-    le = build(:log_event, message: "some message")
-    pid = self()
-    ref = make_ref()
-
-    expect(GoogleApi.BigQuery.V2.Api.Jobs, :bigquery_jobs_query, 1, fn _conn, _proj_id, opts ->
-      query = opts[:body].query
-      send(pid, {ref, query})
-
-      {:ok,
-       TestUtils.gen_bq_response([%{"id" => le.id, "event_message" => le.body["event_message"]}])}
-    end)
-
-    insert(:plan)
-    user = insert(:user)
-    source = insert(:source, user: user)
-
-    assert %{"event_message" => "some message"} =
-             LogEvents.fetch_event_by_id(source.token, le.id, [])
-
-    assert_receive {^ref, query}
-    refute query =~ "timestamp"
-    assert query =~ ".id ="
-  end
 
   test "fetch_event_by_id/3 with id and partition range" do
     le = build(:log_event, message: "some message")
@@ -44,6 +20,7 @@ defmodule Logflare.LogEventsTest do
     insert(:plan)
     user = insert(:user)
     source = insert(:source, user: user)
+    insert(:source_schema, source: source)
 
     assert %{"event_message" => "some message"} =
              LogEvents.fetch_event_by_id(source.token, le.id,
