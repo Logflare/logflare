6ebb97e4f1f3f43aae43917da3d9ddb2b1c3f3a5
Matt Stubbs <matt@yellowrobot.com.au>
2025-07-28 14:48:55 +1000

    remove migration dropping bigquery_udfs_hash
    Migration to be added in later major version release

--- priv/repo/migrations/20250725034224_alter_users_remove_bigquery_udfs_hash.exs
+++
@@ -1,9 +0,0 @@
-defmodule Logflare.Repo.Migrations.AlterUsersRemoveBigqueryUdfsHash do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      remove :bigquery_udfs_hash, :string, default: "", null: false
-    end
-  end
-end
