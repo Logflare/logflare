c3b0dd77b3920d9f4e1f28d873c16fe529f90cc3
Tilman Vogel <tilman.vogel@web.de>
2025-07-30 21:01:42 +0200

    Fix IPv6 support and support listening to a specific IP (#2226)
    * perf: reduce docker rebuild overhead
    
    Reorder build steps such that intermediate steps can more likely be reused,
    in particular installed apt, npm and mix packages.
    
    * fix: support IPv6, allow specific binding to IPs
    
    config/runtime.exs:
    new variable PHX_HTTP_IP for specifying IP address to bind to;
    Logflare.Repo,
    :postgres_backend_adapter:
    	set socket-options for IPv6;
    
    SharedRepo,
    ContextCache.Supervisor:
    hand socket-options on;
    
    update docs;
    update .template.env;
    
    * fix: address code-review
    
    runtime.exs:
    adapt logic from supabase/realtime:Realtime.Database.detect_ip_version/1;
    use :inet.parse_address/1 instead of IP.from_string!/1;
    
    shared_repo.ex:
    apply code-review suggestion;
    
    * address formatting linter
    
    * Update mix.exs: remove dependency not longer required
    
    Co-authored-by: Ziinc <Ziinc@users.noreply.github.com>
    
    * fix: only forward socket opts for single tenant PG
    
    * fix: fallback for host
    
    * fix: fallback for socket options
    
    ---------
    
    Co-authored-by: Chase Granberry <chase@logflare.app>
    Co-authored-by: Ziinc <Ziinc@users.noreply.github.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,6 +4,24 @@ filter_nil_kv_pairs = fn pairs when is_list(pairs) ->
   Enum.filter(pairs, fn {_k, v} -> v !== nil end)
 end
 
+detect_ip_version = fn host ->
+  host = String.to_charlist(host)
+  cond do
+    match?({:ok, _}, :inet6_tcp.getaddr(host)) -> {:ok, :inet6}
+    match?({:ok, _}, :inet.gethostbyname(host)) -> {:ok, :inet}
+    true -> {:error, :nxdomain}
+  end
+end
+
+socket_options_for_host = fn 
+  host when is_binary(host) ->
+    case detect_ip_version.(host) do
+      {:ok, ip_version} -> [ip_version]
+      {:error, reason} -> raise "Failed to detect IP version: #{reason}"
+    end
+  _host -> []
+end
+
 logflare_metadata =
   [cluster: System.get_env("LOGFLARE_METADATA_CLUSTER")]
   |> filter_nil_kv_pairs.()
@@ -14,6 +32,7 @@ logflare_health =
       System.get_env("LOGFLARE_HEALTH_MAX_MEMORY_UTILIZATION", "0.95") |> String.to_float()
   ]
   |> filter_nil_kv_pairs.()
+  
 
 config :logflare,
        Logflare.PubSub,
@@ -68,7 +87,18 @@ config :logflare,
 config :logflare,
        LogflareWeb.Endpoint,
        filter_nil_kv_pairs.(
-         http: filter_nil_kv_pairs.(port: System.get_env("PHX_HTTP_PORT")),
+         http:
+           filter_nil_kv_pairs.(
+             port: System.get_env("PHX_HTTP_PORT"),
+             ip:
+               case System.get_env("PHX_HTTP_IP") do
+                 nil -> nil
+                 value -> case :inet.parse_address(to_charlist(value)) do
+                   {:ok, ip} -> ip
+                   {:error, _} -> raise "Failed to parse IP address: #{value}"
+                 end
+               end
+           ),
          url:
            filter_nil_kv_pairs.(
              host: System.get_env("PHX_URL_HOST"),
@@ -99,6 +129,7 @@ config :logflare,
          hostname: System.get_env("DB_HOSTNAME"),
          password: System.get_env("DB_PASSWORD"),
          username: System.get_env("DB_USERNAME"),
+         socket_options: socket_options_for_host.(System.get_env("DB_HOSTNAME")),
          after_connect:
            if(System.get_env("DB_SCHEMA"),
              do: {Postgrex, :query!, ["set search_path=#{System.get_env("DB_SCHEMA")}", []]},
@@ -225,6 +256,14 @@ if config_env() != :test do
   config :grpc, port: System.get_env("LOGFLARE_GRPC_PORT", "50051") |> String.to_integer()
 end
 
+socket_options_for_url = fn 
+  url when is_binary(url) ->
+    case URI.parse(url) do
+      %URI{host: host} -> socket_options_for_host.(host)
+      _ -> raise "Failed to parse URL: #{inspect(url)}"
+    end
+  _url -> nil
+end
 cond do
   System.get_env("LOGFLARE_SINGLE_TENANT", "false") == "true" &&
       not is_nil(System.get_env("POSTGRES_BACKEND_URL")) ->
@@ -232,7 +271,8 @@ cond do
            :postgres_backend_adapter,
            filter_nil_kv_pairs.(
              url: System.get_env("POSTGRES_BACKEND_URL"),
-             schema: System.get_env("POSTGRES_BACKEND_SCHEMA")
+             schema: System.get_env("POSTGRES_BACKEND_SCHEMA"),
+             socket_options: socket_options_for_url.(System.get_env("POSTGRES_BACKEND_URL"))
            )
 
   config_env() != :test ->
