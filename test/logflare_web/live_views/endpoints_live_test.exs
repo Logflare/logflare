8dd8e2b39d53ba85a33370b1d070f9e73b68f670
Matt Stubbs <matt@yellowrobot.com.au>
2025-07-31 14:38:47 +1000

    feat: display query cost with results for Alert, Endpoint, and Query

--- test/logflare_web/live_views/endpoints_live_test.exs
+++ test/logflare_web/live_views/endpoints_live_test.exs
@@ -229,6 +229,8 @@ defmodule LogflareWeb.EndpointsLiveTest do
       assert has_element?(view, "label", "Max limit")
       assert has_element?(view, "label", "Enable authentication")
 
+      assert view |> render() =~ "1 byte processed"
+
       assert view
              |> element("form#endpoint")
              |> render_submit(%{
