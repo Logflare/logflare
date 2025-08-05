cd80bb288f21085c747029f6d81eb7c067d5f90e
Matt Stubbs <matt@yellowrobot.com.au>
2025-08-05 15:04:54 +1000

    test: use_query_cache option

--- test/logflare/bigquery/bq_repo_test.exs
+++ test/logflare/bigquery/bq_repo_test.exs
@@ -80,5 +80,37 @@ defmodule Logflare.BigQuery.BqRepoTest do
       assert [%{"event_message" => "something", "a" => "value"}] = response.rows
       assert response.total_rows == 1
     end
+
+    test "query_with_sql_and_params respects use_query_cache option", %{user: user} do
+      pid = self()
+
+      GoogleApi.BigQuery.V2.Api.Jobs
+      |> expect(:bigquery_jobs_query, 2, fn
+        _conn, _, opts ->
+          send(pid, {:use_query_cache, opts[:body].useQueryCache})
+          {:ok, TestUtils.gen_bq_response([])}
+      end)
+
+      assert {:ok, _response} =
+               BqRepo.query_with_sql_and_params(
+                 user,
+                 "project-id",
+                 "select timestamp from `my_table`",
+                 []
+               )
+
+      assert_received {:use_query_cache, true}, "query cache enabled by default"
+
+      assert {:ok, _response} =
+               BqRepo.query_with_sql_and_params(
+                 user,
+                 "project-id",
+                 "select timestamp from `my_table`",
+                 [],
+                 use_query_cache: false
+               )
+
+      assert_received {:use_query_cache, false}, "query cache is disabled"
+    end
   end
 end
