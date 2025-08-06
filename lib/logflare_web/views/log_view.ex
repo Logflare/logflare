0b58d9605ae74c9e164eb243b9f256aaaea50040
Matt Stubbs <matt@yellowrobot.com.au>
2025-08-06 11:02:01 +1000

    fix: log event view permalink
    * add log event permalink component
    * pass lql query is passed to log event view component

--- lib/logflare_web/views/log_view.ex
+++ lib/logflare_web/views/log_view.ex
@@ -1,6 +1,8 @@
 defmodule LogflareWeb.LogView do
   use LogflareWeb, :view
 
+  import LogflareWeb.CoreComponents, only: [log_event_permalink: 1]
+
   def render("index.json", %{message: message}) do
     %{message: message}
   end
