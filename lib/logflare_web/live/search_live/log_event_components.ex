b2d6a27667a6f8ee7d22604d919ee07cb9deb166
Matt Stubbs <matt@yellowrobot.com.au>
2025-08-15 13:03:19 +1000

    refactor: log_event component to be more flexible

--- lib/logflare_web/live/search_live/log_event_components.ex
+++ lib/logflare_web/live/search_live/log_event_components.ex
@@ -13,64 +13,13 @@ defmodule LogflareWeb.SearchLive.LogEventComponents do
 
   @log_levels ~W(debug info warning error alert critical notice emergency)
 
-  attr :log, :map
-  attr :search_timezone, :string
-  attr :query_string, :string
-  attr :source, :map
-
-  def log_search_event(%{log: %{body: body}} = assigns) when is_map_key(body, "event_message") do
-    tz_part =
-      DateTimeUtils.humanize_timezone_offset(
-        Timex.Timezone.get(assigns.search_timezone).offset_utc
-      )
-
-    assigns =
-      assigns
-      |> assign(timestamp: body["timestamp"], message: body["event_message"])
-      |> assign(
-        :formatted_timestamp,
-        format_timestamp(body["timestamp"], assigns.search_timezone) <> tz_part
-      )
-      |> assign(
-        :log_level,
-        if(body["level"] in @log_levels, do: body["level"], else: nil)
-      )
-
-    ~H"""
-    <li id={"log-event_#{@log.id || @log.body["timestamp"]}"} class="tw-group">
-      <span class="tw-inline-block">
-        <.metadata timestamp={@timestamp} log_level={@log_level}>
-          <%= format_timestamp(@timestamp, @search_timezone) %>
-        </.metadata>
-        <%= @message %>
-      </span>
-      <span class="tw-inline-block tw-text-[0.65rem] tw-align-text-bottom tw-inline-flex tw-flex-row tw-gap-2">
-        <%= live_modal_show_link(component: LogflareWeb.Search.LogEventViewerComponent, modal_id: :log_event_viewer, title: "Log Event", phx_value_log_event_id: @log.id, phx_value_log_event_timestamp: @log.body["timestamp"], phx_value_lql:  @query_string) do %>
-          <span>view</span>
-        <% end %>
-        <%= live_modal_show_link(component: LogflareWeb.SearchLive.EventContextComponent, modal_id: :log_event_context_viewer, title: "View Event Context", phx_value_log_event_id: @log.id, phx_value_log_event_timestamp: @log.body["timestamp"], phx_value_lql_rules:  assigns[:lql_rules]) do %>
-          <span>context</span>
-        <% end %>
-        <.link class="tw-text-[0.65rem]  group-hover:tw-visible tw-invisible" phx-click={JS.dispatch("logflare:copy-to-clipboard", detail: %{text: "#{@formatted_timestamp}    #{@message}"})} data-toggle="tooltip" data-placement="top" title="Copy to clipboard">
-          copy
-        </.link>
-        <.log_event_permalink log_event_id={@log.id} timestamp={@timestamp} source={@source} lql={@query_string} class="group-hover:tw-visible tw-invisible" />
-      </span>
-    </li>
-    """
-  end
-
-  def log_search_event(assigns), do: ~H""
-
   attr :log_event, Logflare.LogEvent, required: true
   attr :timezone, :string, required: true
   attr :rest, :global, default: %{class: "tw-group"}
   slot :inner_block
   slot :actions
 
-  def log_event(assigns) do
-    %{log_event: %{body: body}} = assigns
-
+  def log_event(%{log: %{body: body}} = assigns) when is_map_key(body, "event_message") do
     assigns =
       assigns
       |> assign(timestamp: body["timestamp"], message: body["event_message"])
@@ -90,6 +39,8 @@ defmodule LogflareWeb.SearchLive.LogEventComponents do
     """
   end
 
+  def log_event(assigns), do: ~H""
+
   attr :log_level, :string, default: nil
   attr :timestamp, :string
   slot :inner_block
@@ -101,5 +52,13 @@ defmodule LogflareWeb.SearchLive.LogEventComponents do
     </mark>
     <mark :if={@log_level} class={"log-level-#{@log_level}"}><%= @log_level %></mark>
     """
+  end
+
+  def formatted_timestamp(log_event, timezone) do
+    tz_part =
+      Timex.Timezone.get(search_timezone).offset_utc
+      |> DateTimeUtils.humanize_timezone_offset()
+
+    format_timestamp(log_event.body["timestamp"], timezone) <> tz_part
   end
 end
