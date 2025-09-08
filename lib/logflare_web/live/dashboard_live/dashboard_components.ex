01767bf06c86af3e77d08a161e952fe61fdf010e
Matt Stubbs <matt@yellowrobot.com.au>
2025-09-08 22:53:01 +1000

    fix vertical spacing

--- lib/logflare_web/live/dashboard_live/dashboard_components.ex
+++ lib/logflare_web/live/dashboard_live/dashboard_components.ex
@@ -14,7 +14,7 @@ defmodule LogflareWeb.DashboardLive.DashboardComponents do
         <:item title="Vercel" link="https://docs.logflare.app/integrations/vercel/" />
         <:item title="Fly" link="https://github.com/Logflare/fly-log-shipper" />
         <:item title="Postgres FDW" link="https://docs.logflare.app/integrations/postgres-fdw" description="SQL" />
-        <:item title="Pino Logflare" link="https://docs.logflare.app/integrations/pino-logflare" description="Javascript" />
+        <:item title="pino-logflare" link="https://docs.logflare.app/integrations/pino-logflare" description="Javascript" />
         <:item title="LoggerBackend" link="https://github.com/Logflare/logflare_logger_backend" description="Elixir" />
         <:item title="logflare_erl" link="https://github.com/Logflare/logflare_erl" description="Erlang" />
       </.integrations_list>
@@ -42,7 +42,7 @@ defmodule LogflareWeb.DashboardLive.DashboardComponents do
     ~H"""
     <h5 class="header-margin"><%= render_slot(@heading) %></h5>
     <ul class="tw-list-none tw-p-0 tw-m-0 tw-mb-3">
-      <li :for={item <- @item} class="tw-px-5 tw-py-2 tw-border-b tw-border-gray-200 tw-bg-[#1d1d1d] tw-text-sm">
+      <li :for={item <- @item} class="tw-px-5 tw-py-3 tw-border-b tw-border-gray-200 tw-bg-[#1d1d1d] tw-text-sm">
         <.link class="tw-text-white" href={item.link}><%= item.title %></.link>
         <span :if={item[:description]} class="tw-text-xs tw-block">
           <%= item.description %>
@@ -53,68 +53,68 @@ defmodule LogflareWeb.DashboardLive.DashboardComponents do
   end
 
   def teams(assigns) do
-     ~H"""
-     <div>
-       <h5 class="header-margin">Teams</h5>
-       <ul class="list-unstyled">
-         <li :if={@home_team}>
-           <strong :if={@current_team.id == @home_team.id}><%= @home_team.name %></strong>
-           <.link :if={@current_team.id != @home_team.id} navigate={~p"/profile/switch?#{%{"user_id" => @home_team.user_id, "redirect_to" => "/dashboard_new"}}"}>
-             <%= @home_team.name %>
-           </.link>
-           <small>home team</small>
-         </li>
+    ~H"""
+    <div>
+      <h5 class="header-margin">Teams</h5>
+      <ul class="list-unstyled">
+        <li :if={@home_team}>
+          <strong :if={@current_team.id == @home_team.id}><%= @home_team.name %></strong>
+          <.link :if={@current_team.id != @home_team.id} navigate={~p"/profile/switch?#{%{"user_id" => @home_team.user_id, "redirect_to" => "/dashboard_new"}}"}>
+            <%= @home_team.name %>
+          </.link>
+          <small>home team</small>
+        </li>
 
-         <li :if={@home_team == nil}>
-           <.link href={~p"/account"} method="post">
-             Create your own Logflare account.
-           </.link>
-         </li>
+        <li :if={@home_team == nil}>
+          <.link href={~p"/account"} method="post">
+            Create your own Logflare account.
+          </.link>
+        </li>
 
-         <%= if Enum.empty?(@team_users) do %>
-           Other teams you are a member of will be listed here.
-         <% end %>
+        <%= if Enum.empty?(@team_users) do %>
+          Other teams you are a member of will be listed here.
+        <% end %>
 
-         <li :for={team_user <- @team_users} :if={team_user.team.id != @current_team.id}>
-           <li>
-           <span :if={team_user.team_id == @current_team.id}><%= team_user.team.name %></span>
-           <.link :if={team_user.team_id != @current_team.id} navigate={~p"/profile/switch?#{%{"user_id" => team_user.team.user_id, "team_user_id" => team_user.id, "redirect_to" => "/dashboard_new"}}"}>
-               <%= team_user.team.name %>
-             </.link>
-           </li>
-         </li>
-       </ul>
-     </div>
-     """
-   end
+        <li :for={team_user <- @team_users} :if={team_user.team.id != @current_team.id}>
+          <li>
+            <span :if={team_user.team_id == @current_team.id}><%= team_user.team.name %></span>
+            <.link :if={team_user.team_id != @current_team.id} navigate={~p"/profile/switch?#{%{"user_id" => team_user.team.user_id, "team_user_id" => team_user.id, "redirect_to" => "/dashboard_new"}}"}>
+              <%= team_user.team.name %>
+            </.link>
+          </li>
+        </li>
+      </ul>
+    </div>
+    """
+  end
 
-   def members(assigns) do
-     ~H"""
-     <div>
-       <h5 class="header-margin">Members</h5>
-       <ul id="team-members" class="tw-mb-1 tw-list-none tw-p-0">
-         <li>
-           <img class="rounded-circle" width="35" height="35" src={@user.image || Auth.gen_gravatar_link(@user.email)} alt={@user.name || @user.email} />
-           <.link href={"mailto:#{@user.email}"}>
-             <%= @user.name || @user.email %>
-           </.link>
-           <small><%= if true, do: "owner", else: "owner, you" %></small>
-         </li>
-         <li :for={member <- @team.team_users} :if={member.id != @user.id }>
-           <img class="rounded-circle" width="35" height="35" src={member.image || Auth.gen_gravatar_link(member.email)} alt={member.name || member.email} />
-           <.link href={"mailto:#{member.email}"}>
-             <%= member.name || member.email %>
-           </.link>
+  def members(assigns) do
+    ~H"""
+    <div>
+      <h5 class="header-margin">Members</h5>
+      <ul id="team-members" class="tw-mb-1 tw-list-none tw-p-0">
+        <li>
+          <img class="rounded-circle" width="35" height="35" src={@user.image || Auth.gen_gravatar_link(@user.email)} alt={@user.name || @user.email} />
+          <.link href={"mailto:#{@user.email}"}>
+            <%= @user.name || @user.email %>
+          </.link>
+          <small><%= if true, do: "owner", else: "owner, you" %></small>
+        </li>
+        <li :for={member <- @team.team_users} :if={member.id != @user.id}>
+          <img class="rounded-circle" width="35" height="35" src={member.image || Auth.gen_gravatar_link(member.email)} alt={member.name || member.email} />
+          <.link href={"mailto:#{member.email}"}>
+            <%= member.name || member.email %>
+          </.link>
 
-           <.link href={~p"/profile/#{member.id}"} data-confirm="Delete member?" class="dashboard-links" method="delete" >
-             <i class="fa fa-trash"></i>
-           </.link>
-           </li>
-       </ul>
-       <.link href={~p"/account/edit#team-members"}>
-         Invite more team members.
-       </.link>
-     </div>
-     """
-   end
+          <.link href={~p"/profile/#{member.id}"} data-confirm="Delete member?" class="dashboard-links" method="delete">
+            <i class="fa fa-trash"></i>
+          </.link>
+        </li>
+      </ul>
+      <.link href={~p"/account/edit#team-members"}>
+        Invite more team members.
+      </.link>
+    </div>
+    """
+  end
 end
