d69583e61e66910051af48513534fc722e71757e
Matt Stubbs <matt@yellowrobot.com.au>
2025-09-09 09:02:28 +1000

    fix: use getAttribute to avoid jQuery.data() caching

--- assets/js/logs.js
+++ assets/js/logs.js
@@ -3,8 +3,10 @@ import $ from "jquery"
 export async function applyToAllLogTimestamps(formatter) {
   const timestamps = $(".log-datestamp")
   for (let time of timestamps) {
-    const timestampMs = $(time).data("timestamp")
     const datetime = formatter(timestampMs)
     $(time).text(datetime)
+    const timestampMs = time.getAttribute("data-timestamp");
+    const datetime = formatter(timestampMs);
+    $(time).text(datetime);
   }
 }
