c4a55da28c7a684d842ef9b86579c516b4df065f
andré marques <andr.crono@hotmail.com>
2025-09-09 12:39:25 -0300

    test: telemetry correctly skips deleted ets tables

--- test/logflare/telemetry_test.exs
+++ test/logflare/telemetry_test.exs
@@ -1,5 +1,6 @@
 defmodule Logflare.TelemetryTest do
   use ExUnit.Case, async: false
+  use Mimic
 
   alias Logflare.Telemetry
   alias Logflare.TestUtils
@@ -45,6 +46,19 @@ defmodule Logflare.TelemetryTest do
       assert_receive {:telemetry_event, ^event, metrics, meta}
       assert match?(%{size: _}, metrics)
       assert match?(%{name: _}, meta)
+    end
+
+    test "ignores tables that were deleted during listing" do
+      # simulates all tables being deleted to simplify testing, so we can
+      # just check if all tables were skipped for returning :undefined
+      Logflare.Utils
+      |> stub(:ets_info, fn _ -> :undefined end)
+
+      event = [:logflare, :system, :top_ets_tables, :individual]
+      TestUtils.attach_forwarder(event)
+      Telemetry.ets_table_metrics()
+
+      refute_receive {:telemetry_event, ^event, _, _}
     end
   end
 end
