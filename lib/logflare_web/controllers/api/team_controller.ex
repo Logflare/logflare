1a9f9830dc8c0935c61f70b4534473ac67e651f2
Adam Mokan <amokan@gmail.com>
2025-10-07 16:17:14 -0700

    adds basic tests for additions to api `team_controller` and leverage pattern-matching to better handle with clause

--- lib/logflare_web/controllers/api/team_controller.ex
+++ lib/logflare_web/controllers/api/team_controller.ex
@@ -3,7 +3,10 @@ defmodule LogflareWeb.Api.TeamController do
   use OpenApiSpex.ControllerSpecs
 
   alias Logflare.Teams
+  alias Logflare.Teams.Team
   alias Logflare.TeamUsers
+  alias Logflare.TeamUsers.TeamUser
+  alias Logflare.User
   alias Logflare.Users
   alias Logflare.Backends.Adaptor.BigQueryAdaptor
   alias LogflareWeb.OpenApi.Accepted
@@ -14,8 +17,8 @@ defmodule LogflareWeb.Api.TeamController do
 
   require Logger
 
-  alias LogflareWeb.OpenApiSchemas.Team
-  alias LogflareWeb.OpenApiSchemas.TeamUser
+  alias LogflareWeb.OpenApiSchemas.Team, as: TeamSchema
+  alias LogflareWeb.OpenApiSchemas.TeamUser, as: TeamUserSchema
 
   action_fallback(LogflareWeb.Api.FallbackController)
 
@@ -23,7 +26,7 @@ defmodule LogflareWeb.Api.TeamController do
 
   operation(:index,
     summary: "List teams",
-    responses: %{200 => List.response(Team)}
+    responses: %{200 => List.response(TeamSchema)}
   )
 
   def index(%{assigns: %{user: user}} = conn, _) do
@@ -35,13 +38,13 @@ defmodule LogflareWeb.Api.TeamController do
     summary: "Fetch team",
     parameters: [token: [in: :path, description: "Team Token", type: :string]],
     responses: %{
-      200 => Team.response(),
+      200 => TeamSchema.response(),
       404 => NotFound.response()
     }
   )
 
   def show(%{assigns: %{user: user}} = conn, %{"token" => token}) do
-    with team when not is_nil(team) <- Teams.get_team_by_user_access(user, token),
+    with %Team{} = team <- Teams.get_team_by_user_access(user, token),
          team <- Teams.preload_fields(team, [:user, :team_users]) do
       json(conn, team)
     end
@@ -49,9 +52,9 @@ defmodule LogflareWeb.Api.TeamController do
 
   operation(:create,
     summary: "Create Team",
-    request_body: Team.params(),
+    request_body: TeamSchema.params(),
     responses: %{
-      201 => Created.response(Team),
+      201 => Created.response(TeamSchema),
       404 => NotFound.response(),
       422 => UnprocessableEntity.response()
     }
@@ -69,9 +72,9 @@ defmodule LogflareWeb.Api.TeamController do
   operation(:update,
     summary: "Update team",
     parameters: [token: [in: :path, description: "Team Token", type: :string]],
-    request_body: Team.params(),
+    request_body: TeamSchema.params(),
     responses: %{
-      201 => Created.response(Team),
+      201 => Created.response(TeamSchema),
       204 => Accepted.response(),
       404 => NotFound.response(),
       422 => UnprocessableEntity.response()
@@ -79,7 +82,7 @@ defmodule LogflareWeb.Api.TeamController do
   )
 
   def update(%{assigns: %{user: user}} = conn, %{"token" => token} = params) do
-    with team when not is_nil(team) <- Teams.get_team_by(token: token, user_id: user.id),
+    with %Team{} = team <- Teams.get_team_by(token: token, user_id: user.id),
          {:ok, team} <- Teams.update_team(team, params),
          team <- Teams.preload_fields(team, [:user, :team_users]) do
       conn
@@ -105,7 +108,7 @@ defmodule LogflareWeb.Api.TeamController do
   )
 
   def delete(%{assigns: %{user: user}} = conn, %{"token" => token}) do
-    with team when not is_nil(team) <- Teams.get_team_by(token: token, user_id: user.id),
+    with %Team{} = team <- Teams.get_team_by(token: token, user_id: user.id),
          {:ok, _} <- Teams.delete_team(team) do
       conn
       |> Plug.Conn.send_resp(204, [])
@@ -118,31 +121,36 @@ defmodule LogflareWeb.Api.TeamController do
     parameters: [
       token: [in: :path, description: "Team Token", type: :string]
     ],
-    request_body: TeamUser.params(),
+    request_body: TeamUserSchema.params(),
     responses: %{
       204 => Accepted.response(),
       404 => NotFound.response()
     }
   )
 
-  def add_member(
-        %{assigns: %{user: user}} = conn,
-        %{"team_token" => token, "email" => email}
-      ) do
-    auth_params = %{
-      email: email,
-      provider_uid: user.provider_uid,
-      provider: user.provider
-    }
+  def add_member(%{assigns: %{user: user}} = conn, %{"team_token" => token, "email" => email}) do
+    auth_params =
+      case Users.get_by(email: email) do
+        nil ->
+          {:ok, new_user} = Users.insert_user(%{email: email, provider: user.provider})
+          Logger.info("Created new user #{email}")
 
-    u = Users.get_by(email: email)
-    # user must exist or be created
-    if is_nil(u) do
-      Users.insert_user(auth_params)
-      Logger.info("Created new user #{email}")
-    end
+          %{
+            email: new_user.email,
+            provider_uid: new_user.provider_uid,
+            provider: new_user.provider
+          }
 
-    with team when not is_nil(team) <- Teams.get_team_by(token: token),
+        %User{} = existing_user ->
+          %{
+            email: existing_user.email,
+            provider_uid: existing_user.provider_uid,
+            provider: existing_user.provider
+          }
+      end
+
+    with %Team{} = team <- Teams.get_team_by(token: token, user_id: user.id),
+         team <- Teams.preload_user(team),
          {:ok, _} <- TeamUsers.insert_or_update_team_user(team, auth_params) do
       BigQueryAdaptor.update_iam_policy()
       BigQueryAdaptor.patch_dataset_access(team.user)
@@ -166,9 +174,9 @@ defmodule LogflareWeb.Api.TeamController do
   )
 
   def remove_member(%{assigns: %{user: user}} = conn, %{"team_token" => token, "id" => id}) do
-    team_user = TeamUsers.get_team_user_by(email: id)
-
-    with team when not is_nil(team) <- Teams.get_team_by(token: token, user_id: user.id),
+    with %TeamUser{} = team_user <- TeamUsers.get_team_user_by(email: id),
+         %Team{} = team <- Teams.get_team_by(token: token, user_id: user.id),
+         team <- Teams.preload_user(team),
          {:ok, _} <- TeamUsers.delete_team_user(team_user) do
       BigQueryAdaptor.update_iam_policy()
       BigQueryAdaptor.patch_dataset_access(team.user)
