0acd05f66d923f9cc56cb5e8c1968987da40276e
Matt Stubbs <matt@yellowrobot.com.au>
2025-10-20 13:13:22 +1000

    add AuthLive

---
+++ lib/logflare_web/live/auth_live.ex
@@ -0,0 +1,27 @@
+defmodule LogflareWeb.AuthLive do
+  @moduledoc """
+  Auth hooks for LiveViews.
+
+  The selected team can be set with the `team_id` query param.
+  It is verified by checking for a team user with the given email and team_id.
+  """
+
+  import Phoenix.Component
+
+  alias Logflare.Teams.TeamContext
+
+  def on_mount(:default, params, %{"current_email" => email}, socket) do
+    team_id = Map.get(params, "team_id")
+
+    case TeamContext.resolve(team_id, email) do
+      {:ok, %TeamContext{team: team, user: user, team_user: team_user}} ->
+        {:cont, assign(socket, user: user, team: team, team_user: team_user)}
+
+      {:error, _reason} ->
+        {:halt, socket}
+    end
+  end
+
+  def on_mount(:default, _params, _session, socket),
+    do: {:halt, socket}
+end
