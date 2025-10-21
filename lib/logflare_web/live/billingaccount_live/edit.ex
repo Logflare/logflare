2b30ae8c5743698ab5304a1c1992f9e139c1dc65
Matt Stubbs <matt@yellowrobot.com.au>
2025-10-21 11:52:24 +1000

    BillingAccountLive uses AuthLive

--- lib/logflare_web/live/billingaccount_live/edit.ex
+++ lib/logflare_web/live/billingaccount_live/edit.ex
@@ -11,14 +11,16 @@ defmodule LogflareWeb.BillingAccountLive.Edit do
 
   require Logger
 
+  on_mount {LogflareWeb.AuthLive, :default}
+
   @impl true
-  def mount(_params, %{"user_id" => user_id}, socket) do
+  def mount(_params, _session, socket) do
     if connected?(socket) do
       Phoenix.PubSub.subscribe(Logflare.PubSub, "billing")
     end
 
     user =
-      Users.get(user_id)
+      socket.assigns.user
       |> Users.preload_sources()
       |> Users.preload_billing_account()
 
