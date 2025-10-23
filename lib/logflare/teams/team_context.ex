0070246a2576c8924bacddc695ce6a1f2564a1e0
Matt Stubbs <matt@yellowrobot.com.au>
2025-10-23 11:13:22 +1000

    refactor: TeamContext handles team_user with not matching %User{} record

--- lib/logflare/teams/team_context.ex
+++ lib/logflare/teams/team_context.ex
@@ -16,16 +16,17 @@ defmodule Logflare.Teams.TeamContext do
           team_user: TeamUser.t() | nil
         }
 
-  @type error_reason :: :invalid_team_id | :missing_email | :not_authorized | :missing_team
+  @type error_reason :: :invalid_team_id | :not_authorized | :missing_team
   @type resolve_result :: {:ok, t()} | {:error, error_reason()}
+  @type team_id_param :: String.t() | non_neg_integer() | nil
 
   defstruct [:user, :team, :team_user]
 
-  @spec resolve(String.t() | nil, String.t() | nil) :: resolve_result()
+  @spec resolve(team_id_param(), String.t()) :: resolve_result()
   def resolve(team_id_param, email) when is_binary(email) do
     with {:ok, role} <- parse_team_id(team_id_param),
-         {:ok, %User{} = current_user} <- fetch_user(email),
-         {:ok, %__MODULE__{} = ctx} <- do_resolve(current_user, role, email) do
+         {:ok, team, role} <- verify_team(email, role),
+         {:ok, %__MODULE__{} = ctx} <- do_resolve(team, role, email) do
       {:ok, ctx}
     end
   end
@@ -35,45 +36,50 @@ defmodule Logflare.Teams.TeamContext do
   def parse_team_id(nil), do: {:ok, :owner}
   def parse_team_id(""), do: {:ok, :owner}
 
-  def parse_team_id(team_id_param) do
+  def parse_team_id(team_id_param) when is_binary(team_id_param) do
     case Integer.parse(team_id_param) do
       {team_id, ""} when team_id >= 0 -> {:ok, team_id}
       _ -> {:error, :invalid_team_id}
     end
   end
 
-  @spec do_resolve(User.t(), :owner | non_neg_integer(), String.t()) :: resolve_result()
-  defp do_resolve(%User{} = current_user, :owner, _email) do
-    with {:ok, team} <- fetch_home_team(current_user) do
-      {:ok, %__MODULE__{user: current_user, team: team, team_user: nil}}
-    end
+  def parse_team_id(team_id_param) when is_integer(team_id_param), do: {:ok, team_id_param}
+
+  def parse_team_id(_), do: {:error, :invalid_team_id}
+
+  defp do_resolve(%Team{} = team, :owner, _email) do
+    team = Logflare.Teams.preload_user(team)
+    {:ok, %__MODULE__{user: team.user, team: team, team_user: nil}}
   end
 
-  defp do_resolve(%User{} = current_user, team_id, email) when is_integer(team_id) do
-    if team_owner?(current_user, team_id) do
-      do_resolve(current_user, :owner, email)
-    else
-      case TeamUsers.Cache.get_team_user_by(email: email, team_id: team_id) do
-        %TeamUser{} = team_user -> resolve_team_user(team_user)
-        _ -> {:error, :not_authorized}
-      end
-    end
-  end
-
-  @spec fetch_user(String.t()) :: {:ok, User.t()} | {:error, :not_authorized}
-  defp fetch_user(email) do
-    case Users.Cache.get_by_and_preload(email: email) do
-      %User{} = user -> {:ok, user}
+  defp do_resolve(_team, team_id, email) when is_integer(team_id) do
+    case TeamUsers.Cache.get_team_user_by(email: email, team_id: team_id) do
+      %TeamUser{} = team_user -> resolve_team_user(team_user)
       _ -> {:error, :not_authorized}
     end
   end
 
-  defp fetch_home_team(%User{team: %Team{} = team}), do: {:ok, Teams.preload_team_users(team)}
+  defp verify_team(email, :owner) when is_binary(email) do
+    case Users.Cache.get_by_and_preload(email: email) do
+      %User{} = user ->
+        {:ok, user.team, :owner}
 
-  defp fetch_home_team(%User{id: user_id}) do
-    case Teams.get_team_by(user_id: user_id) do
-      %Team{} = team -> {:ok, Teams.preload_team_users(team)}
-      _ -> {:error, :missing_team}
+      _ ->
+        {:error, :not_authorized}
+    end
+  end
+
+  defp verify_team(email, team_id) when is_binary(email) do
+    cond do
+      email_is_team_owner?(email, team_id) ->
+        verify_team(email, :owner)
+
+      team_user = fetch_team_user(email, team_id) ->
+        team_user = team_user |> TeamUsers.preload_defaults()
+        {:ok, team_user.team, team_id}
+
+      true ->
+        {:error, :not_authorized}
     end
   end
 
@@ -104,12 +110,17 @@ defmodule Logflare.Teams.TeamContext do
     end
   end
 
-  defp team_owner?(%User{team: %Team{id: id}}, team_id) when id == team_id, do: true
+  defp email_is_team_owner?(email, team_id) do
+    case Users.Cache.get_by_and_preload(email: email) do
+      %User{} = user ->
+        user.team.id == team_id
 
-  defp team_owner?(%User{id: user_id}, team_id) do
-    case Teams.get_team_by(id: team_id) do
-      %Team{user_id: ^user_id} -> true
-      _ -> false
+      _ ->
+        false
     end
+  end
+
+  defp fetch_team_user(email, team_id) do
+    TeamUsers.Cache.get_team_user_by(email: email, team_id: team_id)
   end
 end
