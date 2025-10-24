eceef015db3953421679983409a49d3fee805e6d
Matt Stubbs <matt@yellowrobot.com.au>
2025-10-24 10:30:22 +1000

    EndpointsLive and QueryLive use AuthLive

--- lib/logflare_web/live/endpoints/endpoints_live.ex
+++ lib/logflare_web/live/endpoints/endpoints_live.ex
@@ -34,8 +34,8 @@ defmodule LogflareWeb.EndpointsLive do
     """
   end
 
-  def mount(%{}, %{"user_id" => user_id}, socket) do
-    user = Users.get(user_id)
+  def mount(%{}, _session, socket) do
+    %{assigns: %{user: user}} = socket
 
     allow_access = Enum.any?([Utils.flag("endpointsOpenBeta"), user.endpoints_beta])
 
@@ -44,7 +44,6 @@ defmodule LogflareWeb.EndpointsLive do
     socket =
       socket
       |> assign(:user_id, user.id)
-      |> assign(:user, user)
       #  must be below user_id assign
       |> refresh_endpoints()
       |> assign(:query_result_rows, nil)
