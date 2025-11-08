e47f1a88ed8bc49e141bf06a6f43235e52faade4
Nelson Estevão <nelsonmestevao@proton.me>
2025-11-08 13:50:48 +0000

    test(e2e): add http credentials for Supabase Studio

--- test/e2e/supabase/features/index.spec.ts
+++ test/e2e/supabase/features/index.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle(/Logflare | Cloudflare, Vercel & Elixir Logging/);
+  await expect(page).toHaveTitle(/Default Project | Default Organization | Supabase/);
 });
--- test/e2e/supabase/playwright.config.ts
+++ test/e2e/supabase/playwright.config.ts
@@ -4,9 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, 'supabase/docker/.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -25,7 +25,12 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'http://localhost:4000',
+    baseURL: 'http://localhost:8000',
+
+    httpCredentials: {
+      username: process.env.DASHBOARD_USERNAME!,
+      password: process.env.DASHBOARD_PASSWORD!,
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
