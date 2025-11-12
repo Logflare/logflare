4da4c4951092539cc3483298b0b7ef41dd6c17d4
Ziinc <Ziinc@users.noreply.github.com>
2025-11-12 23:06:23 +0800

    perf: otel metrics exporter optimizations (#2912)

--- mix.exs
+++ mix.exs
@@ -242,7 +242,7 @@ defmodule Logflare.Mixfile do
       {:opentelemetry_phoenix, "~> 2.0.0-rc.2"},
       {:opentelemetry_bandit, "~> 0.2.0-rc.1"},
       {:otel_metric_exporter,
-       git: "https://github.com/supabase/elixir-otel-metric-exporter", ref: "c0fe8c7"},
+       git: "https://github.com/supabase/elixir-otel-metric-exporter", ref: "05743a6"},
       {:live_monaco_editor, "~> 0.2"}
     ]
   end
