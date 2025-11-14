e09a79dc4fd952aa92780b372dbb0e533fe64144
TzeYiing <ty@tzeyiing.com>
2025-11-14 16:57:49 +0800

    perf: bandit gc more frequently

--- config/config.exs
+++ config/config.exs
@@ -37,6 +37,7 @@ config :logflare, LogflareWeb.Endpoint,
   adapter: Bandit.PhoenixAdapter,
   http: [
     http_options: [log_protocol_errors: :short, log_client_closures: false],
+    http1_options: [gc_every_n_keepalive_requests: 3],
     thousand_island_options: [
       num_acceptors: 1250,
       # default backend keepalive timeout is fixed at 600 seconds
