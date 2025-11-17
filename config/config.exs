ec309476ae1dfc1a79837c20d2c9a3b7ac199a0d
TzeYiing <ty@tzeyiing.com>
2025-11-17 21:27:23 +0800

    chore: fix misconfig

--- config/config.exs
+++ config/config.exs
@@ -37,7 +37,7 @@ config :logflare, LogflareWeb.Endpoint,
   adapter: Bandit.PhoenixAdapter,
   http: [
     http_options: [log_protocol_errors: :short, log_client_closures: false],
-    http1_options: [gc_every_n_keepalive_requests: 3],
+    http_1_options: [gc_every_n_keepalive_requests: 3],
     thousand_island_options: [
       num_acceptors: 1250,
       # default backend keepalive timeout is fixed at 600 seconds
