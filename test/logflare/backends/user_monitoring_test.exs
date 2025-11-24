6ecda81058587c0883ae41aba22faa52577fdbf3
TzeYiing <ty@tzeyiing.com>
2025-11-25 05:16:43 +0800

    chore: add more tests

--- test/logflare/backends/user_monitoring_test.exs
+++ test/logflare/backends/user_monitoring_test.exs
@@ -213,6 +213,67 @@ defmodule Logflare.Backends.UserMonitoringTest do
     end
   end
 
+  describe "egress" do
+    setup :start_otel_exporter
+
+    setup do
+      start_supervised!(AllLogsLogged)
+      insert(:plan)
+      :ok
+    end
+
+    test "backends.ingest.egress.request_bytes includes backend metadata" do
+      pid = self()
+
+      GoogleApi.BigQuery.V2.Api.Tabledata
+      |> stub(:bigquery_tabledata_insert_all, fn _, _, _, _, opts ->
+        send(pid, {:insert_all, opts[:body].rows})
+        {:ok, %GoogleApi.BigQuery.V2.Model.TableDataInsertAllResponse{insertErrors: nil}}
+      end)
+
+      user = insert(:user, system_monitoring: true)
+      metrics_source = insert(:source, user: user, system_source_type: :metrics)
+
+      webhook_backend =
+        insert(:backend,
+          user: user,
+          type: :webhook,
+          config: %{url: "http://127.0.0.1:9999/webhook"},
+          metadata: %{"environment" => "test", "region" => "us-west"}
+        )
+
+      source = insert(:source, user: user)
+
+      start_supervised!({SourceSup, metrics_source}, id: :metrics_source)
+      start_supervised!({SourceSup, source}, id: :source)
+
+      {:ok, _} = Backends.update_source_backends(source, [webhook_backend])
+      Backends.Cache.get_backend(webhook_backend.id)
+
+      :timer.sleep(1000)
+
+      assert {:ok, _} = Backends.ingest_logs([%{"message" => "test webhook egress"}], source)
+
+      :timer.sleep(2500)
+
+      assert_receive {:insert_all, [%{json: %{"attributes" => _}} | _] = rows}, 5_000
+
+      rows = for row <- rows, do: row.json
+
+      egress_row =
+        Enum.find(rows, &match?(%{"event_message" => "logflare.backends.ingest.egress.request_bytes"}, &1))
+
+      assert egress_row, "Expected egress metric to be present"
+
+      [attributes] = egress_row["attributes"]
+
+      assert attributes["source_id"] == source.id
+      assert attributes["backend_id"] == webhook_backend.id
+      assert attributes["_backend_environment"] == "test"
+      assert attributes["_backend_region"] == "us-west"
+    end
+  end
+
   describe "endpoints" do
     setup :start_otel_exporter
 
@@ -228,14 +289,14 @@ defmodule Logflare.Backends.UserMonitoringTest do
       GoogleApi.BigQuery.V2.Api.Tabledata
       |> stub(:bigquery_tabledata_insert_all, fn _conn,
                                                  _project_id,
-                                                 dataset_id,
+                                                 _dataset_id,
                                                  _table_name,
                                                  opts ->
         send(pid, {:insert_all, opts[:body].rows})
         {:ok, %GoogleApi.BigQuery.V2.Model.TableDataInsertAllResponse{insertErrors: nil}}
       end)
 
-      expect(GoogleApi.BigQuery.V2.Api.Jobs, :bigquery_jobs_query, 1, fn _conn, _proj_id, opts ->
+      expect(GoogleApi.BigQuery.V2.Api.Jobs, :bigquery_jobs_query, 1, fn _conn, _proj_id, _opts ->
         {:ok, TestUtils.gen_bq_response([%{"result" => "1"}])}
       end)
 
