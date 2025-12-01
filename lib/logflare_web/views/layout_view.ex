12261cad50d6a401ac4f9528556ffc3720bc8d8b
Ziinc <Ziinc@users.noreply.github.com>
2025-12-01 18:10:54 +0800

    fix: navbar session fetching (#2982)
    * fix: navbar session fetching
    
    * chore: add direct error logging for ui routes
    
    * fix: maybe add in error status if not set
    
    * fix: error log only for ui routes
    
    * chore: version bump
    
    * chore: tweak canary deployment
    
    * chore: increase canary surge
    
    * chore: drop surge amount
    
    * chore: add gitignore for tooling
    
    * chore: remove error handler
    
    * chore: version bump

--- lib/logflare_web/views/layout_view.ex
+++ lib/logflare_web/views/layout_view.ex
@@ -1,4 +1,18 @@
 defmodule LogflareWeb.LayoutView do
   use LogflareWeb, :view
+
   import LogflareWeb.Helpers.PageTitle
+
+  @doc """
+  Safely retrieves a value from the session, returning default if session hasn't been fetched.
+  This prevents ArgumentError when rendering error pages before session fetch.
+  """
+  @spec safe_get_session(Plug.Conn.t(), atom(), any()) :: any()
+  def safe_get_session(conn, key, default \\ nil) do
+    if Map.get(conn.private, :plug_session_fetch) == :done do
+      Plug.Conn.get_session(conn, key) || default
+    else
+      default
+    end
+  end
 end
