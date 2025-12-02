84ca5729bda6bff27c8516081a5c8920747c4720
Matt Stubbs <matt@yellowrobot.com.au>
2025-12-03 09:10:43 +1000

    refactor: replace live_modal_show_link references with <.modal_link /> component

--- lib/logflare_web/live/modal_live_helpers.ex
+++ lib/logflare_web/live/modal_live_helpers.ex
@@ -4,8 +4,6 @@ defmodule LogflareWeb.ModalLiveHelpers do
   """
   use Phoenix.Component
 
-  import Phoenix.HTML.Link, only: [link: 2]
-
   alias Phoenix.LiveView.JS
 
   defmacro __using__(_context) do
@@ -58,57 +56,6 @@ defmodule LogflareWeb.ModalLiveHelpers do
     end
   end
 
-  @doc """
-  Creates a link to show a modal when clicked.
-
-  # Customize click behaviour
-  You can optionally assign a custom JS command to `click` which will be executed before showing the modal.
-
-  ```
-    <%= live_modal_show_link(component: LogflareWeb.MyComponent, click: JS.push("some_event"), ...) %>
-  ```
-  """
-  def live_modal_show_link(content \\ [], opts)
-
-  def live_modal_show_link(opts, do: block) when is_list(opts) do
-    live_modal_show_link(block, opts)
-  end
-
-  def live_modal_show_link(contents, opts) when is_list(opts) do
-    {type, module_or_template} =
-      Enum.find(opts, &match?({k, _} when k in [:component, :live_view, :template], &1))
-
-    id = Keyword.fetch!(opts, :modal_id)
-    title = Keyword.fetch!(opts, :title)
-    view = Keyword.get(opts, :view)
-    return_to = Keyword.get(opts, :return_to)
-
-    click =
-      case Keyword.get(opts, :click) do
-        nil ->
-          :show_live_modal
-
-        js_command when is_struct(js_command, JS) ->
-          js_command
-          |> JS.push("show_live_modal")
-      end
-
-    opts =
-      [
-        to: "#",
-        phx_click: click,
-        phx_value_close: Keyword.get(opts, :close),
-        phx_value_module_or_template: module_or_template,
-        phx_value_type: type,
-        phx_value_id: id,
-        phx_value_title: title,
-        phx_value_return_to: return_to,
-        phx_value_view: view
-      ] ++ opts
-
-    link(contents, opts)
-  end
-
   def live_modal(template, opts)
       when is_binary(template) do
     path = Keyword.fetch!(opts, :return_to)
@@ -147,7 +94,12 @@ defmodule LogflareWeb.ModalLiveHelpers do
   end
 
   @doc """
-  Component version of live_modal_show_link for use in HEEx templates.
+  Creates a link to show a modal when clicked.
+
+  Only supports component modals.
+
+  ## Customize click behaviour
+  You can optionally assign a custom JS command to `click` which will be executed before showing the modal.
 
   ## Example
 
@@ -179,15 +131,15 @@ defmodule LogflareWeb.ModalLiveHelpers do
 
     click =
       case assigns.click do
-        nil -> JS.push("show_live_modal")
+        nil -> "show_live_modal"
         js_command when is_struct(js_command, JS) -> JS.push(js_command, "show_live_modal")
       end
 
     assigns =
       assigns
-      |> Phoenix.Component.assign(:type, type)
-      |> Phoenix.Component.assign(:module_or_template, module_or_template)
-      |> Phoenix.Component.assign(:phx_click, click)
+      |> assign(:module_or_template, module_or_template)
+      |> assign(:phx_click, click)
+      |> assign(:type, type)
 
     ~H"""
     <.link href="#" class={@class} phx-click={@phx_click} phx-value-close={@close} phx-value-module-or-template={@module_or_template} phx-value-type={@type} phx-value-id={@modal_id} phx-value-title={@title} phx-value-return-to={@return_to} phx-value-view={@view} {@rest}>
