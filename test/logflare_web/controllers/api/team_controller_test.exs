104451aeedecad330e4275f9009bbb8860a9f792
Matt Stubbs <matt@yellowrobot.com.au>
2025-12-10 14:48:00 +1000

    feat: admin scope required for /api/teams and /api/backends

--- test/logflare_web/controllers/api/team_controller_test.exs
+++ test/logflare_web/controllers/api/team_controller_test.exs
@@ -100,7 +100,7 @@ defmodule LogflareWeb.Api.TeamControllerTest do
 
       response =
         conn
-        |> add_access_token(user, "private")
+        |> add_access_token(user, "private:admin")
         |> post(~p"/api/teams", %{name: name})
         |> json_response(201)
         |> assert_schema("Team")
@@ -111,7 +111,7 @@ defmodule LogflareWeb.Api.TeamControllerTest do
     test "returns 422 on bad arguments", %{conn: conn, user: user} do
       response =
         conn
-        |> add_access_token(user, "private")
+        |> add_access_token(user, "private:admin")
         |> post(~p"/api/teams", %{name: 123})
         |> json_response(422)
         |> assert_schema("UnprocessableEntityResponse")
@@ -122,12 +122,20 @@ defmodule LogflareWeb.Api.TeamControllerTest do
     test "returns 422 on missing arguments", %{conn: conn, user: user} do
       response =
         conn
-        |> add_access_token(user, "private")
+        |> add_access_token(user, "private:admin")
         |> post(~p"/api/teams")
         |> json_response(422)
         |> assert_schema("UnprocessableEntityResponse")
 
       assert response == %{errors: %{"name" => ["can't be blank"]}}
+    end
+
+    test "admin scope is required", %{conn: conn, user: user} do
+      assert conn
+             |> add_access_token(user, "private")
+             |> post(~p"/api/teams", %{name: TestUtils.random_string()})
+             |> response(401)
+             |> assert_schema("Unauthorized") == ~s|{"error":"Unauthorized"}|
     end
   end
 
@@ -141,7 +149,7 @@ defmodule LogflareWeb.Api.TeamControllerTest do
 
       response =
         conn
-        |> add_access_token(user, "private")
+        |> add_access_token(user, "private:admin")
         |> patch(~p"/api/teams/#{main_team.token}", %{name: name})
         |> response(204)
         |> assert_schema("AcceptedResponse")
@@ -152,7 +160,7 @@ defmodule LogflareWeb.Api.TeamControllerTest do
 
       response =
         conn
-        |> add_access_token(user, "private")
+        |> add_access_token(user, "private:admin")
         |> put(~p"/api/teams/#{main_team.token}", %{name: another_name})
         |> json_response(201)
         |> assert_schema("Team")
@@ -165,7 +173,7 @@ defmodule LogflareWeb.Api.TeamControllerTest do
       invalid_user = insert(:user)
 
       conn
-      |> add_access_token(invalid_user, "private")
+      |> add_access_token(invalid_user, "private:admin")
       |> patch(~p"/api/teams/#{main_team.token}", %{name: TestUtils.random_string()})
       |> json_response(404)
       |> assert_schema("NotFoundResponse")
@@ -174,12 +182,20 @@ defmodule LogflareWeb.Api.TeamControllerTest do
     test "returns 422 on bad arguments", %{conn: conn, user: user, main_team: main_team} do
       response =
         conn
-        |> add_access_token(user, "private")
+        |> add_access_token(user, "private:admin")
         |> patch(~p"/api/teams/#{main_team.token}", %{name: 123})
         |> json_response(422)
         |> assert_schema("UnprocessableEntityResponse")
 
       assert response == %{errors: %{"name" => ["is invalid"]}}
+    end
+
+    test "admin scope is required", %{conn: conn, user: user, main_team: main_team} do
+      assert conn
+             |> add_access_token(user, "private")
+             |> patch(~p"/api/teams/#{main_team.token}", %{name: TestUtils.random_string()})
+             |> response(401)
+             |> assert_schema("Unauthorized") == ~s|{"error":"Unauthorized"}|
     end
   end
 
@@ -190,7 +206,7 @@ defmodule LogflareWeb.Api.TeamControllerTest do
       main_team: main_team
     } do
       assert conn
-             |> add_access_token(user, "private")
+             |> add_access_token(user, "private:admin")
              |> delete(~p"/api/teams/#{main_team.token}")
              |> response(204)
              |> assert_schema("AcceptedResponse") == ""
@@ -200,10 +216,18 @@ defmodule LogflareWeb.Api.TeamControllerTest do
       invalid_user = insert(:user)
 
       assert conn
-             |> add_access_token(invalid_user, "private")
+             |> add_access_token(invalid_user, "private:admin")
              |> delete(~p"/api/teams/#{main_team.token}")
              |> json_response(404)
              |> assert_schema("NotFoundResponse") == %{error: "Not Found"}
+    end
+
+    test "admin scope is required", %{conn: conn, user: user, main_team: main_team} do
+      assert conn
+             |> add_access_token(user, "private")
+             |> delete(~p"/api/teams/#{main_team.token}")
+             |> response(401)
+             |> assert_schema("Unauthorized") == ~s|{"error":"Unauthorized"}|
     end
   end
 end
