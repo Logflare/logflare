d6875201ffb14261dff94fb6228050f8ee07ec67
Luis Araújo <44007026+laraujo7@users.noreply.github.com>
2025-12-17 11:15:55 +0000

    test(e2e): checks if logs from Storage are received (#3046)

--- test/e2e/supabase/features/logs.spec.ts
+++ test/e2e/supabase/features/logs.spec.ts
@@ -2,7 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test.beforeAll(async ({ request }) => {
   await request.get('/api/platform/organizations');
-  await new Promise(r => setTimeout(r, 5000))
+
+  await request.post('/api/platform/storage/default/buckets', { data: {id: "bucket", type: "STANDARD", public: false}});
+  await request.post('/storage/v1/object/bucket/folder/.emptyFolderPlaceholder');
+
+  await new Promise(r => setTimeout(r, 10000))
 });
 
 test('receives logs from API Gateway', async ({ page }) => {
@@ -11,4 +15,12 @@ test('receives logs from API Gateway', async ({ page }) => {
   await page.press('input[placeholder="Search events"]', 'Enter');
 
   await expect(page.getByRole('table')).toContainText('/api/platform/organizations');
+});
+
+test('receives logs from Storage', async ({ page }) => {
+  await page.goto('/project/default/logs/storage-logs');
+  await page.fill('input[placeholder="Search events"]', 'folder');
+  await page.press('input[placeholder="Search events"]', 'Enter');
+
+  await expect(page.getByRole('table')).toContainText('/object/bucket/folder/.emptyFolderPlaceholder');
 });
