8187aada07f421f8d70e623e2f6c54e87507fae2
TzeYiing <ty@tzeyiing.com>
2026-01-05 22:19:18 +0800

    perf: increase chunk size for add_to_table

--- test/profiling/ingest_event_queue_add_to_table_bench.exs
+++ test/profiling/ingest_event_queue_add_to_table_bench.exs
@@ -28,8 +28,23 @@ inputs = %{
 
 Benchee.run(
   %{
-    "add_to_table" => fn events ->
-      IngestEventQueue.add_to_table({source_id, backend_id}, events)
+    # "add_to_table" => fn events ->
+    #   IngestEventQueue.add_to_table({source_id, backend_id}, events)
+    # end,
+    "add_to_table - 50 chunk size" => fn events ->
+      IngestEventQueue.add_to_table({source_id, backend_id}, events, chunk_size: 50)
+    end,
+    "add_to_table - 100 chunk size" => fn events ->
+      IngestEventQueue.add_to_table({source_id, backend_id}, events, chunk_size: 100)
+    end,
+    "add_to_table - 250 chunk size" => fn events ->
+      IngestEventQueue.add_to_table({source_id, backend_id}, events, chunk_size: 250)
+    end,
+    "add_to_table - 500 chunk size" => fn events ->
+      IngestEventQueue.add_to_table({source_id, backend_id}, events, chunk_size: 500)
+    end,
+    "add_to_table - 1000 chunk size" => fn events ->
+      IngestEventQueue.add_to_table({source_id, backend_id}, events, chunk_size: 1000)
     end
     # "old (legacy)" => fn events ->
     #   IngestEventQueue.add_to_table({source_id, backend_id}, events, legacy: true)
@@ -67,3 +82,39 @@ Benchee.run(
 
 # Name         Reduction count
 # add_to_table          7.19 K
+
+## 2026-01-05  Increase Chunk Size ##
+# ##### With input 1000 events #####
+# Name                                     ips        average  deviation         median         99th %
+# add_to_table - 100 chunk size         4.80 K      208.52 μs     ±6.77%      208.13 μs      255.90 μs
+# add_to_table - 50 chunk size          4.69 K      213.11 μs     ±7.91%      212.71 μs      244.80 μs
+# add_to_table - 250 chunk size         4.52 K      221.04 μs     ±7.38%      219.17 μs      265.13 μs
+# add_to_table - 1000 chunk size        4.45 K      224.51 μs    ±10.00%      224.08 μs      261.65 μs
+# add_to_table - 500 chunk size         4.13 K      242.20 μs     ±6.15%      241.21 μs      283.29 μs
+
+# Comparison:
+# add_to_table - 100 chunk size         4.80 K
+# add_to_table - 50 chunk size          4.69 K - 1.02x slower +4.59 μs
+# add_to_table - 250 chunk size         4.52 K - 1.06x slower +12.52 μs
+# add_to_table - 1000 chunk size        4.45 K - 1.08x slower +15.99 μs
+# add_to_table - 500 chunk size         4.13 K - 1.16x slower +33.68 μs
+
+# Memory usage statistics:
+
+# Name                              Memory usage
+# add_to_table - 100 chunk size        128.26 KB
+# add_to_table - 50 chunk size         135.13 KB - 1.05x memory usage +6.88 KB
+# add_to_table - 250 chunk size        124.13 KB - 0.97x memory usage -4.12500 KB
+# add_to_table - 1000 chunk size       122.05 KB - 0.95x memory usage -6.20313 KB
+# add_to_table - 500 chunk size        122.76 KB - 0.96x memory usage -5.50000 KB
+
+# **All measurements for memory usage were the same**
+
+# Reduction count statistics:
+
+# Name                           Reduction count
+# add_to_table - 100 chunk size           6.98 K
+# add_to_table - 50 chunk size            7.20 K - 1.03x reduction count +0.22 K
+# add_to_table - 250 chunk size           6.88 K - 0.99x reduction count -0.09900 K
+# add_to_table - 1000 chunk size          7.13 K - 1.02x reduction count +0.151 K
+# add_to_table - 500 chunk size           8.37 K - 1.20x reduction count +1.39 K
