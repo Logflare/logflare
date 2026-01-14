d81ea9f2c8c321b8805890511a63d0a182cb6db7
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2026-01-14 13:46:35 +0100

    Fix tests and bug at busting

--- test/logflare/rules/rule_cache_test.exs
+++ test/logflare/rules/rule_cache_test.exs
@@ -18,6 +18,26 @@ defmodule Logflare.Rules.CacheTest do
   end
 
   describe "rules cache" do
+    test "get rules", %{source: source, rule_ids: rule_ids} do
+      assert rules = @subject.get_rules(rule_ids)
+
+      for %Rule{id: id} <- rules do
+        assert id in rule_ids
+      end
+
+      assert Cachex.size!(@subject) == 2
+      assert %{hits: 0, writes: 2} = Cachex.stats!(@subject)
+
+      Mimic.reject(Rules, :get_rule, 1)
+
+      assert [_r1, _r2] = @subject.get_rules(rule_ids)
+      assert %{hits: 2, writes: 2} = Cachex.stats!(@subject)
+
+      [rid1, _rid2] = rule_ids
+      assert %Rule{id: ^rid1} = @subject.get_rule(rid1)
+      assert %{hits: 3, writes: 2} = Cachex.stats!(@subject)
+    end
+
     test "list by source", %{source: source, rule_ids: expected_rule_ids} do
       assert rules = @subject.list_by_source_id(source.id)
 
@@ -68,18 +88,38 @@ defmodule Logflare.Rules.CacheTest do
       assert %{hits: 2} = Cachex.stats!(@subject)
     end
 
-    test "key busting", %{source: source, backend: backend} do
+    test "source id key busting", %{source: source, backend: backend} do
       assert [_r1, _r2] = @subject.list_rules(source)
-      assert [_r1, _r2] = @subject.list_rules(backend)
+      assert _ = @subject.rules_tree_by_source_id(source.id)
       assert %{misses: 2, writes: 2} = Cachex.stats!(@subject)
 
-      assert {:ok, 1} = @subject.bust_by(source_id: source.id)
+      assert {:ok, 2} = @subject.bust_by(source_id: source.id)
       assert [_r1, _r2] = @subject.list_rules(source)
       assert %{misses: 3, writes: 3} = Cachex.stats!(@subject)
 
+      assert _ = @subject.rules_tree_by_source_id(source.id)
+      assert %{misses: 4, writes: 4} = Cachex.stats!(@subject)
+    end
+
+    test "backend id key busting", %{source: source, backend: backend} do
+      assert [_r1, _r2] = @subject.list_rules(backend)
+      assert %{misses: 1, writes: 1} = Cachex.stats!(@subject)
+
       assert {:ok, 1} = @subject.bust_by(backend_id: backend.id)
       assert [_r1, _r2] = @subject.list_rules(backend)
-      assert %{misses: 4, writes: 4} = Cachex.stats!(@subject)
+      assert %{misses: 2, writes: 2} = Cachex.stats!(@subject)
+    end
+
+    test "rule id key busting", %{rule_ids: [rid1, rid2]} do
+      assert _r1 = @subject.get_rule(rid1)
+      assert %{misses: 1, writes: 1} = Cachex.stats!(@subject)
+
+      assert {:ok, 1} = @subject.bust_by(id: rid1)
+      assert _r1 = @subject.get_rule(rid1)
+      assert %{misses: 2, writes: 2} = Cachex.stats!(@subject)
+
+      # Bust missing key
+      assert {:ok, 0} = @subject.bust_by(id: rid2)
     end
 
     test "cache warming" do
