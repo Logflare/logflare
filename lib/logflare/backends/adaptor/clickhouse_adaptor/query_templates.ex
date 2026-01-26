acaf81ac781aef0e5907e95106d6983d3a4664bf
Adam Mokan <amokan@gmail.com>
2026-01-26 11:55:31 -0700

    change ingest table naming convention to `consolidated_ingest_<backend-token>` to avoid issues when deploying

--- lib/logflare/backends/adaptor/clickhouse_adaptor/query_templates.ex
+++ lib/logflare/backends/adaptor/clickhouse_adaptor/query_templates.ex
@@ -11,7 +11,7 @@ defmodule Logflare.Backends.Adaptor.ClickHouseAdaptor.QueryTemplates do
   @doc """
   Default naming prefix for ingest tables.
   """
-  def default_table_name_prefix, do: "ingest"
+  def default_table_name_prefix, do: "consolidated_ingest"
 
   @doc """
   Generates a ClickHouse query statement to check that the user GRANTs include the needed permissions.
