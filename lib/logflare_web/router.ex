c3d35b4735adf420bf41830624f9d79acf97cbf9
Matt Stubbs <matt@yellowrobot.com.au>
2026-01-28 13:34:27 +1000

    fix: don't require admin token for  Api.KeyValueController routes

--- lib/logflare_web/router.ex
+++ lib/logflare_web/router.ex
@@ -493,6 +493,10 @@ defmodule LogflareWeb.Router do
       param: "token",
       only: [:index, :show]
     )
+
+    get "/key-values", Api.KeyValueController, :index
+    post "/key-values", Api.KeyValueController, :create
+    delete "/key-values", Api.KeyValueController, :delete
   end
 
   scope "/api", LogflareWeb do
@@ -511,10 +515,6 @@ defmodule LogflareWeb.Router do
 
       post("/:token/test", Api.BackendController, :test_connection)
     end
-
-    get "/key-values", Api.KeyValueController, :index
-    post "/key-values", Api.KeyValueController, :create
-    delete "/key-values", Api.KeyValueController, :delete
   end
 
   scope "/api/partner", LogflareWeb do
