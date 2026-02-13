ec6bf1e8852879ff198029481d7847d814595d6c
TzeYiing <ty@tzeyiing.com>
2026-02-13 20:33:20 +0800

    chore: fix failing test

--- test/logflare/log_event_test.exs
+++ test/logflare/log_event_test.exs
@@ -157,7 +157,8 @@ defmodule Logflare.LogEventTest do
                  source: %{source | transform_key_values: nil, transform_key_values_parsed: nil}
                })
 
-      assert body == %{"project" => "abc"}
+      assert body["project"] == "abc"
+      refute Map.has_key?(body, "enriched")
     end
 
     test "2-part pattern sets entire map at destination (pre-parsed)", %{source: source} do
