5e1f744d8e6c0785437867682a1ced2be77d58cf
Ziinc <Ziinc@users.noreply.github.com>
2026-03-11 16:39:42 +0800

    fix: correctly order partitioning and clustering fields. (#3265)

--- lib/logflare/logs/log_events.ex
+++ lib/logflare/logs/log_events.ex
@@ -53,9 +53,9 @@ defmodule Logflare.Logs.LogEvents do
 
     query =
       from(bq_table_id)
-      |> Lql.apply_filter_rules(lql_rules)
-      |> where([t], t.id == ^id)
       |> partition_query([min, max], partition_type)
+      |> where([t], t.id == ^id)
+      |> Lql.apply_filter_rules(lql_rules)
       |> select([t], fragment("*"))
 
     BigQueryAdaptor.execute_query({bq_project_id, dataset_id, source.user.id}, query,
