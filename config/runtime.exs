9e7e58b9b157a98463ed6264a2479e837701bd75
TzeYiing <ty@tzeyiing.com>
2026-05-05 22:46:45 +0800

    fix: ssl_opts should not be used, add :server_name_indication option

--- config/runtime.exs
+++ config/runtime.exs
@@ -342,19 +342,21 @@ if(
     File.exists?("db-client-cert.pem") && File.exists?("db-client-key.pem")
 ) do
   config :logflare, Logflare.Repo,
-    ssl: true,
-    ssl_opts: [
+    ssl: [
       #  ssl opts follow recs here: https://erlef.github.io/security-wg/secure_coding_and_deployment_hardening/ssl
       verify: :verify_peer,
-      cacertfile: "db-server-ca.pem",
-      certfile: "db-client-cert.pem",
-      keyfile: "db-client-key.pem",
-      versions: [:"tlsv1.2"],
-      # support wildcard
+      cacertfile: Path.absname("db-server-ca.pem"),
+      certfile: Path.absname("db-client-cert.pem"),
+      keyfile: Path.absname("db-client-key.pem"),
+      depth: 3,
+      versions: [:"tlsv1.2", :"tlsv1.3"],
+      # otherwise bare IPv4s will fail
+      server_name_indication: :disable,
       customize_hostname_check: [
         match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
       ]
-    ]
+    ],
+    show_sensitive_data_on_connection_error: true
 end
 
 case System.get_env("LOGFLARE_FEATURE_FLAG_OVERRIDE") do
