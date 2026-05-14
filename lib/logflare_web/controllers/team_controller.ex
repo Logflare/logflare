e11528dc84fa4db9e117f5500f4aa08e12b9897b
Matt Stubbs <22266+msmithstubbs@users.noreply.github.com>
2026-05-14 16:16:48 +1000

    feat: team switcher redirects to resource index (#3445)

--- lib/logflare_web/controllers/team_controller.ex
+++ lib/logflare_web/controllers/team_controller.ex
@@ -1,6 +1,7 @@
 defmodule LogflareWeb.TeamController do
   use LogflareWeb, :controller
 
+  alias Logflare.Teams.Team
   alias Logflare.Teams.TeamContext
 
   def switch(conn, %{"team_id" => team_id, "redirect_to" => redirect_to}) do
@@ -10,7 +11,7 @@ defmodule LogflareWeb.TeamController do
       {:ok, %{team: team}} ->
         conn
         |> put_session(:last_switched_team_id, team.id)
-        |> redirect(to: LogflareWeb.Utils.with_team_param(redirect_to, team))
+        |> redirect(to: index_path(redirect_to, team))
 
       {:error, _reason} ->
         conn
@@ -18,4 +19,19 @@ defmodule LogflareWeb.TeamController do
         |> redirect(to: redirect_to)
     end
   end
+
+  @spec index_path(String.t(), Team.t()) :: String.t()
+  defp index_path(redirect_to, team) do
+    redirect_to
+    |> URI.parse()
+    |> Map.get(:path)
+    |> resource_index_path()
+    |> LogflareWeb.Utils.with_team_param(team)
+  end
+
+  @spec resource_index_path(String.t() | nil) :: String.t()
+  defp resource_index_path("/sources" <> _), do: ~p"/dashboard"
+  defp resource_index_path("/endpoints" <> _), do: ~p"/endpoints"
+  defp resource_index_path("/alerts" <> _), do: ~p"/alerts"
+  defp resource_index_path(_), do: ~p"/dashboard"
 end
