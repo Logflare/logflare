f6ad213169846801ba35d11c77e79c89fb047e97
Brian <brianshan@gmail.com>
2026-06-04 14:15:56 +0100

    Adapt pull producer to work with new otelmetric exporter

--- mix.exs
+++ mix.exs
@@ -254,7 +254,7 @@ defmodule Logflare.Mixfile do
       {:opentelemetry_phoenix, "~> 2.0.0-rc.2"},
       {:opentelemetry_bandit, "~> 0.2.0-rc.1"},
       {:otel_metric_exporter,
-       git: "https://github.com/supabase/elixir-otel-metric-exporter", ref: "cb9dbfc"},
+       path: "../elixir-otel-metric-exporter"},
       {:live_monaco_editor, "~> 0.2"}
     ]
   end
