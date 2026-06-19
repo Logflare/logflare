e78099982f420f48ec8617ba7f20e02ac53da3ff
Brian <brianshan@gmail.com>
2026-06-19 16:08:58 +0100

    Update metric exporter and reduce batch size.

--- lib/logflare/backends/user_monitoring.ex
+++ lib/logflare/backends/user_monitoring.ex
@@ -61,7 +61,7 @@ defmodule Logflare.Backends.UserMonitoring do
        [
          metric_store_name: @store_name,
          pull_interval: pull_interval,
-         batch_size: 1_000
+         batch_size: 250
        ]}
 
     [exporter_spec, pipeline_spec]
