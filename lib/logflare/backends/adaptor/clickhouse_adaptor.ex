d8d036fce7ebb7f04fb3281ec926727520cd3a6b
Brian <brianshan@gmail.com>
2026-07-20 18:46:23 +0100

    Format

--- lib/logflare/backends/adaptor/clickhouse_adaptor.ex
+++ lib/logflare/backends/adaptor/clickhouse_adaptor.ex
@@ -622,7 +622,6 @@ defmodule Logflare.Backends.Adaptor.ClickHouseAdaptor do
 
     # other to lazily create the generation (see IngestEventQueue.current_generation_tid/1)
 
-    
     # IngestEventQueue.upsert_tid({:consolidated, backend.id, nil})
     IngestEventQueue.current_generation_tid({:consolidated, backend.id})
 
@@ -653,7 +652,7 @@ defmodule Logflare.Backends.Adaptor.ClickHouseAdaptor do
     Supervisor.init(children, strategy: :one_for_one)
   end
 
-# produce fewer, larger batches for ClickHouse efficiency
+  # produce fewer, larger batches for ClickHouse efficiency
   #
   # Exposed (not private) so it can be unit tested directly, same convention as
   # Backends.handle_resolve_count/3 (BigQuery's counterpart).
