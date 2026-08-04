f241d30fb94756c9b3beb01809968fb65c42dbf0
ishleenk17 <ishleen.kaur@elastic.co>
2026-08-04 16:29:08 +0530

    Remove the alias and add transport type (otlp) in the elastic adaptor

--- lib/logflare_web/live/backends/backends_live.ex
+++ lib/logflare_web/live/backends/backends_live.ex
@@ -41,6 +41,7 @@ defmodule LogflareWeb.BackendsLive do
       |> assign(:show_alert_form?, false)
       |> assign(:alert_options, [])
       |> assign(:form_type, nil)
+      |> assign(:elastic_transport, "filebeat")
       |> assign(:show_default_ingest_form?, false)
       |> assign(:default_ingest_sources, [])
       |> assign(:flag_multibackend, Logflare.Utils.flag("multibackend", user))
@@ -148,7 +149,26 @@ defmodule LogflareWeb.BackendsLive do
   end
 
   def handle_event("change_form_type", %{"backend" => %{"type" => type}}, socket) do
-    {:noreply, assign(socket, form_type: type)}
+    socket =
+      socket
+      |> assign(:form_type, type)
+      |> then(fn s ->
+        if type == "elastic" do
+          assign(s, :elastic_transport, "filebeat")
+        else
+          s
+        end
+      end)
+
+    {:noreply, socket}
+  end
+
+  def handle_event(
+        "change_elastic_transport",
+        %{"backend" => %{"config" => %{"transport" => transport}}},
+        socket
+      ) do
+    {:noreply, assign(socket, :elastic_transport, transport)}
   end
 
   def handle_event(
@@ -391,7 +411,6 @@ defmodule LogflareWeb.BackendsLive do
       {"Sentry", :sentry},
       {"Axiom", :axiom},
       {"OTLP", :otlp},
-      {"Elastic OTLP", :elastic_otlp},
       {"Last9", :last9},
       {"SigNoz", :signoz},
       {"Syslog", :syslog},
@@ -413,6 +432,7 @@ defmodule LogflareWeb.BackendsLive do
     |> assign(:backend, nil)
     |> assign(:connection_status, nil)
     |> assign(:form_type, nil)
+    |> assign(:elastic_transport, "filebeat")
   end
 
   defp refresh_backend(socket, id) do
@@ -445,10 +465,18 @@ defmodule LogflareWeb.BackendsLive do
       end)
       |> Enum.sort_by(& &1.name)
 
+    elastic_transport =
+      if backend.type == :elastic do
+        Logflare.Backends.Adaptor.ElasticAdaptor.transport(backend.config || %{})
+      else
+        "filebeat"
+      end
+
     socket
     |> assign(:backend, backend)
     |> assign(:connection_status, nil)
     |> assign(:form_type, Atom.to_string(backend.type))
+    |> assign(:elastic_transport, elastic_transport)
     |> assign(:default_ingest_sources, default_ingest_sources)
     |> assign(:available_sources, available_sources)
   end
