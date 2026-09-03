f4375aa7359a27eb1f394bf0351a27fe93bd0586
David Whittington <djwhitt@gmail.com>
2026-09-03 20:24:58 +0000

    test: exercise system cache startup with failing warmer

--- test/logflare/system_cache_test.exs
+++ test/logflare/system_cache_test.exs
@@ -35,6 +35,38 @@ defmodule Logflare.SystemCacheTest do
     refute warmer(warmer_spec, :required)
   end
 
+  test "cache starts while the optional warmer handles monitor exits" do
+    previous_env = Application.get_env(:logflare, :env)
+    Application.put_env(:logflare, :env, :prod)
+    on_exit(fn -> Application.put_env(:logflare, :env, previous_env) end)
+    set_mimic_global()
+
+    assert %{start: {Cachex, :start_link, [SystemCache, options]}} = SystemCache.child_spec(nil)
+    assert [warmer_spec] = Keyword.fetch!(options, :warmers)
+
+    cache_name = Module.concat(__MODULE__, IntegrationCache)
+    warmer_name = Module.concat(__MODULE__, IntegrationWarmer)
+    options = Keyword.put(options, :warmers, [warmer(warmer_spec, name: warmer_name)])
+    test_pid = self()
+
+    stub(Logflare.System, :memory_utilization, fn ->
+      send(test_pid, :warmer_called)
+      exit(:monitor_unavailable)
+    end)
+
+    log =
+      capture_log([level: :warning], fn ->
+        cache_pid = start_supervised!({Cachex, [cache_name, options]})
+
+        assert_receive :warmer_called
+        :sys.get_state(warmer_name)
+        assert Process.alive?(cache_pid)
+      end)
+
+    assert log =~ "SystemCache warmer failed"
+    assert log =~ "monitor_unavailable"
+  end
+
   test "warmer treats monitor exits as a cache miss" do
     stub(Logflare.System, :memory_utilization, fn -> exit(:monitor_unavailable) end)
 
