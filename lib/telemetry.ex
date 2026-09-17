cddb7b401b885cccf4a174b703738ffbe6ea827b
ruslandoga <ruslandoga+gh@icloud.com>
2026-09-17 13:51:10 +0700

    fix: count Bandit request stop events

--- lib/telemetry.ex
+++ lib/telemetry.ex
@@ -286,7 +286,7 @@ defmodule Logflare.Telemetry do
       ),
       counter("logflare.total_http_requests",
         measurement: :duration,
-        event_name: "bandit.request.stop.duration"
+        event_name: "bandit.request.stop"
       ),
       sum("logflare.logs.processor.ingest.logs.count",
         tags: [:processor],
